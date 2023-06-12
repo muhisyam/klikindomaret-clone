4a035eef23d677fafe498be0ec9fc738711b054c
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-06-12 21:13:01 +0700

    correcting some file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontendController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('frontend.index');
+    return view('frontend.home.index');
 });
+
+// Route::get('/', [FrontendController::class, 'index'])->name('frontendHome');
