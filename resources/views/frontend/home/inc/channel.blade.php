3ae3a35d0b87d1cdd08d6ad6b2e7822a7b77f16e
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-06-12 21:13:53 +0700

    add swiperjs

---
+++ resources/views/frontend/home/inc/channel.blade.php
@@ -0,0 +1,17 @@
+<div class="channel">
+    <div class="wrapper__channel min-h-[212px] py-4 px-6 bg-cover bg-no-repeat" style="background-image: url('https://www.klikindomaret.com/Assets/image/bg_other_produk.png')">
+        <h2 class="title">
+            Produk Lainnya
+        </h2>
+        <div class="swiper list__channel">
+            <div class="swiper-wrapper card__channel">
+                <a class="swiper-slide" href="#">
+                    <img class="w-[80px] p-4 bg-white rounded-md shadow" src="https://assets.klikindomaret.com/products/banners/Icon_Food.png" alt="">
+                    <h3 class="desc">
+                        Food
+                    </h3>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
