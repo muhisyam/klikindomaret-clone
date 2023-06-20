ec8646818108be7b49ebcfc168e22d1b386ace7c
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-06-20 21:27:00 +0700

    finish footer

--- resources/views/frontend/components/footer.blade.php
+++ resources/views/frontend/components/footer.blade.php
@@ -1,31 +1,17 @@
-<div class="footer">
-    <div class="footer-wrapper container mx-auto max-w-7xl">
+<div class="footer bg-white">
+    <div class="footer-wrapper container mx-auto max-w-7xl py-5">
         <div class="footer-content top-section grid grid-cols-6 text-sm text-[#95989A] mb-10">
             <div class="item-footer">
                 <div class="tagline text-[#313131] text-base font-bold mb-4">Klik Indomaret</div>
                 <div class="list-item-footer">
                     <ul>
-                        <li class="py-1.5">
-                            <a href="https://virtual.klikindomaret.com/">Virtual</a>
-                        </li>
-                        <li class="py-1.5">
-                            <a href="https://virtual.klikindomaret.com/">Travel</a>
-                        </li>
-                        <li class="py-1.5">
-                            <a href="https://virtual.klikindomaret.com/">Tiket Wahana</a>
-                        </li>
-                        <li class="py-1.5">
-                            <a href="https://virtual.klikindomaret.com/">Tentang Klik Indomaret</a>
-                        </li>
-                        <li class="py-1.5">
-                            <a href="https://virtual.klikindomaret.com/">Blog Klik Indomaret</a>
-                        </li>
-                        <li class="py-1.5">
-                            <a href="https://virtual.klikindomaret.com/">Food</a>
-                        </li>
-                        <li class="py-1.5">
-                            <a href="https://virtual.klikindomaret.com/">Official Store</a>
-                        </li>
+                        <li><a href="https://virtual.klikindomaret.com/">Virtual</a></li>
+                        <li><a href="https://virtual.klikindomaret.com/">Travel</a></li>
+                        <li><a href="https://virtual.klikindomaret.com/">Tiket Wahana</a></li>
+                        <li><a href="https://virtual.klikindomaret.com/">Tentang Klik Indomaret</a></li>
+                        <li><a href="https://virtual.klikindomaret.com/">Blog Klik Indomaret</a></li>
+                        <li><a href="https://virtual.klikindomaret.com/">Food</a></li>
+                        <li><a href="https://virtual.klikindomaret.com/">Official Store</a></li>
                     </ul>
                 </div>
             </div>
@@ -33,36 +19,16 @@
                 <div class="tagline text-[#313131] text-base font-bold mb-4">Bantuan</div>
                 <div class="list-item-footer">
                     <ul>
-                        <li class="py-1.5">
-                            <a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Kebijakan</a>
-                        </li>
-                        <li class="py-1.5">
-                            <a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Kebijakan Refund</a>
-                        </li>
-                        <li class="py-1.5">
-                            <a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Cara Berbelanja</a>
-                        </li>
-                        <li class="py-1.5">
-                            <a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Pertanyaan Umum</a>
-                        </li>
-                        <li class="py-1.5">
-                            <a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Produk Virtual</a>
-                        </li>
-                        <li class="py-1.5">
-                            <a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Produk Travel</a>
-                        </li>
-                        <li class="py-1.5">
-                            <a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Saldo Klik</a>
-                        </li>
-                        <li class="py-1.5">
-                            <a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Petunjuk Pembayaran</a>
-                        </li>
-                        <li class="py-1.5">
-                            <a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">i.saku & Poinku</a>
-                        </li>
-                        <li class="py-1.5">
-                            <a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Syarat & Ketentuan</a>
-                        </li>
+                        <li><a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Kebijakan</a></li>
+                        <li><a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Kebijakan Refund</a></li>
+                        <li><a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Cara Berbelanja</a></li>
+                        <li><a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Pertanyaan Umum</a></li>
+                        <li><a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Produk Virtual</a></li>
+                        <li><a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Produk Travel</a></li>
+                        <li><a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Saldo Klik</a></li>
+                        <li><a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Petunjuk Pembayaran</a></li>
+                        <li><a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">i.saku & Poinku</a></li>
+                        <li><a href="https://www.klikindomaret.com/bantuan/kebijakan-klikindomaret">Syarat & Ketentuan</a></li>
                     </ul>
                 </div>
             </div>
@@ -121,7 +87,7 @@
             </div>
         </div>
         <div class="footer-content bottom-section">
-            <div class="item-footer">
+            <div class="item-footer mb-10">
                 <div class="tagline text-[#313131] text-base font-bold mb-4">Metode Pembayaran</div>
                 <div class="list-item-footer">
                     <ul>
@@ -158,6 +124,79 @@
                         <li><span class="indo-paket"></span></li>
                     </ul>
                 </div>
+            </div>
+        </div>
+        <hr class="mt-5 mb-10">
+        <div class="footer-seo-content grid gap-x-5 grid-cols-2 text-xs text-[#B0B0B0]">
+            <div class="item-footer">
+                <div class="item-footer-wrapper mb-4">
+                    <h5 class="title text-[#595959] font-bold mb-2">Klik Indomaret, Toko Belanja Online 'One stop Online shop'</h5>
+                    <p>
+                        <strong>Klik Indomaret</strong> adalah toko online Indomaret atau tempat belanja online Indomaret, salah satu inovasi dari Indomaret berupa <strong><em>one stop online store</em></strong> yang menyediakan berbagai macam produk dalam satu situs untuk memenuhi semua kebutuhan konsumen.
+                    </p>
+                    <p>
+                        Klik Indomaret menyediakan ribuan pilihan produk yang lengkap dengan harga terbaik dari segala kebutuhan. Beragam produk serta promosi pilihan hadir untuk Anda, mulai dari kebutuhan sehari-hari seperti <strong><em>grocery</em>, makanan, minuman, kebutuhan bayi & ibu menyusui, produk <em>fashion</em>, kebutuhan pakaian bayi dan anak, produk kecantikan kesehatan, produk <em>gadget</em> & peralatan elektronik rumah tangga, kebutuhan hobi, produk pesona nusantara, <em>florist</em> atau <em>bouquet</em> bunga, serta <em>home living</em></strong> atau kebutuhan rumah tangga lainnya seperti kompor, elpiji, dan beragam produk rumah tangga lainnya.
+                    </p>
+                    <p>
+                        Kami juga menghadirkan layanan <strong>produk virtual</strong> untuk memenuhi kebutuhan pembelian <strong>pulsa HP dan paket data internet</strong> anda sehari-hari, seperti dari pengisian <strong>pulsa online, pembelian paket data internet hingga voucher <em>game online (Google Play, UniPin, dll).</em></strong> Transaksi anda menjadi lebih mudah dan praktis tanpa membawa uang tunai & menunggu kembalian, cukup isi ulang / top up e-Money karena adanya keuntungan dari penjualan uang elektronik.
+                    </p>
+                    <p>
+                        Bertambahnya layanan baru untuk menunjang proses bisnis online, Klik Indomaret juga menghadirkan beragam fitur belanja online seperti <strong>Produk virtual, Tiket Travel, Tiket Wahana</strong> diantaranya penjualan online <strong>Tiket Kereta Api</strong>, penjualan <strong>Tiket Wahana & Rekreasi,</strong> pembayaran tagihan <strong>BPJS Kesehatan,</strong> bayar tagihan <strong>BPJS</strong> Ketenagakerjaan, bayar <strong>tagihan Air (PDAM),</strong> bayar <strong>tagihan gas,</strong> bayar tagihan <strong>Listrik PLN,</strong> bayar tagihan Asuransi, bayar tagihan cicilan kendaraan atau tagihan multifinance, bayar tagihan <strong>Telkom</strong> ataupun tagihan <strong>telepon pascabayar,</strong> bayar tagihan <strong>Pajak Bumi Bangunan (PBB),</strong> bayar tagihan <strong>internet & TV Kabel,</strong> bayar tagihan <strong>pendidikan,</strong> bayar tagihan <strong>Iuran Pemeliharaan Lingkungan (IPL),</strong> bayar tagihan <strong>STNK (E-Samsat online)</strong> serta beragam produk virtual <strong>payment point</strong> untuk menunjang kemudahan transaksi pembayaran Anda.
+                    </p>
+                    <p>
+                        Inovasi <strong>one stop online store</strong> Klik Indomaret menghadirkan pilihan beragam kebutuhan belanja online sehingga Anda dengan mudah mendapatkan beragam benefit promo spesial dengan harga promo menarik. Kami menawarkan beragam promosi online seperti promo <strong>flash sale,</strong> promosi tebus murah, promo berhadiah, promo <strong>HTH (Hanya Tiga Hari), promo hari gajian</strong> atau <strong>payday</strong> dan promosi menarik setiap harinya. Untuk itu, jangan lewatkan kesempatan Anda untuk mendapatkan penawaran terbaru dan belanja online terbaik lewat promo-promo terbaik Kami.
+                    </p>
+                </div>
+                <div class="item-footer-wrapper">
+                    <h5 class="title text-[#595959] font-bold mb-2">Kemudahan dan Keamanan bertransaksi online</h5>
+                    <p>
+                        Kemudahan dan Keamanan dalam bertransaksi online merupakan nilai lebih Kami kepada Anda sebagai situs belanja online <strong><em>‘One Stop Online Shop’</em></strong> Klik Indomaret Anda dapat memilih berbagai metode pembayaran, seperti transfer antar ATM, <strong>bayar ditempat</strong> atau <strong>Cash On Delivery / COD, <em>internet banking</em></strong> (BCA Klikpay, Mandiri Clickpay, CIMB Niaga), <strong>pembayaran uang elektronik</strong> (Rekening Ponsel, LinkAja, i.saku), <strong><em>virtual payment point,</em> ATM Transfer, Akulaku dan bayar tunai</strong> di Toko Indomaret di Indonesia, hingga pembayaran <strong>kartu kredit (Visa, Master Card, JCB)</strong> dari partner Bank Kami.
+                    </p>
+                    <p>
+                        Kami bekerjasama dengan partner Bank di Indonesia diantaranya <strong>Bank BCA, Bank CIMB Niaga, Bank BNI, Bank BRI, Bank Mandiri</strong> dan lain sebagainya. Nikmati fasilitas cicilan 0% untuk tenor 3 bulan dari berbagai Bank partner kami, serta gratis pengiriman / ongkos kirim produk Toko Indomaret atau produk grocery melalui pesan Ambil atau pesan antar di seluruh Indonesia. Anda pun dapat berbelanja produk lifestyle / produk non grocery (produk yang dikirimkan dari gudang <strong>warehouse</strong> Klik Indomaret) seperti produk <strong><em>fashion, gadget,</em></strong> peralatan elektronik rumah tangga di Klik Indomaret dengan fasilitas pengiriman ke seluruh Indonesia melalui layanan jasa kirim JNE ataupun Indo Paket.
+                    </p>
+                    <p>
+                        Sesuai dengan tagline Klik Indomaret <strong>‘Easy & Safe Online Shopping’,</strong> Kami selalu berusaha memberikan kemudahan dan keamanan dalam setiap transaksi Anda, tentunya selalu memberikan kejutan dengan harga promosi terbaik.
+                    </p>
+                    <p>
+                        Kini belanja online di <a class="underline" href="/">Klik Indomaret</a> semakin aman, mudah dan hemat!
+                    </p>
+                </div>
+            </div>
+            <div class="item-footer">
+                <div class="item-footer-wrapper mb-4">
+                    <h5 class="title text-[#595959] font-bold mb-2">Nikmati Promo Belanja Online setiap saat dengan harga promo terbaik</h5>
+                    <p>
+                        <strong>Beragam promo diskon</strong> selalu dihadirkan KlikIndomaret.com setiap saat diantaranya; <strong><em>grocery deals,</em> promo bulan ini, promo <em>flash sale,</em></strong> promo <strong>hari gajian</strong> atau <strong><em>payday sale,</em></strong> promo <strong>Hanya Tiga Hari</strong> (HTH), <strong>promo tebus murah</strong> item pilihan, <strong>promo parcel, promo festive Lebaran,</strong> promo <strong>Harbolnas,</strong> promo Gratis <strong>hadiah langsung,</strong> promo <strong>cashback voucher belanja,</strong> promo potongan <strong>i-kupon belanja,</strong> promo <strong>Mayday sale, promo pembayaran Bank, promo i.saku</strong> serta beragam promo menarik lainnya.
+                    </p>
+                    <p>
+                        Anda dapat dengan mudah beli produk promosi online dengan harga terbaik dari Kami setiap harinya. Mulai dari promo kebutuhan sehari-hari atau </strong><em>grocery deals</em></strong> hingga promo cashback voucher belanja. Setiap weekend selalu ada penawaran promo spesial dengan konsep <strong>HTH ‘Hanya Tiga Hari’</strong> untuk promosi hadiah langsung ataupun tebus murah item pilihan grocery khusus di hari <strong>Jumat – Minggu. Untuk di akhir bulan, juga hadir promo gajian</strong> atau <strong>payday sale</strong> dengan pilihan promosi produk <strong>grocery sehari-hari, promo bahan pokok sembako, promo personal care kecantikan, promo kosmetik, promo gajian susu (payday milk fair), promo gajian produk baby (payday baby & kids care), promo home living, promo homecare, promo fashion terkini, promo elektronik rumah tangga,</strong> serta beragam promosi terbaik lainnya.
+                    </p>
+                    <p>
+                        Di setiap tahunnya Klik Indomaret menawarkan gebyar promo besar-besaran, yaitu <strong>Promosi Harbolnas (Hari Belanja Online Nasional).</strong> Nantikan <strong>Harbolnas di akhir tahun!</strong> Belanja Irit dengan beragam penawaran seperti </strong>'buy 1 get 1', diskon 50%,</strong> promo <strong>tebus murah, cashback voucher belanja</strong> Rp 50.000,-, <strong>Kupon potongan harga</strong> dan <strong>beragam promo menarik lainnya.</strong>
+                    </p>
+                    <p>
+                        Pastikan Anda tahu segala informasi mengenai beragam produk terbaru, katalog promosi dan dapatkan cashback voucher belanja online di KlikIndomaret.com melalui promosi yang Kami selalu hadirkan setiap minggunya. Untuk itu Anda dapat ikut serta berlangganan <strong><em>newsletter</em></strong> Kami, karena akan banyak promosi menarik lainnya.
+                    </p>
+                </div>
+                <div class="item-footer-wrapper">
+                    <h5 class="title text-[#595959] font-bold mb-2">Jangkauan Area Coverage Pengiriman</h5>
+                    <p>
+                        Toko Indomaret menjadi point terbesar Kami dalam proses bisnis serta situs belanja online <strong>‘One Stop Online Shop’ KlikIndomaret.com.</strong>
+                    </p>
+                    <p>
+                        Indomaret merupakan perusahaan retail yang sudah melayani konsumen selama lebih dari 27 tahun. Indomaret berkembang sangat pesat dengan jumlah gerai mencapai lebih dari 15.287 yang tersebar di wilayah Jawa, Madura, Bali, Lombok, Sumatra, Kalimantan dan Sulawesi dimana 40% -nya merupakan gerai waralaba atau Toko franchise. <strong><em>Area Coverage</em></strong> pengiriman Toko online KlikIndomaret.com sudah menjangkau ekspansi <strong>10 Kota Metropolitan</strong> diantaranya kota <strong>JaBoDeTaBek</strong> (Jakarta, Bogor, Depok, Tangerang, Bekasi), <strong>Bandung, Semarang, Yogyakarta, Surabaya, Malang, Jember, dan Denpasar,</strong> dan area pengiriman di <strong>Medan, Palembang, Pekanbaru, Bandar Lampung, Banjarmasin, Samarinda, Pontianak, Makassar, dan Manado.</strong>
+                    </p>
+                    <p>
+                        Barang-barang pesanan yang Anda beli dapat juga dikirimkan dari Warehouse ke seluruh area di Indonesia melalui jasa pengiriman JNE dan Indo Paket untuk pembelian produk fashion, gadget, peralatan elektronik rumah tangga, dan produk grocery import lainnya. Beragam pilihan pengiriman menjadi nilai lebih bagi Kami untuk terus memberikan penawaran terbaik bagi Anda dalam situs belanja online KlikIndomaret.com.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <hr class="mt-10">
+        <div class="footer-copyright-content text-sm text-[#CCC] text-center">
+            <div class="item-footer py-5">
+                <span>Copyright © 2022 Klik Indomaret</span>
             </div>
         </div>
     </div>
