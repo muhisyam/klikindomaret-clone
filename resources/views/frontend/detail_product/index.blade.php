b27854932b998e267074af1207e16bd4a6e7a700
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-06-30 23:56:30 +0700

    add desc detail product

--- resources/views/frontend/detail_product/index.blade.php
+++ resources/views/frontend/detail_product/index.blade.php
@@ -53,7 +53,7 @@
         </div>
         <div class="right-side w-2/3">
             <div class="product-info-wrapper">
-                <div class="flash-sale-header flex items-center justify-between rounded-lg py-2 px-5">
+                <div class="flash-sale-header flex items-center justify-between rounded-lg py-2 px-5 mb-4">
                     <div class="left-side flex items-center">
                         <img class="w-7 mr-2" src="https://www.klikindomaret.com/Assets/image/icon_flash.png" alt="Flashsale Icon">
                         <span class="text-white text-2xl font-bold italic">Flash Sale</span>
@@ -61,6 +61,44 @@
                     <div class="right-side flex text-white font-bold">
                         <span class="mr-2">Berakhir dalam:</span>
                         <div class="countdown bg-[#ED3128] rounded px-2">00:01:04</div>
+                    </div>
+                </div>
+                <div class="product-desc-wrapper bg-white rounded-lg p-5">
+                    <div class="product-title text-xl font-bold mb-2">
+                        Klik Indomaret Tas Ramah Lingkungan Parasut
+                    </div>
+                    <div class="find-store mb-4">
+                        <button class="flex items-center bg-[#FAE7D4] rounded-full py-0.5 px-2">
+                            <i class="ri-map-pin-2-fill text-[#0079C2] mr-1"></i>
+                            <span class="text-xs">Cari Toko yang Menjual</span>
+                        </button>
+                    </div>
+                    <hr>
+                    <div class="product-price text-sm mt-6 mb-8">
+                        <div class="discount-price flex items-center mb-2">
+                            <div class="discount max-w-[40px] bg-[#FAE7D4] text-[#F28418] text-center font-bold rounded px-1.5 py-1 mr-2">15%</div>
+                            <div class="price text-[#95989A] leading-none line-through">Rp 15.000</div>
+                        </div>
+                        <div class="normal-price text-[#F28418] text-2xl font-bold">
+                            <span>Rp 10.000</span>
+                        </div>
+                    </div>
+                    <div class="product-btn-wrapper">
+                        <div class="product-qty flex items-center">
+                            <span class="font-bold mr-4">Qty</span>
+                            <div class="input-qty-wrapper">
+                                <button type="button" class="text-[#C5C5C5] text-sm font-bold border border-[#C5C5C5] rounded py-2 px-3 hover:bg-[#0079C2] hover:text-white hover:border-[#0079C2]" id="btn-min-qty">
+                                    <i class="ri-add-line"></i>
+                                </button>
+                                <input type="number" class="w-10 h-10 text-center border-0 border-b border-[#C5C5C5] p-0 mx-1 focus:ring-transparent focus:border-[#C5C5C5]" value="1" id="input-qty">
+                                <button type="button" class="text-[#C5C5C5] text-sm font-bold border border-[#C5C5C5] rounded py-2 px-3 hover:bg-[#0079C2] hover:text-white hover:border-[#0079C2]" id="btn-plus-qty">
+                                    <i class="ri-subtract-line"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="btn-cart">
+                            <button type="button max-w-[270px] bg-[#]" class="add-to-cart" id=""></button>
+                        </div>
                     </div>
                 </div>
             </div>
