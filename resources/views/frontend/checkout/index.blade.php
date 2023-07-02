70c2a8143cb87823a230a04aa6395c20f9d35dd3
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-07-02 23:08:30 +0700

    add right side section in checkout page

--- resources/views/frontend/checkout/index.blade.php
+++ resources/views/frontend/checkout/index.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
 <div class="checkout mb-6">
-    <div class="checkout-wrapper flex gap-6">
-        <div class="left-side checkout-info w-2/3 min-h-screen">
+    <div class="checkout-wrapper relative flex gap-6">
+        <div class="left-side checkout-info w-3/4">
             <div id="alert" class="flex items-center p-4 mb-4 text-blue-800 rounded-lg bg-blue-50" role="alert">
                 <i class="ri-information-fill text-[#0079C2] scale-[1.5] leading-none"></i>
                 <div class="ml-3 text-sm font-medium">
@@ -14,18 +14,18 @@
                     <i class="ri-close-line scale-[1.5]"></i>
                 </button>
             </div>
-            <div class="free-ongkir-wrapper flex bg-white text-sm rounded-lg py-3 px-4 mb-4">
-                <i class="ri-takeaway-fill text-[#0079C2] mr-2"></i>
+            <div class="free-ongkir flex bg-white text-sm rounded-lg py-3 px-4 mb-4">
+                <span class="icon text-[#0079C2] mr-2"><i class="ri-takeaway-fill"></i></span>
                 Yuk, tambah belanjaan kamu supaya dapat gratis ongkos kirim!
                 <button class="text-[#0079C2] ml-1">
                     Lihat Selengkapnya
                 </button>
             </div>
-            <div class="product-cart-wrapper bg-white rounded-lg p-5">
-                <div class="product-cart-content">
+            <div class="product-cart bg-white rounded-lg p-5">
+                <div class="product-cart-wrapper">
                     <div class="item-cart-1 border border-[#EEE] rounded" data-store-code="">
-                        <div class="header flex bg-[#F9F9F9] border-b border-[#EEE] text-sm py-2 px-4">
-                            <i class="ri-store-3-line text-[#FF6363] mr-2"></i>
+                        <div class="header flex items-center bg-[#F9F9F9] border-b border-[#EEE] text-sm py-2 px-4">
+                            <span class="icon text-[#FF6363] text-base mr-2"><i class="ri-store-3-line"></i></span>
                             <div class="store-name text-[#FF6363] font-bold mr-1">Toko Indomaret</div>
                             <div class="total-product text-[#313131]">(<span>2</span> Item)</div>
                         </div>
@@ -35,7 +35,7 @@
                                     <img src="https://assets.klikindomaret.com/products/20122571/20122571_thumb.jpg?Version.20.03.1.01" alt="Product Image">
                                 </div>
                                 <div class="info w-11/12 flex text-sm">
-                                    <div class="name w-1/2 mr-2">
+                                    <div class="name w-1/2 mr-4">
                                         <a class="hover:text-[#0079C2]" href="#">Chilgo 3+ Susu Pertumbuhan Vanila 700G Chilgo 3+ Susu Pertumbuhan Vanila 700G Chilgo 3+ Susu Pertumbuhan Vanila 700G Chilgo 3+ Susu Pertumbuhan Vanila 700G Chilgo 3+ Susu Pertumbuhan Vanila 700G Chilgo 3+ Susu Pertumbuhan Vanila 700G Chilgo 3+ Susu Pertumbuhan Vanila 700G Chilgo 3+ Susu Pertumbuhan Vanila 700G</a>
                                     </div>
                                     <div class="summary w-1/2 flex items-center">
@@ -76,7 +76,7 @@
                                     <img src="https://assets.klikindomaret.com/products/20122571/20122571_thumb.jpg?Version.20.03.1.01" alt="Product Image">
                                 </div>
                                 <div class="info w-11/12 flex text-sm">
-                                    <div class="name w-1/2 mr-2">
+                                    <div class="name w-1/2 mr-4">
                                         <a class="hover:text-[#0079C2]" href="#">Chilgo 3+ Susu Pertumbuhan Vanila 700G</a>
                                     </div>
                                     <div class="summary w-1/2 flex items-center">
@@ -112,8 +112,55 @@
                     </div>
                 </div>
             </div>
+            {{-- <div class="h-[1000px]"></div> --}}
         </div>
-        <div class="right-side checkout-summary w-1/3 bg-white"></div>
+        <div class="right-side checkout-summary w-1/4">
+            <div class="summary-wrapper sticky top-20">
+                <div class="change-address bg-white rounded-lg cursor-pointer py-2 px-4 mb-4">
+                    <div class="change-address-content flex">
+                        <span class="icon text-[#0079C2] me-2"><i class="ri-map-pin-fill"></i></span>
+                        <div class="info text-xs line-clamp-1 text-ellipsis leading-6"><strong>Ambil di: </strong><span class="address">JANTI-SLEMAN (FWFU), JL JANTI 03 RT008/004 KEL CATURTUNGGAL KEC DEPOK KAB SLEMAN YOGYAKARTA</span></div>
+                        <span class="icon text-[#0079C2] ms-2"><i class="ri-arrow-down-s-line"></i></span>
+                    </div>
+                </div>
+                <div class="product-summary bg-white rounded-lg p-4 mb-4">
+                    <div class="price-summary-wrapper mb-4">
+                        <div class="total-price flex justify-between text-sm mb-4">
+                            <div class="label text-[#95989A]">Total Harga Pesanan</div>
+                            <div class="info">Rp 111.200.000</div>
+                        </div>
+                        <div class="shipping-cost flex justify-between text-sm mb-4">
+                            <div class="label text-[#95989A]">Ongkos Kirim</div>
+                            <div class="info">GRATIS</div>
+                        </div>
+                        <hr>
+                        <div class="total-dicount flex justify-between text-[#F28418] text-sm my-4">
+                            <div class="label">Total Diskon</div>
+                            <div class="info">(Rp <span>200.000</span>)</div>
+                        </div>
+                        <hr>
+                        <div class="subtotal flex justify-between text-sm font-bold mt-4">
+                            <div class="label">Total Pembayaran</div>
+                            <div class="info">Rp 111.200.000</div>
+                        </div>
+                    </div>
+                    <div class="button-summary-wrapper w-full">
+                        <button class="w-full bg-[#0079C2] text-white text-sm text-center font-bold rounded py-2 px-4">Pilih Metode Pembayaran</button>
+                        {{-- <button class="w-full bg-[#0079C2] text-white text-sm text-center font-bold rounded py-2 px-4">Bayar</button> --}}
+                        {{-- <button class="w-full flex items-center justify-center bg-[#0079C2] text-white text-sm font-bold rounded py-2 px-4">
+                            <span class="icon leading-none me-2"><i class="ri-refresh-line"></i></span>
+                            <span>Perbarui Keranjang</span>
+                        </button> --}}
+                    </div>
+                </div>
+                <div class="input-coupon-voucher bg-white rounded-lg p-4">
+                    <div class="input-voucher-wrapper flex">
+                        <input type="text" placeholder="Masukkan Voucher/Kupon" class="w-full text-sm border-0 border-b border-[#CCC] p-0 me-2 focus:ring-transparent">
+                        <button class="disabled bg-[#09C01A] text-white text-center rounded p-2">Gunakan</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
