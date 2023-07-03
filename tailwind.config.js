f80433ba6d582c7a9382513bd307af766e5e9f2d
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-07-04 00:22:41 +0700

    remove flowbite package

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,10 @@ module.exports = {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
-    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [],
 }
 
