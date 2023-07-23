cff9ac3a378f89ab1c6d6188959934e8d4378900
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-07-23 21:13:02 +0700

    feat(filter): add table head filter

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -71,76 +71,22 @@ List Produk
                 </ul>
             </nav>
         </section>
-        <section class="product-list-wrapper border border-[#eee] rounded-xl p-4">
+        <section class="order-list-wrapper border border-[#eee] rounded-xl p-4">
             <table class="w-full">
-                <thead class="bg-[#f5f5f5] text-[#888] text-sm text-left uppercase rounded-t">
-                    <tr>
-                        <th class="py-3 px-4 rounded-tl">Ref.</th>
-                        <th class="py-3 px-4">Dibuat</th>
-                        <th class="py-3 px-4">Kostumer</th>
-                        <th class="py-3 px-4">List Produk</th>
-                        <th class="py-3 px-4 text-[#414141]">Pengambilan</th>
-                        <th class="py-3 px-4">Lokasi</th>
-                        <th class="py-3 px-4">Status</th>
-                        <th class="py-3 px-4">Pengantaran</th>
-                        <th class="py-3 px-4">Harga</th>
-                        <th class="py-3 px-4 rounded-tr"></th>
-                    </tr>
+                <thead class="bg-[#f5f5f5] text-[#999] text-sm text-left uppercase rounded-t">
+                    @include('admin.order.includes.table-header')
                 </thead>
                 <tbody class="text-sm">
-                    <tr class="border-b">
-                        <td class="py-2 px-4 font-bold">#1445</td>
-                        <td class="py-2 px-4">
-                            <div class="order-created-date">
-                                <div class="date">15 Jul 2023</div>
-                                <div class="time text-xs font-light">16.00</div>
-                            </div>
-                        </td>
-                        <td class="py-2 px-4">Linguistiq Joe</td>
-                        <td class="py-2 px-4">
-                            <div class="list-products flex">
-                                <div class="info me-1"><span>1</span> Produk</div>
-                                <button class="icon h-5 hover:text-[#0079c2]"><i class="ri-eye-fill"></i></button>
-                            </div>
-                        </td>
-                        <td class="py-2 px-4">
-                            <div class="order-take-date">
-                                <div class="date">17 Jul 2023</div>
-                                <div class="time text-xs font-light">10.00</div>
-                            </div>
-                        </td>
-                        <td class="py-2 px-4">
-                            <div class="order-take-place">
-                                <div class="info">Ambil Ditoko</div>
-                                <div class="address flex text-xs font-light">
-                                    <div class="address-info me-1">Lokasi</div>
-                                    <button class="icon h-4 hover:text-[#0079c2]"><i class="ri-eye-fill"></i></button>
-                                </div>
-                            </div>
-                        </td>
-                        <td class="py-2 px-4">
-                            <div class="status bg-green-100 text-green-700 font-bold text-center rounded-lg p-1">
-                                <div class="info">Dalam Perjalanan</div>
-                            </div>
-                        </td>
-                        <td class="py-2 px-4">
-                            <div class="status flex">
-                                <div class="icon h-5 text-green-600 scale-[0.6] me-1"><i class="ri-checkbox-blank-circle-fill"></i></div>
-                                <div class="info">Selesai</div>
-                            </div>
-                        </td>
-                        <td class="py-2 px-4">
-                            <div class="price flex justify-between">
-                                <div class="left-side">Rp</div>
-                                <div class="right-side">3.600.000</div>
-                            </div>
-                        </td>
-                        <td class="py-2 px-4 text-center">
-                            <button class="hover:bg-[#fbde7e] hover:text-[#0079c2] rounded p-1 px-2">
-                                <div class="icon h-6 pt-0.5"><i class="ri-more-2-line"></i></div>
-                            </button>
-                        </td>
-                    </tr>
+                    @include('admin.order.includes.table-content')
+                    @include('admin.order.includes.table-content')
+                    @include('admin.order.includes.table-content')
+                    @include('admin.order.includes.table-content')
+                    @include('admin.order.includes.table-content')
+                    @include('admin.order.includes.table-content')
+                    @include('admin.order.includes.table-content')
+                    @include('admin.order.includes.table-content')
+                    @include('admin.order.includes.table-content')
+                    @include('admin.order.includes.table-content')
                 </tbody>
             </table>
         </section>
