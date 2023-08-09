825dfb7ebe127694c0cf0b57fe3328acb6f3815d
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-08-09 23:27:10 +0700

    feat(section): add progress info section

--- resources/views/admin/category/input-parent.blade.php
+++ resources/views/admin/category/input-parent.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.index')
 
 @section('title')
-Input Kategori
+Tambah Kategori
 @endsection
 
 @section('content')
@@ -9,7 +9,7 @@ Input Kategori
     <div class="category-input-content">
         @php
             $data = [
-                'pagename' => 'Input Kategori',
+                'pagename' => 'Tambah Kategori',
                 'breadcrumb_pages' => [
                     [
                         'info' => 'first',
@@ -23,7 +23,7 @@ Input Kategori
                     ],
                     [
                         'info' => 'last',
-                        'label' => 'Input Kategori'
+                        'label' => 'Tambah Kategori'
                     ],
                 ],
                 'navigation' => [
@@ -35,12 +35,36 @@ Input Kategori
             ]
         @endphp 
         @include('admin.components.header', ['data' => $data])
-        <section class="progress-info-wrapper">
-            <div class="w-full bg-gray-200 rounded-full h-1 dark:bg-gray-700">
-                <div class="bg-[#f9c828] h-1 rounded-full" style="width: 45%"></div>
+        <section class="progress-info-wrapper border border-[#eee] rounded-xl overflow-hidden mb-4">
+            <div class="progress-bar-wrapper flex gap-[2px]">
+                <div class="h-1.5 -skew-x-[24deg] w-full bg-[#f9c828]"></div>
+                <div class="h-1.5 -skew-x-[24deg] w-full bg-[#f9c828]"></div>
+                <div class="h-1.5 -skew-x-[24deg] w-full bg-[#f9c828]"></div>
+                <div class="h-1.5 -skew-x-[24deg] w-full bg-[#eee]"></div>
+                <div class="h-1.5 -skew-x-[24deg] w-full bg-[#eee]"></div>
+                <div class="h-1.5 -skew-x-[24deg] w-full bg-[#eee]"></div>
+                <div class="h-1.5 -skew-x-[24deg] w-full bg-[#eee]"></div>
+                <div class="h-1.5 -skew-x-[24deg] w-full bg-[#eee]"></div>
+            </div>
+            <div class="progress-info-content flex items-center justify-between p-4">
+                <h1 class="font-bold">Tambah Kategori Induk Baru</h1>
+                <div class="text-sm form-info">5 Form belum terisi</div>
+            </div>
+            <div class="error-info bg-red-200 flex py-2 px-4">
+                <div class="icon h-6 text-[#c33] text-xl me-3"><i class="ri-error-warning-fill"></i></div>
+                <div class="info">
+                    <div class="label font-bold pt-0.5">Error !</div>
+                    {{-- <ul class="list-disc text-sm ms-5">
+                        <li>Input masih kosong</li>
+                        <li>Password tidak sama</li>
+                    </ul> --}}
+                </div>
+                <button class="h-6 ms-auto">
+                    <div class="icon text-xl"><i class="ri-arrow-up-s-line"></i></div>
+                </button>
             </div>
         </section>
-        {{-- <form action="">
+        <form action="">
             <div class="category-input-wrapper flex gap-4 mb-5">
                 <section class="left-side relative h-full w-2/5 border border-[#eee] rounded-xl overflow-auto p-4">
                     @include('admin.category.includes.input.image-input')
@@ -52,7 +76,7 @@ Input Kategori
             <div class="form-button text-right">
                 <button class="h-10 w-full max-w-[20%] bg-[#0079c2] text-white rounded py-2 px-4 disabled">Submit</button>
             </div>
-        </form> --}}
+        </form>
     </div>
 </div>
 @endsection
