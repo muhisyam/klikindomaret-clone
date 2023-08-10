3047ca0e103a4da5fd6ba0ca21f4d3d12a83cc23
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-08-10 23:48:33 +0700

    feat(validation): add form validation style

--- resources/views/admin/category/includes/input/form-input.blade.php
+++ resources/views/admin/category/includes/input/form-input.blade.php
@@ -1,6 +1,6 @@
 <div class="item-input-group mb-4">
     <label for="form-select-level" class="block text-sm mb-1">Level Kategori</label>
-    <select id="form-select-level" name="" class="h-10 w-full border !border-[#CCC] rounded py-2 px-3 focus:ring-transparent">
+    <select id="form-select-level" name="" class="h-10 w-full border border-[#CCC] rounded py-2 px-3 focus:ring-transparent">
         <option value="" selected>Pilih level...</option>
         <option value="">Level 1</option>
         <option value="">Level 2</option>
@@ -9,7 +9,7 @@
 </div>
 <div class="item-input-group mb-4">
     <label for="form-select-level" class="block text-sm mb-1">Induk Kategori</label>
-    <select id="form-select-level" name="" class="h-10 w-full border !border-[#ccc] rounded py-2 px-3 focus:ring-transparent">
+    <select id="form-select-level" name="" class="h-10 w-full border border-[#ccc] rounded py-2 px-3 focus:ring-transparent">
         <option value="" selected>Pilih induk...</option>
         <option value="">Makanan</option>
         <option value="">Minuman</option>
@@ -17,18 +17,20 @@
 </div>
 <div class="item-input-group mb-4">
     <label for="form-input-first-name" class="block text-sm mb-1">Nama Kategori</label>
-    <input id="form-input-first-name" type="text" name="first-name"" class="h-10 w-full border !border-[#ccc] rounded py-2 px-3 focus:ring-transparent">
+    <input id="form-input-first-name" type="text" name="first-name" class="h-10 w-full border border-[#ccc] rounded py-2 px-3 focus:ring-transparent">
 </div>
 <div class="item-input-group mb-4">
-    <label for="form-input-first-name" class="block text-sm mb-1">Slug</label>
-    <input id="form-input-first-name" type="text" name="first-name"" class="h-10 w-full border !border-[#ccc] rounded py-2 px-3 focus:ring-transparent">
+    <label for="form-input-slug" class="block text-sm mb-1">Slug</label>
+    <input id="form-input-slug" type="text" name="slug" class="h-10 w-full border border-[#ccc] rounded py-2 px-3 focus:ring-transparent">
 </div>
-<div id="alert" class="desc-alert-info absolute top-2 left-1/2 -translate-x-1/2 flex p-4 mb-6 text-red-800 rounded-lg bg-red-50" role="alert">
-    <span class="icon text-[#c33] text-2xl me-2"><i class="ri-information-fill"></i></span>
-    <div class="text-sm font-medium">
-        Kategori Sudah Ada di Database.
+<div class="item-input-group mb-4">
+    <label for="form-select-level" class="block text-sm mb-1">Status</label>
+    <select id="form-select-level" name="" class="h-10 w-full border border-[#ccc] rounded py-2 px-3 focus:ring-transparent is-invalid">
+        <option value="" selected>Aktif</option>
+        <option value="">Tidak Aktif</option>
+    </select>
+    <div class="invalid-feedback flex text-red-600 text-sm mt-1">
+        <div class="icon h-5 me-1"><i class="ri-error-warning-fill"></i></div>
+        <div class="message">Input salah!</div>
     </div>
-    <button type="button" class="h-8 w-8 grid place-content-center ml-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200" data-dismiss-target="#alert" aria-label="Close">
-        <span class="icon h-8 text-[#c33] text-2xl"><i class="ri-close-line"></i></span>
-    </button>
 </div>
