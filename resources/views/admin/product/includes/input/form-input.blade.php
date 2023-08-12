7e48e145e4fcfc6d748409f76a5ec9f7bf8fef27
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-08-12 23:41:01 +0700

    feat(input): add more addon desc input

--- resources/views/admin/product/includes/input/form-input.blade.php
+++ resources/views/admin/product/includes/input/form-input.blade.php
@@ -37,20 +37,48 @@
         <textarea id="desc-info" name="desc-info" cols="30" rows="5" placeholder="Deskripsi..." class="w-full border !border-[#ccc] rounded py-2 px-3 focus:ring-transparent"></textarea>
     </div>
 </div>
-<div class="item-input-group mb-4">
-    <label for="form-input-desc" class="block text-sm mb-1">Deskripsi Tambahan</label>
+<div id="addon-1" class="item-input-group mb-4">
+    <label for="form-input-desc" class="block text-sm mb-1">Deskripsi Tambahan #1</label>
     <div id="form-input-desc" class="w-full">
         <input id="desc-label" type="text" name="addon-desc-label" placeholder="Label Deskripsi..." class="w-full h-10 border !border-[#ccc] rounded py-2 px-3 mb-4 focus:ring-transparent">
-        <textarea id="desc-info" name="addon-desc-info" cols="30" rows="5" placeholder="Deskripsi..." class="w-full border !border-[#ccc] rounded py-2 px-3 focus:ring-transparent"></textarea>
+        <textarea id="desc-info" name="addon-desc-info-1" cols="30" rows="5" placeholder="Deskripsi..." class="w-full border !border-[#ccc] rounded py-2 px-3 focus:ring-transparent"></textarea>
     </div>
 </div>
-<div class="item-input-group ">
-    <div id="form-input-desc" class="flex gap-4">
+<div id="addon-2" class="item-input-group mb-4 hidden">
+    <label for="form-input-desc" class="block text-sm mb-1">Deskripsi Tambahan #2</label>
+    <div id="form-input-desc" class="w-full">
+        <input id="desc-label" type="text" name="addon-desc-label" placeholder="Label Deskripsi..." class="w-full h-10 border !border-[#ccc] rounded py-2 px-3 mb-4 focus:ring-transparent">
+        <textarea id="desc-info" name="addon-desc-info-2" cols="30" rows="5" placeholder="Deskripsi..." class="w-full border !border-[#ccc] rounded py-2 px-3 focus:ring-transparent"></textarea>
+    </div>
+</div>
+<div id="addon-3" class="item-input-group mb-4 hidden">
+    <label for="form-input-desc" class="block text-sm mb-1">Deskripsi Tambahan #3</label>
+    <div id="form-input-desc" class="w-full">
+        <input id="desc-label" type="text" name="addon-desc-label" placeholder="Label Deskripsi..." class="w-full h-10 border !border-[#ccc] rounded py-2 px-3 mb-4 focus:ring-transparent">
+        <textarea id="desc-info" name="addon-desc-info-3" cols="30" rows="5" placeholder="Deskripsi..." class="w-full border !border-[#ccc] rounded py-2 px-3 focus:ring-transparent"></textarea>
+    </div>
+</div>
+<div id="addon-4" class="item-input-group mb-4 hidden">
+    <label for="form-input-desc" class="block text-sm mb-1">Deskripsi Tambahan #4</label>
+    <div id="form-input-desc" class="w-full">
+        <input id="desc-label" type="text" name="addon-desc-label" placeholder="Label Deskripsi..." class="w-full h-10 border !border-[#ccc] rounded py-2 px-3 mb-4 focus:ring-transparent">
+        <textarea id="desc-info" name="addon-desc-info-4" cols="30" rows="5" placeholder="Deskripsi..." class="w-full border !border-[#ccc] rounded py-2 px-3 focus:ring-transparent"></textarea>
+    </div>
+</div>
+<div id="addon-5" class="item-input-group mb-4 hidden">
+    <label for="form-input-desc" class="block text-sm mb-1">Deskripsi Tambahan #5</label>
+    <div id="form-input-desc" class="w-full">
+        <input id="desc-label" type="text" name="addon-desc-label" placeholder="Label Deskripsi..." class="w-full h-10 border !border-[#ccc] rounded py-2 px-3 mb-4 focus:ring-transparent">
+        <textarea id="desc-info" name="addon-desc-info-5" cols="30" rows="5" placeholder="Deskripsi..." class="w-full border !border-[#ccc] rounded py-2 px-3 focus:ring-transparent"></textarea>
+    </div>
+</div>
+<div class="item-input-group">
+    <div id="btn-addon-desc" class="flex gap-4">
         <button type="button" id="btnAddDesc" class="h-10 flex items-center bg-[#0079c2] text-white rounded py-2 px-4">
             <span class="icon h-6 me-1"><i class="ri-add-fill"></i></span>
             <span class="text">Tambah Deskripsi</span>
         </button>
-        <button type="button" id="btnDelDesc" class="h-10 flex items-center bg-[#c33] text-white rounded py-2 px-4">
+        <button type="button" id="btnDelDesc" class="h-10 flex items-center bg-[#c33] text-white rounded py-2 px-4 hidden">
             <span class="icon h-6 me-1"><i class="ri-delete-bin-6-line"></i></span>
             <span class="text">Hapus Deskripsi</span>
         </button>
