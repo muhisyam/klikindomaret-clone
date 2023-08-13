775930e72448c282671f5cf0ecd55cda862566fd
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-08-13 15:14:59 +0700

    feat(input): add input error message

--- resources/views/admin/product/includes/input/image-input.blade.php
+++ resources/views/admin/product/includes/input/image-input.blade.php
@@ -1,5 +1,5 @@
 <div class="label text-sm mb-1">Tambah Gambar</div>
-<div class="input-image-wrapper w-full h-56 flex flex-col justify-center bg-[#fbf0d0] border-4 border-[#f9c828] border-dashed rounded-lg mb-6">
+<div class="input-image-wrapper w-full h-48 flex flex-col justify-center bg-[#fbf0d0] border-4 border-[#f9c828] border-dashed rounded-lg mb-6">
     <div class="icon h-24 text-[#aca595] text-8xl text-center mb-5"><i class="ri-image-add-fill"></i></div>
     <div class="info flex items-center justify-center">
         <div class="icon h-7 text-[#0079c2] text-xl me-2"><i class="ri-upload-2-line"></i></div>
@@ -7,70 +7,85 @@
             Drop your images here, or <a href="" class="text-[#0079c2] font-bold">Browse</a>
         </div>
     </div>
+    <div class="validation text-[#aca595] text-xs text-center">
+        (max image size 500kb)
+    </div>
 </div>
 <div class="list-image-uploaded flex flex-col gap-2 overflow-auto">
-    <div class="item-image-uploaded flex items-center justify-between border border-[#eee] rounded p-2">
-        <div class="image-info-wrapper w-11/12 flex items-center">
-            <div class="media shrink-0 me-2">
-                <img class="h-12" src="https://assets.klikindomaret.com/products/banner/OBAT%20HERBAL%20TRADISIO.jpeg" alt="">
+    <div class="item-image-uploaded">
+        <div class="image-item-wrapper flex items-center justify-between border border-[#eee] rounded p-2 is-invalid">
+            <div class="image-info-wrapper w-11/12 flex items-center">
+                <div class="media shrink-0 me-2">
+                    <img class="h-12" src="https://assets.klikindomaret.com/products/banner/OBAT%20HERBAL%20TRADISIO.jpeg" alt="">
+                </div>
+                <div class="info">
+                    <div class="text font-bold line-clamp-1 text-ellipsis">OBAT HERBAL TRADISIO OBAT HERBAL TRADISIO OBAT HERBAL TRADISIO.jpeg</div>
+                    <div class="size text-xs font-light">142 KB</div>
+                </div>
             </div>
-            <div class="info">
-                <div class="text font-bold line-clamp-1 text-ellipsis">OBAT HERBAL TRADISIO OBAT HERBAL TRADISIO OBAT HERBAL TRADISIO.jpeg</div>
-                <div class="size text-xs font-light">142 KB</div>
-            </div>
-        </div>
-        <div class="action">
-            <button class="icon h-8 text-2xl rounded px-1 hover:bg-[#fbde7e] hover:text-[#0079c2]">
-                <i class="ri-delete-bin-6-line"></i>
-            </button>
-        </div>
-    </div>
-    <div class="item-image-uploaded flex items-center justify-between border border-[#eee] rounded p-2">
-        <div class="image-info-wrapper w-11/12 flex items-center">
-            <div class="media shrink-0 me-2">
-                <img class="h-12" src="https://assets.klikindomaret.com/products/20035630/20035630_thumb.jpg?Version.20.01.1.01" alt="">
-            </div>
-            <div class="info">
-                <div class="text font-bold line-clamp-1 text-ellipsis">OBAT HERBAL TRADISIO OBAT HERBAL TRADISIO OBAT HERBAL TRADISIO.jpeg</div>
-                <div class="size text-xs font-light">142 KB</div>
+            <div class="action">
+                <button class="icon h-8 text-2xl rounded px-1 hover:bg-[#fbde7e] hover:text-[#0079c2]">
+                    <i class="ri-delete-bin-6-line"></i>
+                </button>
             </div>
         </div>
-        <div class="action">
-            <button class="icon h-8 text-2xl rounded px-1 hover:bg-[#fbde7e] hover:text-[#0079c2]">
-                <i class="ri-delete-bin-6-line"></i>
-            </button>
+        <div class="invalid-feedback flex text-red-600 text-sm mt-1">
+            <div class="icon h-5 me-1"><i class="ri-error-warning-fill"></i></div>
+            <div class="message">Ukuran gambar >500kb</div>
         </div>
     </div>
-    <div class="item-image-uploaded flex items-center justify-between border border-[#eee] rounded p-2">
-        <div class="image-info-wrapper w-11/12 flex items-center">
-            <div class="media shrink-0 me-2">
-                <img class="h-12" src="https://assets.klikindomaret.com/products/banner/OBAT%20HERBAL%20TRADISIO.jpeg" alt="">
+    <div class="item-image-uploaded">
+        <div class="image-item-wrapper flex items-center justify-between border border-[#eee] rounded p-2">
+            <div class="image-info-wrapper w-11/12 flex items-center">
+                <div class="media shrink-0 me-2">
+                    <img class="h-12" src="https://assets.klikindomaret.com/products/20035630/20035630_thumb.jpg?Version.20.01.1.01" alt="">
+                </div>
+                <div class="info">
+                    <div class="text font-bold line-clamp-1 text-ellipsis">OBAT HERBAL TRADISIO OBAT HERBAL TRADISIO OBAT HERBAL TRADISIO.jpeg</div>
+                    <div class="size text-xs font-light">142 KB</div>
+                </div>
             </div>
-            <div class="info">
-                <div class="text font-bold line-clamp-1 text-ellipsis">OBAT HERBAL TRADISIO OBAT HERBAL TRADISIO OBAT HERBAL TRADISIO.jpeg</div>
-                <div class="size text-xs font-light">142 KB</div>
+            <div class="action">
+                <button class="icon h-8 text-2xl rounded px-1 hover:bg-[#fbde7e] hover:text-[#0079c2]">
+                    <i class="ri-delete-bin-6-line"></i>
+                </button>
             </div>
-        </div>
-        <div class="action">
-            <button class="icon h-8 text-2xl rounded px-1 hover:bg-[#fbde7e] hover:text-[#0079c2]">
-                <i class="ri-delete-bin-6-line"></i>
-            </button>
         </div>
     </div>
-    <div class="item-image-uploaded flex items-center justify-between border border-[#eee] rounded p-2">
-        <div class="image-info-wrapper w-11/12 flex items-center">
-            <div class="media shrink-0 me-2">
-                <img class="h-12" src="https://assets.klikindomaret.com/products/20035630/20035630_thumb.jpg?Version.20.01.1.01" alt="">
+    <div class="item-image-uploaded">
+        <div class="image-item-wrapper flex items-center justify-between border border-[#eee] rounded p-2">
+            <div class="image-info-wrapper w-11/12 flex items-center">
+                <div class="media shrink-0 me-2">
+                    <img class="h-12" src="https://assets.klikindomaret.com/products/banner/OBAT%20HERBAL%20TRADISIO.jpeg" alt="">
+                </div>
+                <div class="info">
+                    <div class="text font-bold line-clamp-1 text-ellipsis">OBAT HERBAL TRADISIO OBAT HERBAL TRADISIO OBAT HERBAL TRADISIO.jpeg</div>
+                    <div class="size text-xs font-light">142 KB</div>
+                </div>
             </div>
-            <div class="info">
-                <div class="text font-bold line-clamp-1 text-ellipsis">OBAT HERBAL TRADISIO OBAT HERBAL TRADISIO OBAT HERBAL TRADISIO.jpeg</div>
-                <div class="size text-xs font-light">142 KB</div>
+            <div class="action">
+                <button class="icon h-8 text-2xl rounded px-1 hover:bg-[#fbde7e] hover:text-[#0079c2]">
+                    <i class="ri-delete-bin-6-line"></i>
+                </button>
             </div>
         </div>
-        <div class="action">
-            <button class="icon h-8 text-2xl rounded px-1 hover:bg-[#fbde7e] hover:text-[#0079c2]">
-                <i class="ri-delete-bin-6-line"></i>
-            </button>
+    </div>
+    <div class="item-image-uploaded">
+        <div class="image-item-wrapper flex items-center justify-between border border-[#eee] rounded p-2">
+            <div class="image-info-wrapper w-11/12 flex items-center">
+                <div class="media shrink-0 me-2">
+                    <img class="h-12" src="https://assets.klikindomaret.com/products/20035630/20035630_thumb.jpg?Version.20.01.1.01" alt="">
+                </div>
+                <div class="info">
+                    <div class="text font-bold line-clamp-1 text-ellipsis">OBAT HERBAL TRADISIO OBAT HERBAL TRADISIO OBAT HERBAL TRADISIO.jpeg</div>
+                    <div class="size text-xs font-light">142 KB</div>
+                </div>
+            </div>
+            <div class="action">
+                <button class="icon h-8 text-2xl rounded px-1 hover:bg-[#fbde7e] hover:text-[#0079c2]">
+                    <i class="ri-delete-bin-6-line"></i>
+                </button>
+            </div>
         </div>
     </div>
     <div class="item-image-upload flex items-center justify-between border border-[#eee] rounded p-2">
@@ -93,13 +108,4 @@
             </button>
         </div>
     </div>
-</div>
-<div id="alert" class="image-alert-info absolute top-2 left-1/2 -translate-x-1/2 flex p-4 mb-6 text-red-800 rounded-lg bg-red-50" role="alert">
-    <span class="icon text-[#c33] text-2xl me-2"><i class="ri-information-fill"></i></span>
-    <div class="text-sm font-medium">
-        Ukuran Gambar > 500 KB.
-    </div>
-    <button type="button" class="h-8 w-8 grid place-content-center ml-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200" data-dismiss-target="#alert" aria-label="Close">
-        <span class="icon h-8 text-[#c33] text-2xl"><i class="ri-close-line"></i></span>
-    </button>
 </div>
