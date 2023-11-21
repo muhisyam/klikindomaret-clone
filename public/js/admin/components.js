8d5b3690413c4f496a276b823a1d504092fef992
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-11-21 16:24:54 +0700

    feat: add new custom colors

--- public/js/admin/components.js
+++ public/js/admin/components.js
@@ -18,16 +18,16 @@ let totalTime = 5;
 function showNotification(title, message) {
     const isLongText = message.length > 25 ? ' animation-running' : '';
     const notificationParentClass = ['action-notification', 'flex', 'items-center', 'animate__animated', 'animate__bounceInRight'];
-    const timerClass = ['timer-notification', 'absolute', 'left-0', 'bottom-0', 'w-full', 'h-1', 'bg-[#0079c2]'];
+    const timerClass = ['timer-notification', 'absolute', 'left-0', 'bottom-0', 'w-full', 'h-1', 'bg-secondary'];
 
     let notificationInner = `<div class="icon h-6 -ms-2 me-2"><i class="ri-delete-bin-6-fill"></i></div>
-                                <div class="info flex items-center text-sm">
-                                    <h5 class="title capitalize me-1">${title}</h5>
-                                    <div class="desc max-w-[175px] font-bold whitespace-nowrap overflow-hidden">
-                                        <p class="text relative${isLongText}" title="${message}">"${message}"</p>
-                                    </div>
+                            <div class="info flex items-center text-sm">
+                                <h5 class="title capitalize me-1">${title}</h5>
+                                <div class="desc max-w-[175px] font-bold whitespace-nowrap overflow-hidden">
+                                    <p class="text relative${isLongText}" title="${message}">${message}</p>
                                 </div>
-                                <button type="button" onclick="closeNotification()" class="close h-6 rounded-md px-1 ms-2 -me-4 hover:bg-[#0079c2] hover:text-[#fbde7e]"><i class="ri-close-line"></i></button>`
+                            </div>
+                            <button type="button" onclick="closeNotification()" class="close h-6 rounded-md px-1 ms-2 -me-4 hover:bg-secondary hover:text-tertiary "><i class="ri-close-line"></i></button>`
                                 
     if (!greetingText) {
         greetingText = document.querySelector('.greeting .greet-text');
@@ -83,16 +83,16 @@ function updateCountdown() {
 function btnDataAction(triggerEl) {
     const actionId = triggerEl.getAttribute('data-target-action');
     const actionEl = document.querySelector(`#${actionId}-action`);
-    const isOpened = triggerEl.classList.contains('bg-[#fbde7e]', 'text-[#0079c2]');
+    const isOpened = triggerEl.classList.contains('bg-tertiary', 'text-secondary');
     const triggerAction = triggerEl.querySelector('.icon-action');
     const triggerClose = triggerEl.querySelector('.icon-close');
 
     actionEl.classList.toggle('hidden');
     
     if (isOpened) {
-        triggerEl.classList.remove('bg-[#fbde7e]', 'text-[#0079c2]')
+        triggerEl.classList.remove('bg-tertiary', 'text-secondary')
     } else {
-        triggerEl.classList.add('bg-[#fbde7e]', 'text-[#0079c2]')
+        triggerEl.classList.add('bg-tertiary', 'text-secondary')
     };
 
     triggerAction.classList.toggle('hidden');
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,21 @@
 module.exports = {
   content: [
     "./resources/**/*.blade.php",
+    "./public/**/*.js",
     "./resources/**/*.js",
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary': '#f9c828',
+        'secondary': '#0079c2',
+        'tertiary': '#fbde7e',
+        'accent': '#fbf0d0',
+        'grey': '#ccc',
+        'light-grey': '#eee',
+      },
+    },
   },
   plugins: [],
 }
