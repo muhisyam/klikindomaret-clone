46166e401d47d3861a989404b7b6503ae232a40b
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-11-24 23:44:47 +0700

    fix: fixing error and param

--- app/Services/Backend/ImageService.php
+++ app/Services/Backend/ImageService.php
@@ -2,18 +2,17 @@
 
 namespace App\Services\Backend;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 
 class ImageService 
 {
     /**
-     * Check if image exists in the public directory
+     * Check if image exists in the public directory, then delete it
      */
-    public function findImage(object $data, string $section) 
+    public function deleteExistsImage(string $dataImageName, string $folderSaveName) 
     {
-        if(!is_null($data->category_image_name)) {
-            $path = 'img/uploads/' . $section . '/' . $data->category_image_name;
+        if(!is_null($dataImageName)) {
+            $path = 'img/uploads/' . $folderSaveName . '/' . $dataImageName;
             
             if (File::exists($path)) {
                 File::delete($path);
@@ -23,36 +22,26 @@ class ImageService
 
     /**
      * Store image to db and public directory
-     * 
-     * @param Request    $request          
-     * @param string     $dataName          name data section (category, product, etc)
-     * @param string     $folderDataName    folder name (categories, products, etc)
      */
-    public function storeImage(Request $request, string $dataName, string $folderDataName)
+    public function storeImage(Object $dataImage, string $folderSaveName)
     {
-        $randFileName = null;
+        $randomImageName = null;
+        
+        $randomImageName = time() . '.' . strtolower($dataImage->getClientOriginalExtension());
+        $dataImage->move('img/uploads/' . $folderSaveName . '/', $randomImageName);
 
-        if ($request->hasFile($dataName . '_image')) {
-            $file = $request->file($dataName . '_image');
-            $randFileName = time() . '.' . strtolower($file->getClientOriginalExtension());
-            // $file->move('img/uploads/' . $folderDataName . '/', $randFileName);
-        }
-
-        return $randFileName; 
+        return $randomImageName; 
     }
 
     /**
      * Store image name to db
      */
-    public function storeImageName(Request $request)
+    public function storeImageName(Object $dataImage)
     {
         $fileName = null;
 
-        if ($request->hasFile('category_image')) {
-            $file = $request->file('category_image');
-            $originalName = explode('.', $file->getClientOriginalName());
-            $fileName = $originalName[0]  . '.' . strtolower($file->getClientOriginalExtension());
-        }
+        $originalName = explode('.', $dataImage->getClientOriginalName());
+        $fileName = $originalName[0]  . '.' . strtolower($dataImage->getClientOriginalExtension());
         
         return $fileName; 
     }
