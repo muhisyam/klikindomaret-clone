90236068740bdb53c22ee8c106a9cc1039cadb5d
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-11-25 17:44:01 +0700

    feat: add store api resource

---
+++ app/Http/Resources/StoreResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StoreResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'flag' => $this->flag,
+            'store_code' => $this->store_code,
+            'store_name' => $this->store_name,
+        ];
+    }
+}
