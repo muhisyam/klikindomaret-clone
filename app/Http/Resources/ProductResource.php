2422e54f3fb7609989949825b72184589296fda2
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-11-26 23:16:32 +0700

    chore: consisting field order

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -18,13 +18,13 @@ class ProductResource extends JsonResource
             'id' => $this->id,
             'category_id' => $this->category_id,
             'store_id' => $this->store_id,
+            'plu' => $this->plu,
             'product_name' => $this->product_name,
             'product_slug' => $this->product_slug,
             'normal_price' => $this->normal_price,
             'discount_price' => $this->discount_price,
-            'product_status' => $this->product_status,
             'product_stock' => $this->product_stock,
-            'plu' => $this->plu,
+            'product_status' => $this->product_status,
         ];
     }
 }
