673c5d9eb43f1cd7aeb085465e2fbdf6b50191c4
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-11-28 16:08:21 +0700

    feat: add product image resource

---
+++ app/Http/Resources/ProductImageResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductImageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'product_id' => $this->product_id,
+            'product_image_name' => $this->product_image_name,
+            'original_product_image_name' => $this->original_product_image_name,
+        ];
+    }
+}
