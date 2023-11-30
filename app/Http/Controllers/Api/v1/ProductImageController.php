be5edd27683087609b54e808b5ca15f00d405d38
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-12-01 00:26:07 +0700

    fix: data product source in product image controller

--- app/Http/Controllers/Api/v1/ProductImageController.php
+++ app/Http/Controllers/Api/v1/ProductImageController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Models\ProductImage;
-use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use App\Services\Backend\ImageService;
-use App\Http\Requests\ProductImageRequest;
 use App\Http\Resources\ProductImageResource;
 
 class ProductImageController extends Controller
@@ -15,31 +13,28 @@ class ProductImageController extends Controller
         protected ImageService $imageService,
     ) {}
 
-    public function store(ProductImageRequest $request): JsonResponse
+    public function store(Array $dataProduct, int $dataProductId): Array
     {
-        $dataImages = $request->validated();
-        $folderName = 'products/' . $dataImages['product_slug'];
+        $folderName = 'products/' . $dataProduct['product_slug'];
         
-        foreach ($dataImages['product_images'] as $index => $dataImage) {
+        foreach ($dataProduct['product_images'] as $index => $dataImage) {
             $productImage = new ProductImage();
 
-            $productImage->product_id = $dataImages['product_id'];
+            $productImage->product_id = $dataProductId;
             $productImage->product_image_name = $this->imageService->storeMultipleImage($dataImage, $index, $folderName);
             $productImage->original_product_image_name = $this->imageService->storeImageName($dataImage);
-        
             $productImage->save();
+        
             $productImagesData[] = new ProductImageResource($productImage);
-        };
+        }
 
-        return response()->json(['data' => $productImagesData], 201);
+        return $productImagesData;
     }
 
-    public function update(ProductImageRequest $request)
+    public function update(Array $dataProduct, int $dataProductId): Array
     {
-        ProductImage::where('product_id', $request['product_id'])->delete();
-        
-        $saveNewImages = $this->store($request);
+        ProductImage::where('product_id', $dataProductId)->delete();
 
-        return $saveNewImages;
+        return $this->store($dataProduct, $dataProductId);
     }
 }
