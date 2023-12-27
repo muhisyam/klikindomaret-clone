dd33ea6c15e6dfba6e1e99f28006ec08f831d357
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-12-27 21:35:00 +0700

    refactor: change sort and place thead inside

--- resources/views/admin/product/includes/index/table-header.blade.php
+++ resources/views/admin/product/includes/index/table-header.blade.php
@@ -1,51 +1,38 @@
-<tr>
-    <th class="py-3 px-4 rounded-tl">No.</th>
-    <th class="py-3 px-4">
-        <div class="header-col-wrapper flex active-desc">
-            <div class="label me-1">Produk</div>
-            <div class="flex flex-col text-[12px]">
-                <button class="asc h-2 leading-none" aria-label="Sort data ascending"><i class="ri-arrow-up-s-fill block h-2"></i></button>
-                <button class="desc h-2 leading-none" aria-label="Sort data descending"><i class="ri-arrow-down-s-fill block h-2 -mt-1"></i></button>
+<thead class="bg-[#f5f5f5] text-[#999] text-sm text-left uppercase rounded-t">
+    <tr>
+        <th class="py-3 px-4 rounded-tl">No.</th>
+        <th class="py-3 px-4">
+            <div class="header-col-wrapper relative flex cursor-pointer active-asc">
+                <div class="label me-1">Produk</div>
+                <div class="sortable" data-sort></div>
             </div>
-        </div>
-    </th>
-    <th class="py-3 px-4">
-        <div class="header-col-wrapper flex">
-            <div class="label me-1">Kategori</div>
-            <div class="flex flex-col text-[12px]">
-                <button class="asc h-2 leading-none" aria-label="Sort data ascending"><i class="ri-arrow-up-s-fill block h-2"></i></button>
-                <button class="desc h-2 leading-none" aria-label="Sort data descending"><i class="ri-arrow-down-s-fill block h-2 -mt-1"></i></button>
+        </th>
+        <th class="py-3 px-4">
+            <div class="header-col-wrapper relative flex cursor-pointer">
+                <div class="label me-1">Kategori</div>
+                <div class="sortable" data-sort></div>
             </div>
-        </div>
-    </th>
-    <th class="py-3 px-4">Toko</th>
-    <th class="py-3 px-4">
-        <div class="header-col-wrapper flex">
-            <div class="label me-1">Status</div>
-            <div class="flex flex-col text-[12px]">
-                <button class="asc h-2 leading-none" aria-label="Sort data ascending"><i class="ri-arrow-up-s-fill block h-2"></i></button>
-                <button class="desc h-2 leading-none" aria-label="Sort data descending"><i class="ri-arrow-down-s-fill block h-2 -mt-1"></i></button>
+        </th>
+        <th class="py-3 px-4">Toko</th>
+        <th class="py-3 px-4">
+            <div class="header-col-wrapper relative flex cursor-pointer">
+                <div class="label me-1">Status</div>
+                <div class="sortable" data-sort></div>
             </div>
-        </div>
-    </th>
-    <th class="py-3 px-4">
-        <div class="header-col-wrapper flex">
-            <div class="label me-1">Stok</div>
-            <div class="flex flex-col text-[12px]">
-                <button class="asc h-2 leading-none" aria-label="Sort data ascending"><i class="ri-arrow-up-s-fill block h-2"></i></button>
-                <button class="desc h-2 leading-none" aria-label="Sort data descending"><i class="ri-arrow-down-s-fill block h-2 -mt-1"></i></button>
+        </th>
+        <th class="py-3 px-4">
+            <div class="header-col-wrapper relative flex cursor-pointer">
+                <div class="label me-1">Stok</div>
+                <div class="sortable" data-sort></div>
             </div>
-        </div>
-    </th>
-    <th class="py-3 px-4">Diskon</th>
-    <th class="py-3 px-4">
-        <div class="header-col-wrapper flex">
-            <div class="label me-1">Harga</div>
-            <div class="flex flex-col text-[12px]">
-                <button class="asc h-2 leading-none" aria-label="Sort data ascending"><i class="ri-arrow-up-s-fill block h-2"></i></button>
-                <button class="desc h-2 leading-none" aria-label="Sort data descending"><i class="ri-arrow-down-s-fill block h-2 -mt-1"></i></button>
+        </th>
+        <th class="py-3 px-4">Diskon</th>
+        <th class="py-3 px-4">
+            <div class="header-col-wrapper relative flex cursor-pointer">
+                <div class="label me-1">Harga</div>
+                <div class="sortable" data-sort></div>
             </div>
-        </div>
-    </th>
-    <th class="py-3 px-4 rounded-tr"></th>
-</tr>
+        </th>
+        <th class="py-3 px-4 rounded-tr"></th>
+    </tr>
+</thead>
