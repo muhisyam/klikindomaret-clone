1f1a1aae2ab09dbff06a289870c8cb034f08141a
mhisyam25 <muhammadhisyam25031@gmail.com>
2023-12-28 22:59:50 +0700

    feat: add product order by condition

--- app/Http/Controllers/Api/v1/ProductController.php
+++ app/Http/Controllers/Api/v1/ProductController.php
@@ -2,29 +2,55 @@
 
 namespace App\Http\Controllers\Api\v1;
 
-use App\Models\Product;
-use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\File;
 use App\Http\Requests\ProductRequest;
 use App\Http\Resources\ProductResource;
+use App\Models\Product;
 use App\Services\Backend\ImageService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\File;
 
 class ProductController extends Controller
 {
-    public function index(): JsonResource
+    public function index(Request $request): JsonResource
     {
-        $products = Product::orderBy('category_id', 'asc')
-            ->withCount([
-                'images',
-            ])
-            ->with([
-                'category', 
-                'store',
-                'images',
-            ])
-            ->paginate(10);
+        $columnSortBy = $request->has('sortby') ? $request['sortby'] : false;
+        $columnOrderBy = $request->has('orderby') ? $request['orderby'] : false;
+
+        $query = Product::with(['category', 'store', 'images']);
+
+        // TODO: Move to service class
+        if (!$columnSortBy && !$columnOrderBy) {
+            $products = $query->paginate(10);
+
+            return ProductResource::collection($products);
+        }
+
+        // Sortby category handler
+        if ($columnSortBy === 'category_name') {
+            $query = $query
+                ->join('categories', 'products.category_id', '=', 'categories.id')
+                ->orderBy('categories.category_name', $columnOrderBy);
+                
+        // Sortby price handler
+        } elseif ($columnSortBy === 'product_price') {
+            $query = $query
+                ->orderByRaw(
+                    'CASE
+                        WHEN discount_price > 0 THEN discount_price
+                        ELSE normal_price
+                    END ' . $columnOrderBy
+                );
+
+        // Sortby product name, status, stock
+        } else {
+            $query = $query
+                ->orderBy($columnSortBy, $columnOrderBy);
+        }
+        
+        $products = $query->paginate(10);
 
         return ProductResource::collection($products);
     }
