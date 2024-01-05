4ef75df4b4ad249a022d0c3636c669b3c3d206fe
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-01-05 21:17:29 +0700

    feat: add new api endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Backend\UserController;
+use App\Http\Controllers\Api\v1\StoreController;
+use App\Http\Controllers\Api\v1\RegionController;
 use App\Http\Controllers\Api\v1\ProductController;
 use App\Http\Controllers\Api\v1\CategoryController;
+use App\Http\Controllers\Api\v1\SupplierController;
 use App\Http\Controllers\Api\v1\ProductImageController;
-use App\Http\Controllers\Api\v1\StoreController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,9 +24,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('categories', CategoryController::class);
-Route::get('categories/sub/{category}', [CategoryController::class, 'subIndex'])->name('category.subIndex');
-Route::get('categories/query/{category}', [CategoryController::class, 'selectQuery'])->name('category.selectQuery');
+// group categories
+Route::group(['prefix' => 'categories'], function() {
+    Route::apiResource('/', CategoryController::class);
+    Route::get('/sub/{category}', [CategoryController::class, 'subIndex'])->name('category.subIndex');
+    Route::get('/top-children/{category}', [CategoryController::class, 'getTopLevelWithChildren'])->name('category.topChildren');
+});
+Route::apiResource('regions', RegionController::class);
+Route::apiResource('suppliers', SupplierController::class);
 Route::apiResource('stores', StoreController::class);
 Route::apiResource('products', ProductController::class);
 Route::apiResource('product-images', ProductImageController::class);
