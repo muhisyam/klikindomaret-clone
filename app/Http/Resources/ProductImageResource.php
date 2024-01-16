7d63c2f3e05da8dbf96796437496ce898afd7647
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-01-16 20:29:58 +0700

    refactor: move get image size to model

--- app/Http/Resources/ProductImageResource.php
+++ app/Http/Resources/ProductImageResource.php
@@ -3,7 +3,6 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductImageResource extends JsonResource
@@ -15,15 +14,12 @@ class ProductImageResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $imagePath = 'img/uploads/products/coba-save-image/' . $this->product_image_name;
-        $imageSize = File::exists($imagePath) ? round(filesize($imagePath) / 1024) : null;
-
         return [
             'id' => $this->id,
             'product_id' => $this->product_id,
             'product_image_name' => $this->product_image_name,
             'original_product_image_name' => $this->original_product_image_name,
-            'product_image_size' => $imageSize,
+            'product_image_size' => $this->getImageSize(),
         ];
     }
 }
--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\File;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ProductImage extends Model
 {
@@ -16,4 +17,16 @@ class ProductImage extends Model
         'product_image_name',
         'original_product_image_name',
     ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function getImageSize()
+    {
+        $imagePath = 'img/uploads/products/'. $this->product->product_slug . '/' . $this->product_image_name;
+        
+        return File::exists($imagePath) ? round(filesize($imagePath) / 1024) : null;
+    }
 }
