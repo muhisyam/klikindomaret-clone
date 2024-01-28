79d163c038beb13ae14721a539f00012a5dbfc9b
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-01-28 18:44:13 +0700

    refactor: add static login background image

--- resources/views/frontend/components/login.blade.php
+++ resources/views/frontend/components/login.blade.php
@@ -1,25 +1,27 @@
-<div class="login fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[60] modal">
-    <div class="login-wrapper max-w-sm rounded-lg">
-        <section class="header-section min-h-[160px]">
-            <div class="nav-back absolute top-4 left-4">
+<div class="login | fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[60] modal">
+    <div class="login-wrapper | max-w-sm bg-white rounded-lg">
+        <section class="header-section | min-h-[160px]">
+            <div class="nav-back | absolute top-4 left-4">
                 <button>
-                    <div class="icon text-2xl"><i class="ri-arrow-left-line"></i></div>
+                    <div class="icon | text-2xl"><i class="ri-arrow-left-line"></i></div>
                 </button>
             </div>
             <div class="img">
-                <img class="w-full rounded-t-xl" src="https://account.klikindomaret.com/static/media/background-login.89f29c74.jpg" alt="">
+                <img class="w-full rounded-t-xl" src="{{ asset('img/auth/background-login.jpg') }}" alt="">
             </div>
         </section>
-        <section class="body-section relative z-10 bg-white rounded-t-xl p-4 -mt-8">
-            <div class="nav flex items-center justify-between py-2 mb-4">
-                <div class="title font-bold">Masuk</div>
-                <a href="" class="btn-register text-[#0079c2] text-sm">Daftar</a>
+        <section class="body-section | relative z-10 bg-white rounded-t-xl p-4 -mt-8">
+            <div class="nav | flex items-center justify-between py-2 mb-4">
+                <div class="title | font-bold">Masuk</div>
+                <a href="" class="btn-register | text-[#0079c2] text-sm">Daftar</a>
             </div>
             <div class="form">
-                <form class="form-input-wrapper" action="">
+                <form class="form-input-wrapper" method="POST" action="{{ route('login.attempt') }}">
+                    @csrf
+
                     <div class="item-input-group mb-4">
                         <label for="form-input-phone-email" class="block text-sm mb-1">Nomor HP/Email</label>
-                        <input id="form-input-phone-email" type="text" name="phone-email" class="h-10 w-full text-sm border border-[#ccc] rounded py-2 px-3 focus:ring-transparent" placeholder="Masukkan nomor HP/email">
+                        <input id="form-input-phone-email" type="text" name="phone_email" class="h-10 w-full text-sm border border-[#ccc] rounded py-2 px-3 focus:ring-transparent" placeholder="Masukkan nomor HP/email">
                     </div>
                     <div class="item-input-group mb-4">
                         <label for="form-input-password" class="block text-sm mb-1">Kata Sandi</label>
@@ -27,11 +29,11 @@
                     </div>
                     <div class="item-input-group flex justify-between mb-4">
                         <div class="remember-me-wrapper flex items-center">
-                            <input id="form-input-rememberme" type="checkbox" name="rememberme" class="border border-[#ccc] rounded me-2 focus:ring-transparent" placeholder="Masukkan rememberme">
-                            <label for="form-input-rememberme" class="block text-sm">Ingat Saya</label>
+                            <input id="form-input-rememberme" type="checkbox" name="remember_me" class="border border-[#ccc] rounded me-2 focus:ring-transparent" placeholder="Masukkan rememberme">
+                            <label for="form-input-remember-me" class="block text-sm">Ingat Saya</label>
                         </div>
                         <div class="forgot-password-wrapper">
-                            <a href="" class="forgot-password text-[#0079c2] text-sm">Lupa Kata Sandi?</a>
+                            <a href="" class="forgot-password | text-[#0079c2] text-sm">Lupa Kata Sandi?</a>
                         </div>
                     </div>
                     <div class="form-button">
@@ -40,7 +42,7 @@
                 </form>
             </div>
         </section>
-        <section class="footer-section bg-white rounded-b-xl py-6 px-4">
+        <section class="footer-section | bg-white text-sm  rounded-b-xl py-6 px-4">
             <a href="" class="help text-[#0079c2]">Butuh Bantuan?</a>
         </section>
     </div>
