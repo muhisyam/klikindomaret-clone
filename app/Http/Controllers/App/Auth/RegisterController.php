eb3ab6d564797680f3b3b7ccfbe984687fdfacf9
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-02-10 20:34:58 +0700

    feat: add register app controller

--- app/Http/Controllers/App/Auth/RegisterController.php
+++ app/Http/Controllers/App/Auth/RegisterController.php
@@ -6,9 +6,51 @@ use App\Actions\ClientRequestAction;
 use App\Actions\CreateMultipartAction;
 use App\DataTransferObjects\ClientRequestDto;
 use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    
+    protected string $endpoint;
+
+    public function __construct(
+        protected ClientRequestAction $clientAction,
+        protected CreateMultipartAction $multipartAction,
+    ) {
+        $this->endpoint = env('API_URL') . '/v1/register';
+    }
+
+    public function register(Request $request)
+    {
+        $formData = $this->multipartAction->create($request->all());
+        
+        $data = $this->clientAction->request(
+            new ClientRequestDto(
+                method: 'POST',
+                endpoint: $this->endpoint,
+                formData: $formData,
+            )
+        );
+
+        if ($data['meta']['status_code'] === 400) {
+            return redirect()
+                ->back()
+                ->with([
+                    'step' => 'complete_register', 
+                    'inputError' => $data,
+                ])
+                ->withInput();
+        }
+
+        // TODO: move to authenticate event
+        $request->session()->regenerate();
+
+        session([
+            'auth_token' => $data['data']['token'],
+            'fullname' => $data['data']['user']['fullname'],
+            'username' => $data['data']['user']['username'],
+        ]);
+
+        return redirect(RouteServiceProvider::HOME);
+    }
 }
