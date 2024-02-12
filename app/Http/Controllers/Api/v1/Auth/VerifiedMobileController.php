f5e1cc68294da7d0a3830bc22729b4a2e6bfb6f8
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-02-12 21:04:39 +0700

    refactor: move otp verify logic in api controller

--- app/Http/Controllers/Api/v1/Auth/VerifiedMobileController.php
+++ app/Http/Controllers/Api/v1/Auth/VerifiedMobileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\v1\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\VerifyOtpRequest;
 use App\Http\Requests\VerifyMobileRequest;
 use App\Traits\VerifyUserMobile;
 
@@ -10,10 +11,34 @@ class VerifiedMobileController extends Controller
 {
     use VerifyUserMobile;
 
-    public function store(VerifyMobileRequest $request)
+    public function verifyMobile(VerifyMobileRequest $request)
     {
-        $this->verifyMobile($request);
+        $this->verify($request);
 
-        return response()->json(['otp' => $this->otpCode]);
+        return response()->json([
+            'data' => [
+                'otp' => $this->otpCode,
+                'step' => 'verify_otp',
+            ],
+            'meta' => [
+                'status_code' => 200,
+                'message' => 'Success',
+            ],
+        ], 200);
+    }
+
+    public function verifyOtp(VerifyOtpRequest $request)
+    {
+        $this->verified($request);
+
+        return response()->json([
+            'data' => [
+                'step' => 'complete_register'
+            ],
+            'meta' => [
+                'status_code' => 202,
+                'message' => 'Accepted',
+            ],
+        ], 202);
     }
 }
