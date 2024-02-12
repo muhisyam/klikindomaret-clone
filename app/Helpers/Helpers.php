87363bf775f4e4c5d730336de5ab6c75d2da4086
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-02-12 23:37:35 +0700

    feat: add format mobile number helper

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -4,3 +4,13 @@ function formatCurrencyIDR($price)
 {
     return number_format($price, 0, '.', '.');
 }
+
+function prettierMobileNumber($number)
+{
+    $number = substr($number, 1);
+
+    $prettierNumber = '+62 ' . substr_replace($number, '-', 3, 0);
+    $prettierNumber = substr_replace($prettierNumber, '-', 12, 0);
+
+    return $prettierNumber;
+}
