e45ea74efe9d25327e9b682f018ef3bcc58c0c5d
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-02-14 17:29:58 +0700

    refactor: sort tw class and add invalid message

--- resources/views/frontend/auth/register-complete.blade.php
+++ resources/views/frontend/auth/register-complete.blade.php
@@ -1,49 +1,49 @@
 <div class="complete-wrapper | max-w-sm rounded-xl bg-white">
     <section class="min-h-auth-header">
-        <figure>
-            <img class="w-full rounded-t-xl" src="{{ asset('img/auth/background-registration.jpg') }}" alt="">
-        </figure>
+        <button type="button" class="button-auth | absolute top-3 left-3 h-9 w-9 rounded-full pt-0.5 bg-white text-xl" data-modal-close="register">
+            <i class="ri-arrow-left-line"></i>
+        </button>
+        <img class="header-thumb | w-full rounded-t-xl" src="{{ asset('img/auth/background-registration.jpg') }}" alt="Register Background">
     </section>
-    <section class="relative z-10 min-h-auth bg-white rounded-t-xl p-4 -mt-8">
-        <div class="flex items-center justify-between py-2 mb-4">
-            <div class="title font-bold">Daftar</div>
-        </div>
-        <div class="form mb-4">
-            <form class="form-input-wrapper" action="">
-                <section class="block">
-                    <div class="item-input-group mb-4">
-                        <label for="form-input-phone" class="block text-sm mb-1">Tanggal Lahir</label>
-                        <input id="form-input-phone" type="date" name="phone" class="h-10 w-full text-sm border border-[#ccc] rounded py-2 px-3 focus:ring-transparent" placeholder="Masukkan nomor HP/email">
-                    </div>
-                    <div class="item-input-group mb-4">
-                        <label for="form-input-phone" class="block text-sm mb-1">Nama Lengkap</label>
-                        <input id="form-input-phone" type="text" name="phone" class="h-10 w-full text-sm border border-[#ccc] rounded py-2 px-3 focus:ring-transparent" placeholder="Masukkan nomor HP/email">
-                    </div>
-                    <div class="item-input-group mb-4">
-                        <label for="form-input-phone" class="block text-sm mb-1">Username</label>
-                        <input id="form-input-phone" type="text" name="phone" class="h-10 w-full text-sm border border-[#ccc] rounded py-2 px-3 focus:ring-transparent" placeholder="Masukkan nomor HP/email">
-                    </div>
-                </section>
-                <section class="hidden">
-                    <div class="item-input-group mb-4">
-                        <label for="form-input-phone" class="block text-sm mb-1">Password</label>
-                        <input id="form-input-phone" type="text" name="phone" class="h-10 w-full text-sm border border-[#ccc] rounded py-2 px-3 focus:ring-transparent" placeholder="Masukkan nomor HP/email">
-                    </div>
-                    <div class="item-input-group mb-4">
-                        <label for="form-input-phone" class="block text-sm mb-1">Confirm Password</label>
-                        <input id="form-input-phone" type="text" name="phone" class="h-10 w-full text-sm border border-[#ccc] rounded py-2 px-3 focus:ring-transparent" placeholder="Masukkan nomor HP/email">
-                    </div>
-                </section>
-                <div class="form-button">
-                    <button class="h-10 w-full bg-[#0079c2] text-white rounded py-2 px-4 disabled">Daftar</button>
+    <section class="body-section | relative z-10 -mt-8 min-h-auth rounded-t-xl p-4 bg-white">
+        <h1 class="title | mb-4 py-2 font-bold">Daftar</h1>
+        <form class="form-input-wrapper | mb-4" method="POST" action="{{ route('register.complete') }}">
+            @csrf
+
+            <input type="text" name="mobile_number" class="hidden" value="{{ session('mobile_number') }}">
+            <section class="first-form">
+                <div class="item-input-group mb-4">
+                    <label for="form-input-birthdate" class="block text-sm mb-1">Tanggal Lahir</label>
+                    <input id="form-input-birthdate" type="date" name="birthdate" class="h-10 w-full rounded py-2 px-3 border border-[#ccc] text-sm focus:ring-transparent">
                 </div>
-            </form>
-        </div>
-        <div class="term-condition text-[#95989a] text-xs">
+                <div class="item-input-group mb-4">
+                    <label for="form-input-fullname" class="block text-sm mb-1">Nama Lengkap</label>
+                    <input id="form-input-fullname" type="text" name="fullname" class="h-10 w-full rounded py-2 px-3 border border-[#ccc] text-sm focus:ring-transparent">
+                </div>
+                <div class="form-button">
+                    <button type="button" class="button-switch-form | h-10 w-full py-2 px-4 rounded bg-[#0079c2] text-white" data-switch-form="section-second" disabled>Lanjut</button>
+                </div>
+            </section>
+            <section class="second-form hidden">
+                <div class="item-input-group mb-4">
+                    <label for="form-input-username-registration" class="block text-sm mb-1">Username</label>
+                    <input id="form-input-username-registration" type="text" name="username" class="h-10 w-full rounded py-2 px-3 border border-[#ccc] text-sm focus:ring-transparent">
+                </div>
+                <div class="item-input-group mb-4">
+                    <label for="form-input-password-registration" class="block text-sm mb-1">Password</label>
+                    <input id="form-input-password-registration" type="password" name="password" class="h-10 w-full rounded py-2 px-3 border border-[#ccc] text-sm focus:ring-transparent">
+                </div>
+                <div class="form-button | flex gap-2">
+                    <button type="button" class="button-switch-form | h-10 w-full py-2 px-4 rounded text-secondary" data-switch-form="section-first">Kembali</button>
+                    <button type="submit" class="button-submit-form | h-10 w-full py-2 px-4 rounded bg-[#0079c2] text-white" disabled>Daftar</button>
+                </div>
+            </section>
+        </form>
+        <div class="term-condition | text-xs text-[#95989a]">
             <p>Dengan mendaftar, kamu menyetujui <a href="" class="text-[#0079c2]">Syarat & Ketentuan</a> dari Klik Indomaret</p>
         </div>
     </section>
-    <section class="footer-section bg-white rounded-b-xl py-6 px-4">
-        <a href="" class="help text-[#0079c2]">Butuh Bantuan?</a>
+    <section class="footer-section | rounded-b-xl py-6 px-4 bg-white">
+        <a href="" class="help | text-[#0079c2] hover:underline-offset-2">Butuh Bantuan?</a>
     </section>
 </div>
--- resources/views/frontend/auth/register-verify-mobile.blade.php
+++ resources/views/frontend/auth/register-verify-mobile.blade.php
@@ -1,40 +1,51 @@
 <div class="verify-wrapper | w-96 rounded-xl bg-white">
     <section class="header-section | flex flex-col items-center min-h-auth-header">
-        <button type="button" class="nav-back | absolute top-3 left-3 h-9 w-9 pt-0.5 rounded-full bg-white text-xl">
+        <button type="button" class="button-auth | absolute top-3 left-3 h-9 w-9 rounded-full pt-0.5 bg-white text-xl" data-modal-close="login">
             <i class="ri-arrow-left-line"></i>
         </button>
         <h1 class="header-title | font-bold mt-4">Verifikasi Nomor HP</h1>
-        <img class="header-thumb | mt-2 mb-6 w-40 rounded-t-xl" src="{{ asset('img/auth/verification-phone-number.png') }}" alt="">
+        <img class="header-thumb | w-40 rounded-t-xl" src="{{ asset('img/auth/verification-phone-number.png') }}" alt="">
     </section>
-    <section class="body-section | relative z-10 -mt-8 min-h-auth p-4 rounded-t-xl bg-white">
-        <p class="body-message | mt-2 mb-4 text-center text-xs">Silahkan masukkan kode verifikasi <br> yang sudah dikirimkan ke Whatsapp dibawah</p>
-        <p class="body-mobile-number | font-bold text-center text-secondary text-lg">{{ prettierMobileNumber(session('mobile_number')) }}</p>
+    <section class="body-section | relative z-10 min-h-auth rounded-t-xl p-4 bg-white">
+        <p class="body-message | mb-4 text-center text-xs">Silahkan masukkan kode verifikasi <br> yang sudah dikirimkan ke Whatsapp dibawah</p>
+        <p class="body-mobile-number | font-bold text-center text-lg text-secondary">{{ prettierMobileNumber(session('mobile_number')) }}</p>
         <form class="form-input-wrapper | mt-4 mb-2" method="POST" action="{{ route('verify.otp') }}">
             @csrf
             
-            <div class="item-input-group | flex justify-center gap-2 mb-4">
-                <input id="form-input-otp" type="text" name="otp[]" class="h-10 w-10 rounded border border-[#ccc] text-center text-sm focus:ring-transparent" maxlength="1" autofocus>
-                <input id="form-input-otp" type="text" name="otp[]" class="h-10 w-10 rounded border border-[#ccc] text-center text-sm focus:ring-transparent" maxlength="1">
-                <input id="form-input-otp" type="text" name="otp[]" class="h-10 w-10 rounded border border-[#ccc] text-center text-sm focus:ring-transparent" maxlength="1">
-                <input id="form-input-otp" type="text" name="otp[]" class="h-10 w-10 rounded border border-[#ccc] text-center text-sm focus:ring-transparent" maxlength="1">
-                <input id="form-input-otp" type="text" name="otp[]" class="h-10 w-10 rounded border border-[#ccc] text-center text-sm focus:ring-transparent" maxlength="1">
-                <input id="form-input-otp" type="text" name="otp[]" class="h-10 w-10 rounded border border-[#ccc] text-center text-sm focus:ring-transparent" maxlength="1">
+            <div class="item-input-group | hidden">
+                <input type="text" name="mobile_number" class="hidden" value="{{ session('mobile_number') }}">
             </div>
-            @if(! is_null(session('verify_wrong')))
-                <div class="invalid-feedback | -mt-2 mb-2 text-red-700 text-center text-sm">
-                    <p>{{ session('verify_wrong') }}</p>
+            <div class="item-input-group | hidden">
+                <input type="text" name="otp" class="hidden" value="{{ session('otp') }}">
+            </div>
+            <div class="item-input-group | flex justify-center gap-2 mb-4">
+                <input type="text" name="otp_confirmation[]" class="h-10 w-10 rounded border border-[#ccc] text-center text-sm focus:ring-transparent" maxlength="1" autofocus>
+                <input type="text" name="otp_confirmation[]" class="h-10 w-10 rounded border border-[#ccc] text-center text-sm focus:ring-transparent" maxlength="1">
+                <input type="text" name="otp_confirmation[]" class="h-10 w-10 rounded border border-[#ccc] text-center text-sm focus:ring-transparent" maxlength="1">
+                <input type="text" name="otp_confirmation[]" class="h-10 w-10 rounded border border-[#ccc] text-center text-sm focus:ring-transparent" maxlength="1">
+                <input type="text" name="otp_confirmation[]" class="h-10 w-10 rounded border border-[#ccc] text-center text-sm focus:ring-transparent" maxlength="1">
+                <input type="text" name="otp_confirmation[]" class="h-10 w-10 rounded border border-[#ccc] text-center text-sm focus:ring-transparent" maxlength="1">
+            </div>
+            @unless (is_null(session('input_error')))
+                <div class="invalid-feedback | -mt-2 mb-2 text-center text-sm text-red-700">
+                    <p>{{ session('input_error')['errors']['incorrect_otp'][0] }}</p>
                 </div>
-            @endif
+            @endunless
             <div class="item-input-button">
-                <button type="submit" class="h-10 w-full py-2 px-4 rounded bg-[#0079c2] text-white disabled">Verifikasi</button>
+                <button type="submit" class="button-submit-otp | h-10 w-full rounded py-2 px-4 bg-[#0079c2] text-white disabled">Verifikasi</button>
             </div>
         </form>
-        <div class="term-condition | text-[#95989a] text-xs">
-            <p>Coba lagi atau gunakan metode verifikasi lain</p>
-            <p>Segera verifikasi 59 detik</p>
+        <div class="resend-otp | flex justify-center text-xs text-[#95989a]">
+            <p class="message"></p>
+            <form class="form-input-wrapper hidden" method="POST" action="{{ route('verify.mobile') }}">
+                @csrf
+                
+                <input type="text" name="mobile_number" class="hidden" value="{{ session('mobile_number') }}">
+                <button type="submit" class="text-secondary underline">Kirim OTP kembali.</button>                
+            </form>
         </div>
     </section>
-    <section class="footer-section | py-6 px-4 bg-white rounded-b-xl">
+    <section class="footer-section | rounded-b-xl py-6 px-4 bg-white">
         <a href="#" class="help | text-[#0079c2] hover:underline-offset-2">Butuh Bantuan?</a>
     </section>
 </div>
