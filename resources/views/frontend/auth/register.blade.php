0df5cb919c5fb25023e5e6f4b686ce46e9abdb40
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-02-15 21:00:59 +0700

    refactor: change from condition to switch again

--- resources/views/frontend/auth/register.blade.php
+++ resources/views/frontend/auth/register.blade.php
@@ -1,21 +1,17 @@
-<div class="register | fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[60]">
-    @php 
-        $step = session('step') ?? null;
-        $errorKey = session('input_error') ? array_keys(session('input_error')['errors'])[0] : null; 
-    @endphp
+<div @class([
+        'register modal | fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[60]', 
+        'show' => ! is_null(session('input_error')) || ! is_null(session('step')),
+    ])>
 
-    @if ($step === 'verify_otp' || ($step === null && $errorKey === 'incorrect_otp'))
-        {{ dd($step, $errorKey, $step === 'verify_otp' || ($step === null && $errorKey === 'incorrect_otp')) }}
-        @include('frontend.auth.register-verify-mobile')
-        <div>{{ session('otp') }}</div>
-    @elseif ($step === 'complete_register' || ($step === null && $errorKey !== 'mobile_number'))
-    {{ dd($step, $errorKey, ($step === null && $errorKey !== 'mobile_number')) }}
-
-        @include('frontend.auth.register-complete')
-    @else
-    {{ dd($step, $errorKey) }}
-
-        @include('frontend.auth.register-mobile')
-    @endif
-
+    @switch(session('step'))
+        @case('Verify OTP')
+            @include('frontend.auth.register-verify-mobile')
+            <div>{{ session('otp') }}</div>
+            @break
+        @case('Complete Registration')
+            @include('frontend.auth.register-complete')    
+            @break
+        @default
+            @include('frontend.auth.register-mobile')
+    @endswitch
 </div>
