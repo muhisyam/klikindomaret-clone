5c8589df0270f24c6259326544cdc6764168127a
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-02-16 21:55:45 +0700

    feat: add api config

---
+++ config/api.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | API URL
+    |--------------------------------------------------------------------------
+    |
+    | This URL is used by the console to properly generate URLs when using
+    | API for fecting data. You should set this to the root of
+    | your application so that it is used when running Artisan tasks.
+    |
+    */
+
+    'url' => env('API_URL', 'http://127.0.0.1:8080/api/'),
+    
+];
