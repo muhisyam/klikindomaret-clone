b7466afef72f94ebeb2ed8a3d2c67b7c067ebef5
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-02-16 21:02:15 +0700

    feat: add overlay show condition

--- resources/views/frontend/components/overlay.blade.php
+++ resources/views/frontend/components/overlay.blade.php
@@ -1,3 +1,4 @@
-<div id="bg-overlay" class="overlay fixed top-0 left-0 h-full w-full z-[55] hidden">
-    <div class="fixed w-full h-full bg-black opacity-50"></div>
-</div>
+<div id="bg-overlay" @class([
+    'overlay | fixed top-0 left-0 h-full w-full z-[55] bg-black opacity-50', 
+    'hidden' => is_null(session('input_error')) && is_null(session('step')),
+])></div>
