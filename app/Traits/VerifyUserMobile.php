1a919ccdc1e73b6c2497f554e0372bb6b781980e
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-02-19 19:02:48 +0700

    feat: register mobile verify event

--- app/Traits/VerifyUserMobile.php
+++ app/Traits/VerifyUserMobile.php
@@ -3,26 +3,17 @@
 namespace App\Traits;
 
 use App\Actions\ErrorTraceAction;
+use App\Events\Auth\MobileVerify;
 use App\Http\Requests\Auth\VerifyMobileRequest;
 use App\Http\Requests\Auth\VerifyOtpRequest;
-use App\Models\User;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Str;
-use Twilio\Rest\Client;
 
 trait VerifyUserMobile
 {
-    protected $request, $twilio, $targetFormattedNumber, $otpCode;
-
-    public function __construct()
-    {
-        $this->twilio = new Client(
-            getenv('TWILIO_ACCOUNT_SID'), 
-            getenv('TWILIO_AUTH_TOKEN')
-        );
-    }
+    protected $request;
     
     /**
      * Attempt mobile to get otp code.
@@ -30,14 +21,17 @@ trait VerifyUserMobile
     public function verify(VerifyMobileRequest $request): void
     {
         $this->request = $request;
-        // TODO: Move to request
-        $this->ensureIsNotInDatabase();
         $this->ensureIsNotRateLimited();
         
-        $this->targetFormattedNumber = $this->formatFirstNumberCode($request['mobile_number']);
-        $this->otpCode = $this->createOTP();
+        $targetNumber = $this->formatFirstNumberCode($request['mobile_number']);
+        $otpCode = $this->createOTP();
 
-        $this->sendMobileVerificationNotification();
+        event(new MobileVerify([
+            'register_to_twilio' => false, 
+            'target_number' => $targetNumber, 
+            'otp' => $otpCode,
+            'via' => $request['via'],
+        ]));
     }
 
     /**
@@ -50,51 +44,13 @@ trait VerifyUserMobile
     }
 
     /**
-     * Ensure the mobile is not exist in database.
-     */
-    protected function ensureIsNotInDatabase(): void 
-    {
-        if (! User::where('mobile_number', $this->request['mobile_number'])->first()) {
-            return;
-        }
-
-        $trace = app(ErrorTraceAction::class)->execute();
-        $this->sendMobileAlreadyExists($trace);
-    }
-
-    /**
-     * Send response when mobile detected exist in database.
-     *
-     * @throws \Illuminate\Http\Exceptions\HttpResponseException
-     */
-    protected function sendMobileAlreadyExists(array $trace): JsonResponse
-    {
-        throw new HttpResponseException(response([
-            'errors' => [
-                'mobile_number' => [
-                    trans('validation.unique', [
-                        'attribute' => 'Number',
-                    ])
-                ],
-            ],
-            'meta' => [
-                'status_code' => 400,
-                'message' => 'Bad Request',
-                'trace' => [
-                    'File' => $trace['filename'],
-                    'Line' => $trace['line'],
-                ],
-            ],
-        ])->setStatusCode(400));
-    }
-
-    /**
      * Ensure the mobile request is not rate limited.
      */
     public function ensureIsNotRateLimited(): mixed
     {
         if (! RateLimiter::tooManyAttempts($this->throttleKey(), 3)) {
-            return RateLimiter::hit($this->throttleKey());
+            // Set 300s for resend otp session 
+            return RateLimiter::hit($this->throttleKey(), 300);
         }
 
         // TODO: change event lockout
@@ -120,11 +76,14 @@ trait VerifyUserMobile
     protected function sendTooManyMobileAttempts(array $trace): JsonResponse
     {
         $seconds = RateLimiter::availableIn($this->throttleKey());
-        $message = 'Too many attempts. Please try again in ' . $seconds > 60 ? $seconds . ' seconds.' : ceil($seconds / 60) . ' minutes.';
+        $seconds = $seconds > 60 ? $seconds . ' seconds.' : ceil($seconds / 60) . ' minutes.';
+        $message = 'Please try again in ' . $seconds . ' Or use another number.';
 
         throw new HttpResponseException(response([
             'errors' => [
-                'mobile_number' => [$message],
+                'mobile_number' => [
+                    $message
+                ],
             ],
             'meta' => [
                 'status_code' => 400,
@@ -153,33 +112,5 @@ trait VerifyUserMobile
     protected function createOTP(): int
     {
         return mt_rand(111111, 999999);
-    }
-
-    /**
-     * Send a message that contain otp code to target.
-     */
-    // TODO: move to event listener? VerifyMobile?
-    public function sendMobileVerificationNotification(): void
-    {
-        $this->twilio->messages->create(
-            'whatsapp:' . $this->targetFormattedNumber,
-            [
-                'from' => 'whatsapp:+14155238886',
-                'body' => '*' . $this->otpCode . '* adalah kode verifikasi anda. Demi keamanan, *jangan bagikan kode ini.*',
-            ]
-        );
-    }
-
-    /**
-     * Register mobile to VerifyId list in Twilio.
-     */
-    public function sendMobileToTwilio(): void
-    {
-        $this->twilio->validationRequests->create(
-            $this->targetFormattedNumber,
-            [
-                "friendlyName" => substr($this->targetFormattedNumber, 0)
-            ]
-        );
     }
 }
