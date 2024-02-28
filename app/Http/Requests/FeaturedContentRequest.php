55c31a986f0737822f5ea783ba184de3c394d5d3
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-02-28 22:37:15 +0700

    feat: featured content request

---
+++ app/Http/Requests/FeaturedContentRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Actions\ErrorTraceAction;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class FeaturedContentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'featured_name' => ['required', 'string', 'max:100'],
+            'featured_slug' => ['required', 'string', 'max:200'],
+            'product_ids' => ['required'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $trace = app(ErrorTraceAction::class)->execute();
+        
+        throw new HttpResponseException(response([
+            'errors' => $validator->getMessageBag(),
+            'meta' => [
+                'status_code' => 400,
+                'message' => 'Bad Request',
+                'trace' => [
+                    'File' => $trace['filename'],
+                    'Line' => $trace['line'],
+                ],
+            ],
+        ], 400));
+    }
+}
