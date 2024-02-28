5f9ae6a7444541a059dadb40ec95cae9f1b29e5f
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-02-28 22:36:19 +0700

    feat: add featured content table

---
+++ database/migrations/2024_02_28_134005_create_featured_contents_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('featured_contents', function (Blueprint $table) {
+            $table->id();
+            $table->string('featured_name', 100);
+            $table->string('featured_slug', 200)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('featured_contents');
+    }
+};
---
+++ database/migrations/2024_02_28_134118_create_featured_content_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('featured_content_product', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('featured_content_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreignId('product_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('featured_content_product');
+    }
+};
