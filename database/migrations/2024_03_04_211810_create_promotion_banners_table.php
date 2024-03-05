56b9777352d099032c4439aa45c27294cfe2f16f
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-03-05 23:35:25 +0700

    refactor:change image field name, drop nullable in date

--- database/migrations/2024_03_04_211810_create_promotion_banners_table.php
+++ database/migrations/2024_03_04_211810_create_promotion_banners_table.php
@@ -24,14 +24,14 @@ return new class extends Migration
             $table->smallInteger('promotion_quota')->nullable();
             $table->string('short_term_condition', 100)->nullable();
             $table->text('term_condition')->nullable();
-            $table->string('product_image_name', 50)->nullable();
-            $table->string('original_product_image_name', 200)->nullable();
+            $table->string('banner_image_name', 50)->nullable();
+            $table->string('original_banner_image_name', 200)->nullable();
             $table->string('deploy_status', 15);
             $table->string('route_name', 50);
             $table->string('redirect_url', 200)->nullable();
             $table->string('redirect_out_site_url', 200)->nullable();
-            $table->date('start_date')->nullable();
-            $table->date('end_date')->nullable();
+            $table->date('start_date');
+            $table->date('end_date');
             $table->string('model_type', 10);
             $table->text('banner_meta_keyword');
             $table->timestamps();
