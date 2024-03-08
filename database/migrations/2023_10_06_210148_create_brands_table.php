ae87f0b43b452aab43d46f30fa5d246d9afc5027
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-03-08 23:48:11 +0700

    feat: add brand table

---
+++ database/migrations/2023_10_06_210148_create_brands_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('official_store_id')
+                ->nullable()
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+            $table->string('store_name', 100);
+            $table->string('store_slug', 200)->unique();
+            $table->string('model_type', 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brands');
+    }
+};
