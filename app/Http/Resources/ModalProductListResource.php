25820eab0dd44424a1d27fc1dba902779da71cc3
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-03-16 22:32:07 +0700

    feat: add new return key modal product list

--- app/Http/Resources/ModalProductListResource.php
+++ app/Http/Resources/ModalProductListResource.php
@@ -12,13 +12,14 @@ class ModalProductListResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
-        foreach ($this->resource as $value) {
-            $data[] = [
-                'id' => $value->id,
-                'product_name' => $value->product_name,
-            ];
-        }
-
-        return $data;
+        return [
+            "id" => $this->id,
+            "category_lvl_1" => $this->category_lvl_1,
+            "category_lvl_3" => $this->category_lvl_3,
+            "product_name" => $this->product_name,
+            "product_slug" => $this->product_slug,
+            "product_meta_keyword" => $this->product_meta_keyword,
+            "product_image_name" => $this->product_image_name,
+        ];
     }
 }
