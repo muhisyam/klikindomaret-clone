7a577ddf59feaaa3cd1d4ff5c7b155245b9e22af
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-03-17 19:20:50 +0700

    fix: promo content table

--- resources/views/livewire/admin/content-management/promotion-banner/table.blade.php
+++ resources/views/livewire/admin/content-management/promotion-banner/table.blade.php
@@ -1,27 +1,26 @@
-<table class="w-full">
+<table class="w-full" wire:init="loadContent">
     <thead class="border-b bg-light-gray-50 text-light-gray-400 text-sm text-left uppercase">
         <tr>
             <th class="p-3 rounded-tl-md w-12"><input type="checkbox" class="block m-auto" aria-label="Checkbox select all data"></th>
             <th class="py-3 px-4">
                 <div @class([
                     'relative flex cursor-pointer', 
-                    'active-asc' => $sortBy === "featured_name" && $orderBy === "asc",
-                    'active-desc' => $sortBy === "featured_name" && $orderBy === "desc",
-                ]) wire:click="sortBy('featured_name')">
+                    'active-asc' => $sortBy === "banner_name" && $orderBy === "asc",
+                    'active-desc' => $sortBy === "banner_name" && $orderBy === "desc",
+                ]) wire:click="sortBy('banner_name')">
                     <div class="label me-1">Promosi</div>
                     <div class="sortable" data-sort></div>
                 </div>
             </th>
             <th class="py-3 px-4">Kuota</th>
-            <th class="py-3 px-4">Status</th>
+            <th class="py-3 px-4 w-32 text-center">Status</th>
             <th class="py-3 px-4">Tenggat Waktu</th>
-            <th class="py-3 px-4">
+            <th class="py-3 px-4 w-40">
                 <div @class([
-                    'relative flex cursor-pointer', 
-                    'active-asc' => $sortBy === "product_count" && $orderBy === "asc",
-                    'active-desc' => $sortBy === "product_count" && $orderBy === "desc",
-                ]) wire:click="sortBy('product_count')">
-                    {{-- with button show --}}
+                    'relative mx-auto flex cursor-pointer', 
+                    'active-asc' => $sortBy === "promo_products_count" && $orderBy === "asc",
+                    'active-desc' => $sortBy === "promo_products_count" && $orderBy === "desc",
+                ]) wire:click="sortBy('promo_products_count')">
                     <div class="label me-1">Jumlah Produk</div>
                     <div class="sortable" data-sort></div>
                 </div>
@@ -32,22 +31,39 @@
         </tr>
     </thead>
     <tbody class="text-sm">
+        @unless (is_null($data))
+
         @forelse ($data['data'] as $index => $content)
         <tr class="border-b">
             <td class="py-2 px-3">
                 <input type="checkbox" class="block m-auto" aria-label="Checkbox select data">
             </td>
-            <td class="py-2 px-4">{{ $content['featured_name'] }}</td>
-            <td class="py-2 px-4">{{ $content['featured_products_count'] }}</td>
             <td class="py-2 px-4">
-                <x-button class="mx-auto py-1.5 px-2" buttonStyle="secondary">
+                <div class="flex gap-2 h-8">
+                    <img class="rounded" src="{{ asset('img/uploads/promo-banners/' . $content['banner_image_name']) }}" alt="Promo Banner Image">
+                    <div>
+                        <div class="font-bold line-clamp-1">{{ $content['banner_name'] }}</div>
+                        <div class="text-xs font-light">Kode: {{ $content['promotion_code'] ?? '-' }}</div>
+                    </div>
+                </div>
+            </td>
+            <td class="py-2 px-4 text-end">{{ formatNumber($content['promotion_quota']) }}</td>
+            <td class="py-2 px-4 text-center">{!! \App\Enums\DeployStatus::getStyle($content['banner_deploy_status']) !!}</td>
+        @php
+            // TODO: Continue this
+        @endphp
+            <td class="py-2 px-4">{{ $content['banner_date_diff'] }}</td>
+            <td class="py-2 px-4 flex items-center">
+                <x-button class="mx-auto p-2 gap-2" buttonStyle="secondary">
+                    <span class="leading-4 font-bold">{{ $content['promo_products_count'] }}</span>
                     <x-icon class="w-4 contrast-[6]" src="{{ asset('img/icons/icon-auth-visibility-password.webp') }}"/>
                 </x-button>
             </td>
+            <td class="py-2 px-4">{{ $content['banner_route_name'] }}</td>
             <td class="py-2 px-4">
                 <div class="relative">
                     {{-- Product Action Button --}}
-                    <x-button class="ms-auto overflow-hidden p-1 hover:bg-tertiary hover:text-secondary" data-target-action="{{ $content['featured_slug'] . '-action' }}">
+                    <x-button class="ms-auto overflow-hidden p-1 hover:bg-tertiary hover:text-secondary" data-target-action="{{ $content['banner_slug'] . '-action' }}">
                         <div class="icon-action | h-6 pt-0.5 px-1" data-tooltip-target="action-tooltip-{{ $index }}" data-tooltip-placement="bottom" action-icon-open><i class="ri-more-2-line"></i></div>
                         <div class="icon-close | h-6 pt-0.5 px-1 animation animation-bounceInRight animation-delay-200 hidden" data-tooltip-target="action-close-tooltip-{{ $index }}" data-tooltip-placement="bottom" action-icon-close><i class="ri-close-line"></i></div>
                     </x-button>
@@ -61,12 +77,12 @@
                     </div>
 
                     {{-- Product Action Wrapper --}}
-                    <div class="absolute top-0 right-10 hidden" data-trigger-action="{{ $content['featured_slug'] . '-action' }}">
+                    <div class="absolute top-0 right-10 hidden" data-trigger-action="{{ $content['banner_slug'] . '-action' }}">
                         <div class="w-24 flex justify-end gap-1 overflow-x-clip">
-                            <a href="{{ route('products.edit', ['product' => $content['featured_slug']]) }}" class="action-edit | bg-[#f5f5f5] text-[#999] rounded animation animation-bounceInRight animation-delay-100 shadow-sm p-1 px-2 hover:text-[#0079c2]" title="Edit">
+                            <a href="{{ route('products.edit', ['product' => $content['banner_slug']]) }}" class="action-edit | bg-[#f5f5f5] text-[#999] rounded animation animation-bounceInRight animation-delay-100 shadow-sm p-1 px-2 hover:text-[#0079c2]" title="Edit">
                                 <div class="icon-edit h-6 pt-0.5"><i class="ri-edit-box-fill"></i></div>
                             </a>
-                            <button class="action-del | bg-[#f5f5f5] text-[#999] rounded animation animation-bounceInRight shadow-sm p-1 px-2 hover:text-[#0079c2]" data-content-name="{{ $content['featured_name'] }}" wire:click="dispatchModal({{ json_encode($content) }})" title="Delete">
+                            <button class="action-del | bg-[#f5f5f5] text-[#999] rounded animation animation-bounceInRight shadow-sm p-1 px-2 hover:text-[#0079c2]" data-content-name="{{ $content['banner_name'] }}" wire:click="dispatchModal({{ json_encode($content) }})" title="Delete">
                                 <div class="icon-del h-6 pt-0.5"><i class="ri-delete-bin-6-fill"></i></div>
                             </button>
                         </div>
@@ -82,6 +98,14 @@
             </td>
         </tr>
         @endforelse
+        
+        @else
+        <tr>
+            <td class="rounded-b-md py-2 px-3 bg-light-gray-50" colspan="8">
+                Loading...
+            </td>
+        </tr>
+        @endunless
     </tbody>    
 </table>
 
@@ -95,7 +119,6 @@
 
             btnNoContent.addEventListener('click', () => document.querySelector('button[data-target-modal]').click())
         }
-        noContentBtn(); //remove
 
         document.addEventListener('livewire:initialized', () => {
             @this.on('contents-loaded', event => {
