9c5d3278640c3f6cdb0214a2ca06b1fb4c395377
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-03-21 16:33:47 +0700

    refactor: change tooltip to a class

--- public/js/components.js
+++ public/js/components.js
@@ -85,7 +85,7 @@ export function hideOpenedComponentsFromOutside() {
         const isBtnClosedModal = ! triggerEl.matches('button[data-target-modal]');
         const isBtnSwitchModal = ! triggerEl.matches('button[data-switch-form]');
         const isBtnRemoveSelect2 = ! triggerEl.matches('button.select2-selection__choice__remove');
-        const isPreventClose = ! triggerEl.matches('[prevent-close]');
+        const isPreventClose = ! triggerEl.hasAttribute('[prevent-close]');
 
         ! triggerEl.matches('button[data-target-dropdown]') ? hideOpenedDropdown() : '';
         isClickedInsideModal && isBtnClosedModal && isBtnSwitchModal && isBtnRemoveSelect2 && isPreventClose ? hideOpenedModal() : '';
@@ -152,74 +152,79 @@ export function createElement({parentTag = 'div', parentClass = [], innerBody = 
     return newHtmlObject;
 }
 
-var Tooltip = (function () { 
-    function Tooltip(targetEl, triggerEl, placementEl) {
-        this._targetEl = targetEl;
-        this._triggerEl = triggerEl;
-        this._placementEl = placementEl;
-        this._init();
+class Tooltip {
+    constructor(targetEl, triggerEl, placementEl, sidebarWidth) {
+        this.targetEl = targetEl;
+        this.triggerEl = triggerEl;
+        this.placementEl = placementEl;
+        this.sidebarWidth = sidebarWidth;
+        this.init();
     }
 
-    Tooltip.prototype._init = function () {
-        if (this._triggerEl) {
-            this._setupEventListeners();
-            this._setupAttribute();
+    init() {
+        if (this.triggerEl) {
+            this.setupEventListeners();
+            this.setupAttribute();
         }
+    }
+
+    setupEventListeners() {
+        this.handleStyle();
+        this.triggerEl.addEventListener('mouseenter', () => this.show());
+        this.triggerEl.addEventListener('mouseleave', () => this.hide());
+    }
+
+    handleStyle() {
+        const arrowEl = this.targetEl.querySelector('.tooltip-arrow');
+
+        this.setupStyleElement(arrowEl);
+        this.setupStyleArrow(arrowEl);
+    }
+
+    setupAttribute() {
+        this.targetEl.setAttribute('data-popper-placement', this.placementEl);
+    }
+
+    setupStyleElement(arrowEl) {
+        const triggerPos = this.triggerEl.getBoundingClientRect();  
+        const translateX = triggerPos.left - this.sidebarWidth + (triggerPos.width - this.targetEl.offsetWidth) / 2;
+        const translateY = triggerPos.top + triggerPos.height + arrowEl.offsetHeight;
+
+        this.targetEl.style.position = 'absolute';
+        this.targetEl.style.inset = '0px auto auto 0px';
+        this.targetEl.style.margin = '0px';
+        this.targetEl.style.transform = `translate3d(${translateX}px, ${translateY}px, 0px)`;
     };
 
-    Tooltip.prototype._setupAttribute = function () {
-        this._targetEl.setAttribute('data-popper-placement', this._placementEl);
-    };
-
-    Tooltip.prototype._setupEventListeners = function () {
-        var _this = this;
-        _this._handleStyle();
-        this._triggerEl.addEventListener('mouseenter', function() {
-            _this.show();
-        });
-        this._triggerEl.addEventListener('mouseleave', function() {
-            _this.hide();
-        });
-    };
-
-    Tooltip.prototype._handleStyle = function () {
-        var _this = this;
-        const arrow = this._targetEl.querySelector('.tooltip-arrow');
-
-        _this._setupStyleElement(arrow);
-        _this._setupStyleArrow(arrow);
-    };
-
-    Tooltip.prototype._setupStyleElement = function (arrow) {
-        const translateX = this._triggerEl.offsetLeft + (this._triggerEl.offsetWidth - this._targetEl.offsetWidth) / 2;
-        const translateY = this._triggerEl.offsetTop + this._triggerEl.offsetHeight + arrow.offsetHeight;
-
-        this._targetEl.style.position = 'absolute';
-        this._targetEl.style.inset = '0px auto auto 0px';
-        this._targetEl.style.margin = '0px';
-        this._targetEl.style.transform = `translate3d(${translateX}px, ${translateY}px, 0px)`;
-    };
-
-    Tooltip.prototype._setupStyleArrow = function (arrow) {
-        const translateX = (this._targetEl.offsetWidth / 2) - (arrow.offsetWidth / 2);
+    setupStyleArrow(arrowEl) {
+        const translateX = (this.targetEl.offsetWidth / 2) - (arrowEl.offsetWidth / 2);
         
-        arrow.style.position = 'absolute';
-        arrow.style.left = '0px';
-        arrow.style.transform = `translate3d(${translateX}px, 0px, 0px)`;
+        arrowEl.style.position = 'absolute';
+        arrowEl.style.left = '0px';
+        arrowEl.style.transform = `translate3d(${translateX}px, 0px, 0px)`;
     };
 
-    Tooltip.prototype.show = function () {
-        this._targetEl.classList.remove('opacity-0', 'invisible');
-        this._targetEl.classList.add('opacity-100', 'visible');
+    show() {
+        this.targetEl.classList.remove('opacity-0', 'invisible');
+        this.targetEl.classList.add('opacity-100', 'visible');
     };
 
-    Tooltip.prototype.hide = function () {
-        this._targetEl.classList.remove('opacity-100', 'visible');
-        this._targetEl.classList.add('opacity-0', 'invisible');
+    hide() {
+        this.targetEl.classList.remove('opacity-100', 'visible');
+        this.targetEl.classList.add('opacity-0', 'invisible');
     };
+}
 
-    return Tooltip;
-}());
+export function initTooltips() { 
+    document.querySelectorAll('[data-tooltip-target]').forEach(function(triggerEl) {
+        const tooltipId = triggerEl.getAttribute('data-tooltip-target');
+        const tooltipEl = document.getElementById(tooltipId);
+        const hasSidebar = document.querySelector('.sidebar');
+        const sidebarWidth = hasSidebar ? hasSidebar.getBoundingClientRect().width : 0;
+
+        new Tooltip(tooltipEl, triggerEl, 'bottom', sidebarWidth);
+    });
+};
 
 export class SimpleImageUploader {
     constructor(formInputImg) {
@@ -280,13 +285,3 @@ export class SimpleImageUploader {
 
     }
 }
-
-export function initTooltips() { 
-    document.querySelectorAll('[data-tooltip-target]').forEach(function(triggerEl) {
-        const tooltipId = triggerEl.getAttribute('data-tooltip-target');
-        const placement = triggerEl.getAttribute('data-tooltip-placement');
-        const tooltipEl = document.getElementById(tooltipId);
-
-        new Tooltip(tooltipEl, triggerEl, placement);
-    });
-};
