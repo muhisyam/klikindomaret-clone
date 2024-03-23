1cf3c5c8630c7e846c00ee25f0d4459d30c47886
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-03-23 21:02:09 +0700

    refactor: simplifying data action

--- resources/views/livewire/admin/content-management/promotion-banner/table.blade.php
+++ resources/views/livewire/admin/content-management/promotion-banner/table.blade.php
@@ -27,7 +27,7 @@
             </th>
             {{-- page or redirect, hover show the link --}}
             <th class="py-3 px-4">Link</th> 
-            <th class="py-3 px-4 rounded-tr-md"></th>
+            <th class="py-3 px-2 rounded-tr-md w-[124px]"></th>
         </tr>
     </thead>
     <tbody class="text-sm">
@@ -63,35 +63,22 @@
                 </div>
             </td>
             <td class="py-2 px-4">{{ $content['banner_route_name'] }}</td>
-            <td class="py-2 px-4">
-                <div class="relative">
-                    {{-- Product Action Button --}}
-                    <x-button class="ms-auto overflow-hidden p-1 hover:bg-tertiary hover:text-secondary" data-target-action="{{ $content['banner_slug'] . '-action' }}">
-                        <div class="icon-action | h-6 pt-0.5 px-1" data-tooltip-target="action-tooltip-{{ $index }}" data-tooltip-placement="bottom" action-icon-open><i class="ri-more-2-line"></i></div>
-                        <div class="icon-close | h-6 pt-0.5 px-1 animation animation-bounceInRight animation-delay-200 hidden" data-tooltip-target="action-close-tooltip-{{ $index }}" data-tooltip-placement="bottom" action-icon-close><i class="ri-close-line"></i></div>
-                    </x-button>
-                    <div id="action-tooltip-{{ $index }}" role="tooltip" class="absolute z-50 invisible inline-block px-3 py-2 text-xs font-medium text-white whitespace-nowrap transition-opacity duration-300 bg-gray-900 rounded-md shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                        Action
-                        <div class="tooltip-arrow" data-popper-arrow></div>
-                    </div>
-                    <div id="action-close-tooltip-{{ $index }}" role="tooltip" class="absolute z-50 invisible inline-block px-3 py-2 text-xs font-medium text-white whitespace-nowrap transition-opacity duration-300 bg-gray-900 rounded-md shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                        Hide
-                        <div class="tooltip-arrow" data-popper-arrow></div>
-                    </div>
+            <td class="py-2 px-2 flex justify-end">
+                <div class="-me-2.5 rounded-md h-9 w-0 bg-light-gray-50 transition-width duration-700">
+                    <div class="flex opacity-0 duration-200" data-trigger-action="{{ $content['banner_slug'] . '-action' }}">
+                        <x-button class="h-9 w-9 group hover:bg-light-gray-100">
+                            <x-icon class="mx-auto w-3 grayscale group-hover:grayscale-0" src="{{ asset('img/icons/icon-delete.webp') }}"/>
+                        </x-button>
 
-                    {{-- Product Action Wrapper --}}
-                    <div class="absolute top-0 right-10 hidden" data-trigger-action="{{ $content['banner_slug'] . '-action' }}">
-                        <div class="w-24 flex justify-end gap-1 overflow-x-clip">
-                            <a href="{{ route('products.edit', ['product' => $content['banner_slug']]) }}" class="action-edit | bg-[#f5f5f5] text-[#999] rounded animation animation-bounceInRight animation-delay-100 shadow-sm p-1 px-2 hover:text-[#0079c2]" title="Edit">
-                                <div class="icon-edit h-6 pt-0.5"><i class="ri-edit-box-fill"></i></div>
-                            </a>
-                            <button class="action-del | bg-[#f5f5f5] text-[#999] rounded animation animation-bounceInRight shadow-sm p-1 px-2 hover:text-[#0079c2]" data-content-name="{{ $content['banner_name'] }}" wire:click="dispatchModal({{ json_encode($content) }})" title="Delete">
-                                <div class="icon-del h-6 pt-0.5"><i class="ri-delete-bin-6-fill"></i></div>
-                            </button>
-                        </div>
+                        <x-nav-link href="{{ route('products.edit', ['product' => $content['banner_slug']]) }}" class="rounded-md h-9 w-9 cursor-pointer group hover:bg-light-gray-100">
+                            <x-icon class="mx-auto w-3 grayscale group-hover:grayscale-0" src="{{ asset('img/icons/icon-header-chevron-right.webp') }}"/>
+                        </x-nav-link>
                     </div>
-                    @php /*TODO: Add tooltip*/ @endphp
                 </div>
+                <x-button class="justify-center h-9 w-9 group bg-blacks hover:bg-tertiary" data-target-action="{{ $content['banner_slug'] . '-action' }}">
+                    <x-icon class="w-3 rotate-90 grayscale group-hover:grayscale-0" src="{{ asset('img/icons/icon-dots-vertical.webp') }}" action-icon-open />
+                    <x-icon class="w-2.5 hidden" src="{{ asset('img/icons/icon-header-close.webp') }}" action-icon-close />
+                </x-button>
             </td>
         </tr>
         @empty
