707306684345daf0521a7bc84d25efdb18f23c32
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-03-30 17:35:57 +0700

    feat: add icon style

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -1,1 +1,11 @@
-<img {{ $attributes->merge(['alt' => 'Icon']) }} loading="lazy">
+@props(['iconStyle' => 'none'])
+
+@php
+    $style = match ($iconStyle) {
+        'white'       => 'brightness-0 invert',
+        'hover-white' => 'group-hover:brightness-0 group-hover:invert',
+        'none'        => '',
+    }
+@endphp
+
+<img {{ $attributes->merge(['class' => $style, 'alt' => 'Icon']) }} loading="lazy">
