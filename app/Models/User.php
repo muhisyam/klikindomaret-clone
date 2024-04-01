3489bdfb44b124cc7a758753607d1fbf96857a60
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-04-01 22:40:01 +0700

    feat: add cart relations in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+
+    public function carts()
+    {
+        return $this->belongsToMany(Product::class, 'carts')->withPivot('quantity')->withTimestamps();
+    }
 }
