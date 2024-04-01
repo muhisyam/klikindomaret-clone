1ce14bab1c533528a848f50b06ee577b42f27a3f
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-04-01 22:37:03 +0700

    feat: refactoring client request code, add metadata resource

--- app/Actions/ClientRequestAction.php
+++ app/Actions/ClientRequestAction.php
@@ -5,50 +5,62 @@ namespace App\Actions;
 use App\DataTransferObjects\ClientRequestDto;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Support\Arr;
 
 class ClientRequestAction
 {
     /**
      * Create and send an HTTP request.
      *
-     * @param string              $method  HTTP method.
-     * @param string|UriInterface $uri     URI object or string.
-     * @param array               $param   Parameter from Request. See \GuzzleHttp\RequestOptions.
+     * @param ClientRequestDto $dto       Client param object transfer include below.
+     * @param string           $method    HTTP method.
+     * @param string           $endpoint  URI of endpoint.
+     * @param array            $header    Associative array of headers to add to the request.
+     * @param array            $formData  Parameter from Request. See \GuzzleHttp\RequestOptions.
      */
     public function request(ClientRequestDto $dto): array
     {
-        $options = [];
-        $client = new Client();
-
-        if (!empty($dto->headers)) {
-            $options = [
-                'headers' => $dto->headers,
-            ];
-        }
-
-        if (!empty($dto->formData)) {
-            $options = [
-                'headers' => $dto->headers,
-                // 'headers' => ['Accept-Type', 'application/json'],
-                'multipart' => $dto->formData,
-            ];
-        }
+        $client     = new Client();
+        $acceptJson = ['Accept' => 'application/json'];
+        $options    = [
+            'headers'   => array_merge($acceptJson, $dto->headers),
+            'multipart' => $dto->formData,
+        ];
 
         try {
             $response = $client->request($dto->method, $dto->endpoint, $options);
 
-            $content = $response->getBody()->getContents();
-            $data = json_decode($content, true);
-
-            return $data;
+            return $this->withMetadata($response);
 
         } catch (ClientException $exception) {
-            $response = $exception->getResponse()->getBody()->getContents();
-            
-            $error = json_decode($response, true);
-            $error['errors']['code'] = $exception->getCode();
-            
-            return $error;
+            $response = $exception->getResponse();
+
+            return $this->withMetadata($response);
         }
+    }
+
+    /**
+     * Add metadata resource to the response for convinience checking responses.
+     * 
+     * @param Response $response Response data from request.
+     * 
+     * @return array [data => [...], meta => [...]]
+    */
+    private function withMetadata(Response $response): array
+    {
+        $content    = json_decode($response->getBody()->getContents(), true);
+        $hasMessage = isset($content['message']);
+
+        if (! isset($content['meta'])) {
+            // If has $message, remove message and status code key from current response data
+            $meta['message']     = $hasMessage ? $content['message'] : $response->getReasonPhrase();
+            $meta['status_code'] = $response->getStatusCode();
+            $content             = $hasMessage ? Arr::except($content, ['message', 'status_code']) : $content;
+
+            return array_merge($content, ['meta' => $meta]);
+        }
+
+        return $content;
     }
 }
