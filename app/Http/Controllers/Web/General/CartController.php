48d19a7d49636a1de0ffe5fe993f7064480779c5
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-04-02 22:41:31 +0700

    feat: add cart web controller

---
+++ app/Http/Controllers/Web/General/CartController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Web\General;
+
+use App\Actions\ClientRequestAction;
+use App\Actions\CreateMultipartAction;
+use App\DataTransferObjects\ClientRequestDto;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CartRequest;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    protected string $endpoint;
+
+    public function __construct(
+        protected ClientRequestAction $clientAction,
+        protected CreateMultipartAction $multipartAction,
+    ) {
+        $this->endpoint = config('api.url') . 'carts';
+    }
+    
+    public function store(CartRequest|array $request): array
+    {
+        $dataRequest = $request instanceof CartRequest ? $request->all() : $request;
+        $formData    = $this->multipartAction->create($dataRequest);
+        
+        return $this->clientAction->request(
+            new ClientRequestDto(
+                method: 'POST',
+                endpoint: $this->endpoint,
+                formData: $formData,
+                headers: [
+                    'Authorization' => 'Bearer ' . session('auth_token')
+                ],
+            )
+        );
+    }
+}
