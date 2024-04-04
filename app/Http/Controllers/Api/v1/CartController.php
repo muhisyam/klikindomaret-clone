ae5361070d2fc67654f3c10af1b1f76aeb24dcad
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-04-04 22:02:37 +0700

    feat: add method show and update data cart in controller

--- app/Http/Controllers/Api/v1/CartController.php
+++ app/Http/Controllers/Api/v1/CartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CartRequest;
+use App\Models\Cart;
 use App\Traits\General\ShoppingCart;
 use Illuminate\Http\JsonResponse;
 
@@ -14,7 +15,24 @@ class CartController extends Controller
     public function store(CartRequest $request): JsonResponse
     {
         $this->addToCart($request);
-        $productName = ['product_name' => $this->product->name];
+
+        $productName = ['product_name' => $this->product['product_name']];
+
+        return response()->json(['data' => $productName], 200);
+    }
+
+    public function show(int $userId): JsonResponse
+    {
+        $carts = Cart::userProducts($userId);
+
+        return response()->json(['data' => $carts], 200);
+    }
+    
+    public function update(CartRequest $request, int $userId): JsonResponse
+    {
+        $this->updateProductQuantity($request);
+        
+        $productName = ['product_name' => $this->product['product_name']];
 
         return response()->json(['data' => $productName], 200);
     }
