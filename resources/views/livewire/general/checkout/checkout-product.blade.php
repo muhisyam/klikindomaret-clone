44757ae49f77c4f74c9e7d756f35bb2e1f8d120e
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-04-15 22:31:23 +0700

    fix: active selected datetime delivery

--- resources/views/livewire/general/checkout/checkout-product.blade.php
+++ resources/views/livewire/general/checkout/checkout-product.blade.php
@@ -26,64 +26,67 @@
         <tbody>
 
         @foreach ($productByGroup as $product)
-            
-            @if ($loop->first)
-                <tr>
-                    <td colspan="5" class="py-4 border-b border-light-gray-100">
-                        <x-dropdown section="user-account-ewallet-{{ Str::slug($retailerName) }}">
-                            <x-slot:trigger class="p-4 flex-col gap-2 w-full bg-light-gray-50">
-                                <div class="flex items-center gap-2 w-full" data-delivery-type="">
-                                    <x-icon class="mr-auto w-40" src="{{ asset('img/checkout/choose-time.webp') }}"/>
-                                    <div>Rp {{ formatCurrencyIDR(5000) }}</div>
-                                    <x-icon class="w-4 duration-500" src="{{ asset('img/icons/icon-header-chevron-down.webp') }}" data-arrow-dropdown=""/>
-                                </div>
-                                <div class="w-full text-left text-sm" data-delivery-info="">Hari ini, 07 April 2024, 16:00-16:59</div>
-                            </x-slot>
+
+        @php $retailerSlug = Str::slug($retailerName) @endphp
         
-                            <x-slot:content class="overflow-hidden drop-shadow-md w-full bg-white before:hidden">
-                                <x-button class="border-b border-light-gray-100 !rounded-none p-4 flex-col !items-baseline gap-2 w-full hover:bg-secondary-50">
-                                    <x-icon class="w-40" src="{{ asset('img/checkout/choose-reguler.webp') }}"/>
-                                    <div class="text-left text-sm">Tidak tersedia untuk pesanan dari penjual ini</div>
-                                </x-button>
+        @if ($loop->first)
+            <tr>
+                <td colspan="5" class="py-4 border-b border-light-gray-100">
+                    <x-dropdown section="user-account-ewallet-{{ $retailerSlug }}">
+                        <x-slot:trigger class="p-4 flex-col gap-2 w-full bg-light-gray-50">
+                            <div class="flex items-center gap-2 w-full" data-delivery-type="">
+                                <x-icon class="mr-auto w-40" src="{{ asset('img/checkout/choose-time.webp') }}"/>
+                                <div>Rp {{ formatCurrencyIDR(5000) }}</div>
+                                <x-icon class="w-4 duration-500" src="{{ asset('img/icons/icon-header-chevron-down.webp') }}" data-arrow-dropdown=""/>
+                            </div>
+                            <div class="w-full text-left text-sm" data-delivery-info="{{ $retailerSlug }}">Hari ini, 07 April 2024, 16:00-16:59</div>
+                        </x-slot>
+    
+                        <x-slot:content class="overflow-hidden drop-shadow-md w-full bg-white before:hidden">
+                            <x-button class="border-b border-light-gray-100 !rounded-none p-4 flex-col !items-baseline gap-2 w-full hover:bg-secondary-50">
+                                <x-icon class="w-40" src="{{ asset('img/checkout/choose-reguler.webp') }}"/>
+                                <div class="text-left text-sm">Tidak tersedia untuk pesanan dari penjual ini</div>
+                            </x-button>
 
-                                @php  $section = 'choose-time-' . Str::slug($retailerName) @endphp
+                            @php  $section = 'choose-time-' . $retailerSlug @endphp
 
-                                <x-modal :section="$section" withOverlay="false">
-                                    <x-slot:trigger class="border-b border-light-gray-100 !rounded-none p-4 flex-col !items-baseline gap-2 w-full hover:bg-secondary-50">
-                                        <x-icon class="w-40" src="{{ asset('img/checkout/choose-time.webp') }}"/>
-                                        <div class="text-left text-sm">Pilih sendiri waktu yang kamu mau</div>
-                                    </x-slot>
-                
-                                    <x-slot:content class="separated-modal">
-                                    @push('modal-date-delivery')
-                                        @include('general.checkout.modal-date-delivery', [
-                                            'section'      => $section,
-                                            'retailerName' => $retailerName,
-                                        ])
-                                    @endpush
-                                    </x-slot>
-                                </x-modal>
+                            <x-modal :section="$section" withOverlay="false">
+                                <x-slot:trigger class="border-b border-light-gray-100 !rounded-none p-4 flex-col !items-baseline gap-2 w-full hover:bg-secondary-50">
+                                    <x-icon class="w-40" src="{{ asset('img/checkout/choose-time.webp') }}"/>
+                                    <div class="text-left text-sm">Pilih sendiri waktu yang kamu mau</div>
+                                </x-slot>
+            
+                                <x-slot:content class="separated-modal">
+                                @push('modal-date-delivery')
+                                    @include('general.checkout.modal-date-delivery', [
+                                        'section'      => $section,
+                                        'retailerName' => $retailerName,
+                                        'retailerSlug' => $retailerSlug,
+                                    ])
+                                @endpush
+                                </x-slot>
+                            </x-modal>
 
-                                <x-button class="border-b border-light-gray-100 !rounded-none p-4 flex-col !items-baseline gap-2 w-full hover:bg-secondary-50">
-                                    <x-icon class="w-40" src="{{ asset('img/checkout/choose-sameday.webp') }}"/>
-                                    <div class="text-left text-sm">Tidak tersedia untuk pesanan dari penjual ini</div>
-                                </x-button>
+                            <x-button class="border-b border-light-gray-100 !rounded-none p-4 flex-col !items-baseline gap-2 w-full hover:bg-secondary-50">
+                                <x-icon class="w-40" src="{{ asset('img/checkout/choose-sameday.webp') }}"/>
+                                <div class="text-left text-sm">Tidak tersedia untuk pesanan dari penjual ini</div>
+                            </x-button>
 
-                                <x-button class="!rounded-none p-4 flex-col !items-baseline gap-2 w-full hover:bg-secondary-50">
-                                    <x-icon class="w-40" src="{{ asset('img/checkout/choose-express.webp') }}"/>
-                                    <div class="text-left text-sm">Pesanan dikirim maksimal 1 jam setelah pembayaran lunas</div>
-                                </x-button>
-                            </x-slot>
-                        </x-dropdown>   
-                    </td>
-                </tr>
-            @endif
+                            <x-button class="!rounded-none p-4 flex-col !items-baseline gap-2 w-full hover:bg-secondary-50">
+                                <x-icon class="w-40" src="{{ asset('img/checkout/choose-express.webp') }}"/>
+                                <div class="text-left text-sm">Pesanan dikirim maksimal 1 jam setelah pembayaran lunas</div>
+                            </x-button>
+                        </x-slot>
+                    </x-dropdown>   
+                </td>
+            </tr>
+        @endif
 
-            @php
-                $discountPercent    = round((($product['normal_price'] - $product['discount_price']) / $product['normal_price']) * 100);
-                $totalEachProduct   = $product['quantity'] * ($product['discount_price'] ?? $product['normal_price']);
-                $totalEachRetailer += $totalEachProduct;
-            @endphp
+        @php
+            $discountPercent    = round((($product['normal_price'] - $product['discount_price']) / $product['normal_price']) * 100);
+            $totalEachProduct   = $product['quantity'] * ($product['discount_price'] ?? $product['normal_price']);
+            $totalEachRetailer += $totalEachProduct;
+        @endphp
     
             <tr class="relative border-b border-light-gray-100 last:border-b-0">
                 <td class="py-4 ps-4 w-1/12">
@@ -179,7 +182,7 @@
                     toggleDropdown();
                     toggleModal();
                     switchDateDelivery();
-                    changeDateDeliveryInfo();
+                    updateDateDeliveryInfo();
                     pickDateDelivery();
                 }, 1);
             });
@@ -230,14 +233,19 @@
                 btnDate.addEventListener('click', () => {
                     const dataDate     = btnDate.getAttribute('data-delivery-date');
                     const dataTime     = btnDate.getAttribute('data-delivery-time');
-                    const dateDelivery = document.querySelector('#date-delivery-picker');
-                    const timeDelivery = document.querySelector('#time-delivery-picker');
+                    const dataRetailer = btnDate.closest('[data-retailer]').getAttribute('data-retailer');
+                    const dateDelivery = document.querySelector(`input[name*="date-delivery-picker"][data-retailer="${dataRetailer}"]`);
+                    const timeDelivery = document.querySelector(`input[name*="time-delivery-picker"][data-retailer="${dataRetailer}"]`);
 
-                    // Reset button style
+                    // Reset button style TODO: fix this
                     listBtnDatePicker.forEach(btnDate => {
-                        btnDate.classList.add('bg-white', 'text-secondary');
-                        btnDate.classList.remove('bg-secondary', 'text-white');
-                        btnDate.innerHTML  = 'Pilih jam ini';
+                        const isBtnHasRightRetailer = btnDate.closest('[data-retailer]').matches(`[data-retailer="${dataRetailer}"]`);
+
+                        if (isBtnHasRightRetailer) {
+                            btnDate.classList.add('bg-white', 'text-secondary');
+                            btnDate.classList.remove('bg-secondary', 'text-white');
+                            btnDate.innerHTML  = 'Pilih jam ini';
+                        }
                     })
 
                     dateDelivery.value = dataDate;
@@ -247,35 +255,64 @@
                     btnDate.classList.add('bg-secondary', 'text-white');
                     btnDate.classList.remove('bg-white', 'text-secondary');
 
-                    changeDateDeliveryInfo();
+                    updateDateDeliveryInfo();
                 })
             })
         }
 
-        function changeDateDeliveryInfo() {
-            const dateDelivery  = document.querySelector('#date-delivery-picker').value;
-            const timeDelivery  = document.querySelector('#time-delivery-picker').value;
-            const deliveryInfo  = document.querySelector('[data-delivery-info]');
-            const dateJS        = new Date();
-            const currentDay    = dateJS.getDate();
-            const deliveryDay   = dateDelivery.split(" ")[0];
-            const dayDifference = deliveryDay - currentDay;
-            let willBeDelivered = '';
-            
-            if (dayDifference >= '2') {
-                const weekday = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', "Jum'at",'Sabtu'];
+        function updateDateDeliveryInfo() {
+            const dateDeliveryList = document.querySelectorAll('input[name*="date-delivery-picker"]');
+            const timeDeliveryList = document.querySelectorAll('input[name*="time-delivery-picker"]');
+
+            dateDeliveryList.forEach((dateDelivery, index) => {
+                const dateValue     = dateDelivery.value;
+                const timeValue     = timeDeliveryList[index].value;
+                const dateJS        = new Date();
+                const currentDay    = dateJS.getDate();
+                const deliveryDay   = dateValue.split(" ")[0];
+                const dayDifference = deliveryDay - currentDay;
+                const dataRetailer  = dateDelivery.getAttribute('data-retailer');
+                const deliveryInfo  = document.querySelector(`[data-delivery-info="${dataRetailer}"]`);
+                let willBeDelivered = '';
                 
-                willBeDelivered = weekday[dateJS.getDay() + dayDifference];
-            
-            } else if (dayDifference == '1') {
-                willBeDelivered = 'Besok';
-            
-            } else {
-                willBeDelivered = 'Hari ini';
+                if (dayDifference >= '2') {
+                    const weekday = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', "Jum'at",'Sabtu'];
+                    
+                    willBeDelivered = weekday[dateJS.getDay() + dayDifference];
+                
+                } else if (dayDifference == '1') {
+                    willBeDelivered = 'Besok';
+                
+                } else {
+                    willBeDelivered = 'Hari ini';
 
-            }
+                }
 
-            deliveryInfo.innerHTML = `${willBeDelivered}, ${dateDelivery}, ${timeDelivery}`;
+                deliveryInfo.innerHTML = `${willBeDelivered}, ${dateValue}, ${timeValue}`;
+            })
+            // const dateDelivery  = document.querySelector('#date-delivery-picker').value;
+            // const timeDelivery  = document.querySelector('#time-delivery-picker').value;
+            // const deliveryInfo  = document.querySelector('[data-delivery-info=]');
+            // const dateJS        = new Date();
+            // const currentDay    = dateJS.getDate();
+            // const deliveryDay   = dateDelivery.split(" ")[0];
+            // const dayDifference = deliveryDay - currentDay;
+            // let willBeDelivered = '';
+            
+            // if (dayDifference >= '2') {
+            //     const weekday = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', "Jum'at",'Sabtu'];
+                
+            //     willBeDelivered = weekday[dateJS.getDay() + dayDifference];
+            
+            // } else if (dayDifference == '1') {
+            //     willBeDelivered = 'Besok';
+            
+            // } else {
+            //     willBeDelivered = 'Hari ini';
+
+            // }
+
+            // deliveryInfo.innerHTML = `${willBeDelivered}, ${dateDelivery}, ${timeDelivery}`;
         }
     </script>
 @endpush
