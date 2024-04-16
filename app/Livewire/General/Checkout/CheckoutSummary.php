f8d8335adc833c9508e2992fe35491232383864e
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-04-16 22:29:41 +0700

    feat: add new dispatch qty changed event

--- app/Livewire/General/Checkout/CheckoutSummary.php
+++ app/Livewire/General/Checkout/CheckoutSummary.php
@@ -7,9 +7,9 @@ use Livewire\Component;
 
 class CheckoutSummary extends Component
 {
-    public int $discountTotal;
-    public int $grandTotal;
-    public int $normalTotal;
+    public int $discountTotal = 0;
+    public int $grandTotal    = 0;
+    public int $normalTotal   = 0;
 
     #[On('content-loaded')]
     public function initSummary(array $summary)
@@ -19,6 +19,11 @@ class CheckoutSummary extends Component
         $this->normalTotal   = $this->discountTotal + $this->grandTotal;
     }
 
+    public function updateCart($quantityChanged)
+    {
+        $this->dispatch('qty-content-changed', quantityChanged: $quantityChanged);
+        $this->reset('discountTotal', 'grandTotal', 'normalTotal');
+    }
 
     public function render()
     {
