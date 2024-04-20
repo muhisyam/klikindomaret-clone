4e12613cedab462bce6ef87ddb474631a1d08a6c
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-04-20 22:50:01 +0700

    feat: add pickup method api controller

---
+++ app/Http/Controllers/Api/v1/PickupMethodController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Services\PickupMethodService;
+use Illuminate\Http\Request;
+
+class PickupMethodController extends Controller
+{
+    public function show(Request $request, PickupMethodService $pickupService, string $username)
+    {
+        $userPickupMethods = $request->user()->pickupMethod;
+        $detailAddress     = $pickupService->getPickupDetailAddress($userPickupMethods);
+
+        return $detailAddress;
+    }
+}
