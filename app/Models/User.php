df401035d7ada2d36d91da7c22fff2c47b63ed0d
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-04-20 22:48:35 +0700

    feat: add eloquent relation to pickup method table

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -53,8 +55,13 @@ class User extends Authenticatable
         return $this->belongsTo(Role::class);
     }
 
-    public function carts()
+    public function carts(): BelongsToMany
     {
         return $this->belongsToMany(Product::class, 'carts')->withPivot('quantity')->withTimestamps();
+    }
+
+    public function pickupMethod(): HasMany
+    {
+        return $this->hasMany(UserPickupMethod::class);
     }
 }
