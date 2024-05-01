859a9fba857e2b77bb3639a84fd0b7aa5041ecc7
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-05-01 23:16:03 +0700

    feat: add method call api payment complete handler

--- app/Http/Controllers/Web/General/GeneralController.php
+++ app/Http/Controllers/Web/General/GeneralController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers\Web\General;
 
+use App\Actions\ClientRequestAction;
+use App\Actions\CreateMultipartAction;
+use App\DataTransferObjects\ClientRequestDto;
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class GeneralController extends Controller
 {
+    protected string $endpoint;
+
+    public function __construct(
+        protected ClientRequestAction $clientAction,
+        protected CreateMultipartAction $multipartAction,
+    ) {
+        $this->endpoint = config('api.url') . 'carts';
+    }
+    
     public function homepage(): View
     {
         return view('general.home.index');
@@ -20,17 +32,34 @@ class GeneralController extends Controller
 
     public function checkout(): View
     {
+        $this->forgetPaymentSession();
+
+        return view('general.checkout.index');
+    }
+
+    public function paymented(array $request)
+    {
+        $formData = $this->multipartAction->create($request);
+
+        $this->forgetPaymentSession();
+        $this->clientAction->request(
+            new ClientRequestDto(
+                method: 'POST',
+                endpoint: $this->endpoint . 'complete',
+                formData: $formData,
+                headers: [
+                    'Authorization' => 'Bearer ' . session('auth_token')
+                ],
+            )
+        );
+    }
+
+    private function forgetPaymentSession()
+    {
         $username          = session('user')['username'];
         $sessionTokenKey   = $username . '-payment-token';
         $sessionPaymentKey = $username . '-payment-created';
 
         session()->forget([$sessionTokenKey, $sessionPaymentKey]);
-
-        return view('general.checkout.index');
-    }
-
-    public function paymented(Request $request)
-    {
-
     }
 }
