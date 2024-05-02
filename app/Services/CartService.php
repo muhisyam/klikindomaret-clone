09fd4a5cede4b47e0b9983c15f3e34af39a70e61
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-05-02 19:26:53 +0700

    refactor: change retailer to supplier

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -6,14 +6,16 @@ use Illuminate\Support\Arr;
 
 class CartService 
 {
+    //TODO: jadiin [product => ...products, detail => ...details]
+
     /**
      * Adding additional information including:
      * - product discount percent
      * - total price each product (*quantity)
-     * - total price each retailer
+     * - total price each supplier
      * - total discount
      * - grand total price
-     * - delivery options each retailer
+     * - delivery options each supplier
      * 
      * @param array $carts Data Model Cart
      * 
@@ -22,10 +24,10 @@ class CartService
     public function getMoreInformation(array $carts): array
     {
         $eachProductDiscount  = 0;
-        $totalEachRetailer    = 0;
+        $totalEachSupplier    = 0;
         $grandTotal           = 0;
         $defaultDeliveryOpt   = [];
-        $availDeliveryOptions = [//TODO: jadi gratis klo total sub tertentu
+        $availDeliveryOptions = [
             'regular' => [
                 'message' => 'Estimasi 2-3 hari',
                 'price'   => 5000,
@@ -44,9 +46,9 @@ class CartService
             ],
         ];
 
-        foreach ($carts as $retailerName => $productByGroup) {
-            $quantities[$retailerName] = [];
-            $totalEachRetailer         = 0;
+        foreach ($carts as $supplierName => $productByGroup) {
+            $quantities[$supplierName] = [];
+            $totalEachSupplier         = 0;
             
             foreach ($productByGroup as $index => $product) {
                 /**
@@ -55,27 +57,28 @@ class CartService
                  * product using product slug, the prop will be like 
                  * --> ['Warehouse' => ['product_slug' => 'quantity', etc...]]
                 */
-                $quantities[$retailerName] = array_merge($quantities[$retailerName], [$product['product_slug'] => $product['quantity']]);
+                $quantities[$supplierName] = array_merge($quantities[$supplierName], [$product['product_slug'] => $product['quantity']]);
                 $discountPercent           = round((($product['normal_price'] - $product['discount_price']) / $product['normal_price']) * 100);
                 $eachProductPrice          = $product['quantity'] * ($product['discount_price'] ?? $product['normal_price']);
                 $eachProductDiscount      += $product['quantity'] * ($product['normal_price'] - ($product['discount_price'] ?? $product['normal_price']));
-                $totalEachRetailer        += $eachProductPrice;
+                $totalEachSupplier        += $eachProductPrice;
                 
-                $carts[$retailerName][$index] = array_merge($carts[$retailerName][$index], [
+                $carts[$supplierName][$index] = array_merge($carts[$supplierName][$index], [
                     'discount_percent' => $discountPercent,
                     'total_price'      => $eachProductPrice,
                 ]);
             }
             
             $productCount              = count($productByGroup);
-            $retailerIcon              = $retailerName !== 'Toko Indomaret' ? $retailerName : 'Store';
-            $availDeliveryEachRetailer = $retailerName !== 'Toko Indomaret' ? Arr::except($availDeliveryOptions, 'express') : $availDeliveryOptions;
-            $deliveryFirstListType     = array_key_first($availDeliveryEachRetailer);
-            $deliveryFirstListPrice    = $availDeliveryEachRetailer[$deliveryFirstListType]['price'];
-            $deliveryFirstListMessage  = $availDeliveryEachRetailer[$deliveryFirstListType]['message'];
-            $grandTotal               += $totalEachRetailer;
+            $supplierIcon              = $supplierName !== 'Toko Indomaret' ? $supplierName : 'Store';
+            $availDeliveryOptions      = $this->getFreeDelivery($availDeliveryOptions, $totalEachSupplier);
+            $availDeliveryEachSupplier = $supplierName !== 'Toko Indomaret' ? Arr::except($availDeliveryOptions, 'express') : $availDeliveryOptions;
+            $deliveryFirstListType     = array_key_first($availDeliveryEachSupplier);
+            $deliveryFirstListPrice    = $availDeliveryEachSupplier[$deliveryFirstListType]['price'];
+            $deliveryFirstListMessage  = $availDeliveryEachSupplier[$deliveryFirstListType]['message'];
+            $grandTotal               += $totalEachSupplier;
             $defaultDeliveryOpt        = array_merge($defaultDeliveryOpt, [
-                $retailerName => [
+                $supplierName => [
                     'option'  => $deliveryFirstListType,
                     'price'   => $deliveryFirstListPrice,
                     'message' => $deliveryFirstListMessage,
@@ -83,21 +86,38 @@ class CartService
             ]);
 
 
-            $carts[$retailerName] = array_merge($carts[$retailerName], [
-                'retailer_icon'             => $retailerIcon,
-                'total_price_each_retailer' => $totalEachRetailer,
+            $carts[$supplierName] = array_merge($carts[$supplierName], [
+                'supplier_icon'             => $supplierIcon,
+                'total_price_each_supplier' => $totalEachSupplier,
                 'product_count'             => $productCount,
-                'delivery_options'          => $availDeliveryEachRetailer,
+                'delivery_options'          => $availDeliveryEachSupplier,
             ]);
         }
 
         $carts = array_merge($carts, [
             'default_delivery_option'   => $defaultDeliveryOpt,
-            'qty_product_each_retailer' => $quantities,
+            'qty_product_each_supplier' => $quantities,
             'total_product_discount'    => $eachProductDiscount,
             'grand_total'               => $grandTotal
         ]);
 
         return $carts;
+    }
+
+    private function getFreeDelivery(array $availDeliveryOptions, int $totalEachSupplier)
+    {
+        if ($totalEachSupplier >= 150000) {
+            $availDeliveryOptions['express']['price'] = 0;
+        }
+
+        if ($totalEachSupplier >= 100000) {
+            $availDeliveryOptions['sameday']['price'] = 0;
+        }
+
+        if ($totalEachSupplier >= 50000) {
+            $availDeliveryOptions['time']['price'] = 0;
+        }
+
+        return $availDeliveryOptions;
     }
 }
