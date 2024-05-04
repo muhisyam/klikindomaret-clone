d771e10ad5b776ffcfc1fac88ce4fe7ccfe9a0a9
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-05-04 22:47:29 +0700

    refactor: make param request mid accord data user and cart

--- app/Services/CheckoutService.php
+++ app/Services/CheckoutService.php
@@ -2,29 +2,35 @@
 
 namespace App\Services;
 
+use App\Http\Controllers\Api\v1\CartController;
+use App\Http\Controllers\Api\v1\PickupMethodController;
+use App\Models\Product;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Midtrans\Config;
 use Midtrans\Snap;
 
 class CheckoutService
 {
     protected User $user;
-    protected string $orderkey;
-    protected object $userAddress;
-    protected object $userPickupAddress;
+    protected string $orderKey;
+    protected array $userPickupAddress;
     protected array $userCarts;
+    protected array $productIds = [];
 
     public function getSnapToken(Request $request)
     {
+        $this->initGlobalData($request);
+
         Config::$serverKey    = config('midtrans.server_key');
         Config::$isProduction = config('midtrans.env');
         Config::$isSanitized  = config('midtrans.sanitized');
         Config::$is3ds        = config('midtrans.3ds');
 
-        $params = $this->setRequestBody();
+        $params = $this->createRequestBody();
 
         try {
             return Snap::getSnapToken($params);
@@ -33,51 +39,106 @@ class CheckoutService
         }
     }
 
-    private function setRequestBody()
+    /**
+     * Cant pass request from parent to constructor
+    */
+    private function initGlobalData(Request $request)
+    {
+        $this->user      = $request->user();
+        $this->orderKey  = ''; 
+        $this->userCarts = app(CartController::class)->getUserCart($request);
+        $pickupAddresses = app(PickupMethodController::class)->getUserPickupData($request);
+        $pickupAddresses = Arr::except($pickupAddresses, ['is_picked_up_in_store', 'pickup_icon']);
+        
+        /**
+         * Order key combination, consists of 3 digits of binary numbers. Each digit
+         * will be true when supplier exists in cart. First digit is supplier store, 
+         * second is warehouse, and last is seller.
+        */
+        $keySupplier  = '';
+        $keySupplier .= array_key_exists('Toko Indomaret', $this->userCarts) ? '1' : '0';
+        $keySupplier .= array_key_exists('Warehouse', $this->userCarts) ? '1' : '0';
+        $keySupplier .= array_key_exists('Seller', $this->userCarts) ? '1' : '0';
+        
+        /**
+         * One of order key combination. This value only "D" or "T". D is mean delivery to
+         * user address. T is mean taken, when user pickup products in store.
+        */
+        $keyMethod     = '';
+        $keyDate       = today()->format('ymd');
+        $keyUsername   = strtoupper($this->user['username']);
+
+        foreach ($pickupAddresses as $address) {
+            if ($address['is_selected_method']) {
+                $this->userPickupAddress = $address;
+                $keyMethod               = $address['last_pickup_with_retailer'] ? 'T' : 'D';
+            }
+        }
+
+        $this->orderKey = $keyUsername . '-' . $keySupplier . '-' . $keyMethod . $keyDate;
+    }
+
+    private function createRequestBody()
     {
         $params = [
             'transaction_details' => [
-                'order_id'     => 'DEMO-' . rand(),
-                'gross_amount' => 10000,
+                'order_id'     => $this->orderKey,
+                'gross_amount' => $this->userCarts['other_info']['grand_total'],
             ],
-            'customer_details' => [
-                'first_name' => 'budi',
-                'last_name'  => 'pratama',
-                'email'      => 'budi.pra@example.com',
-                'phone'      => '08111222333',
-                "billing_address"=> [
-                    "address"=> "Sudirman",
-                    "city"=> "Jakarta",
-                    "postal_code"=> "12190",
-                    "country_code"=> "IDN"
-                  ],
-                "shipping_address"=> [
-                    "first_name"=> "SHIPPING",
-                    "last_name"=> "MIDTRANSER",
-                    "phone"=> "0 8128-75 7-9338",
-                    "address"=> "Cempaka",
-                    "city"=> "Cilegon",
-                    "postal_code"=> "12190",
-                    "country_code"=> "IDN"
-                ],
-            ],
-            'item_details' => [
-                [
-                    "id"            => "ITEM1",
-                    "price"         => 2500,
-                    "quantity"      => 2,
-                    "name"          => "Midtrans Bear",
-                ],
-                [
-                    "id"            => "ITEM2",
-                    "price"         => 5000,
-                    "quantity"      => 1,
-                    "name"          => "Seller Bearbrand",
-                ],
-            ],
+            'customer_details' => $this->setDataCustomer(),
+            'item_details'     => $this->setDataItems(),
         ];
 
         return $params;
+    }
+
+    private function setDataCustomer()
+    {
+        $shippingDetail = $this->userPickupAddress['place_detail'];
+        $userFullname   = explode(' ', $this->user['fullname'], 2);
+        $recieverName   = explode(' ', $shippingDetail['reciever_name'], 2);
+        $dataCustomer   = [
+            'first_name' => $userFullname[0],
+            'last_name'  => $userFullname[1],
+            'email'      => $this->user['email'],
+            'phone'      => $this->user['mobile_number'],
+            'shipping_address' => [
+                'first_name'   => $recieverName[0],
+                'last_name'    => $recieverName[1],
+                'phone'        => $shippingDetail['reciever_phone_number'],
+                'address'      => $shippingDetail['place_address'],
+                'city'         => $shippingDetail['place_location'],
+                'postal_code'  => $shippingDetail['place_postal_code'],
+                'country_code' => 'IDN'
+            ],
+        ];
+
+        return $dataCustomer;
+    }
+
+    private function setDataItems()
+    {
+        $dataEachItem = [];
+        $carts        = Arr::except($this->userCarts, 'other_info');
+
+        foreach ($carts as $groupBySupplier) {
+            $productSlugs     = Arr::pluck($groupBySupplier['products'], 'product_slug');
+            $productIds       = Product::whereIn('product_slug', $productSlugs)->pluck('id')->toArray();
+            $this->productIds = array_merge($this->productIds, $productIds);
+
+            foreach ($groupBySupplier['products'] as $index => $product) {
+                $dataEachItem = array_merge($dataEachItem, [
+                    [
+                        "id"       => $productIds[$index],
+                        "price"    => $product['discount_price'] ?? $product['normal_price'],
+                        "quantity" => $product['quantity'],
+                        "name"     => $product['product_name'],
+                    ]
+                ]);
+            }
+        }
+
+        return $dataEachItem;
     }
 
     private function createDataUserOrder()
