19b3ed318a400f5db94646317be51e7f6ec260d1
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-05-08 22:22:06 +0700

    feat: add new method relationship table

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Order extends Model
 {
@@ -17,14 +17,16 @@ class Order extends Model
     ];
 
     public static $userStatus = [
-        'create'    => 'Dibuat',
-        'pending'   => 'Menunggu Pembayaran',
-        'process'   => 'Sedang Diproses',
-        'completed' => 'Selesai',
+        'create'     => 'Dibuat',
+        'expire'     => 'Kadaluwarsa',
+        'pending'    => 'Menunggu Pembayaran',
+        'settlement' => 'Sedang Diproses',
+        'completed'  => 'Selesai',
     ];
     
     public static $retailerStatus = [
-        'create'   => 'Masuk',
+        'create'   => 'Dibuat',
+        'incoming' => 'Masuk',
         'accept'   => 'Diterima',
         'delivery' => 'Dikirim',
         'ready'    => 'Siap',
@@ -32,12 +34,18 @@ class Order extends Model
     ];
 
     public static $retailerStatusMessage = [
-        'Incoming' => 'Pesanan Masuk',
-        'Accepted' => 'Pesanan diterima oleh',
-        'Delivered',
-        'Ready',
-        'Completed',
+        'Dibuat'   => 'Pesanan Dibuat',
+        'Masuk'    => 'Pesanan Masuk',
+        'Diterima' => 'Pesanan diterima oleh',
+        'Dikirim',
+        'Siap',
+        'Selesai',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function products(): BelongsToMany
     {
@@ -49,27 +57,13 @@ class Order extends Model
         return $this->belongsToMany(Retailer::class)->withTimestamps();
     }
 
-    public function retailerAddress(): MorphToMany
+    public function supplierDeliveries(): BelongsToMany
     {
-        return $this->morphedByMany(Retailer::class, 'order_pickup_address')->withTimestamps();
+        return $this->belongsToMany(Supplier::class, 'order_supplier_delivery')->withTimestamps();
     }
 
-    public function userAddress(): MorphToMany
+    public function pickupAddress(): MorphTo
     {
-        return $this->morphedByMany(UserAddress::class, 'order_pickup_address')->withTimestamps();
-    }
-
-    public function morphAddressTo(string $addressType): MorphToMany
-    {
-        return match ($addressType) {
-            'retailer'     => $this->retailerAddress(),
-            'user_address' => $this->userAddress(),
-        };
-    }
-    
-    public function detachMorphAddress(): void
-    {
-        $this->retailerAddress()->detach();
-        $this->userAddress()->detach();
+        return $this->morphTo();
     }
 }
