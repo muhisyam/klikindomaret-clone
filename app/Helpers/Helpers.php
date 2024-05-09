8bdfd1a14e012b55d51e3eb5398a60ce15ad5567
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-05-09 22:26:03 +0700

    feat: add helper method carbon parser

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -2,17 +2,17 @@
 
 use Carbon\Carbon;
 
-function formatNumber($price) 
+function formatNumber($price): string 
 {
     return number_format($price, 0, '.', '.');
 }
 
-function formatCurrencyIDR($price) 
+function formatCurrencyIDR($price): string
 {
     return number_format($price, 0, '.', '.');
 }
 
-function prettierMobileNumber($number)
+function prettierMobileNumber($number): string
 {
     $number = substr($number, 1);
 
@@ -34,4 +34,21 @@ function formatFullname(string $fullname): string
 function formatToIdnLocale(Carbon $datetime, string $formatDate = ''): string
 {
     return $datetime->locale('id')->settings(['formatFunction' => 'translatedFormat'])->format($formatDate);
+}
+
+function parseToCarbon(string $date, string $separator = ' '): Carbon 
+{
+    if (str_contains($date, '|')) {
+        $date = explode('|', $date)[0];
+    }
+
+    $monthInt = [
+        'Januari'   => 1, 'Februari' => 2,  'Maret'    => 3,  'April'    => 4, 
+        'Mei'       => 5, 'Juni'     => 6,  'Juli'     => 7,  'Agustus'  => 8, 
+        'September' => 9, 'Oktober'  => 10, 'November' => 11, 'Desember' => 12
+    ];
+
+    $arrDate = explode($separator, $date);
+
+    return Carbon::create($arrDate[2], $monthInt[$arrDate[1]], $arrDate[0]);
 }
