cff6e8da0998bef031677afda78b105308334e11
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-05-11 19:16:12 +0700

    feat: add api resource user order

--- app/Http/Resources/OrderUserResource.php
+++ app/Http/Resources/OrderUserResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Services\OrderService;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,19 +16,23 @@ class OrderUserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $orderService = app(OrderService::class);
+        $createdDate  = formatToIdnLocale(Carbon::parse($this->created_at), 'j M Y | H:i') . ' WIB';
+
         return [
-            'order_key'           => $this->order_key,
-            'user_order_status'   => $this->user_order_status,
-            'pickup_info'         => $this->pickup_info,
-            // 'pickup_address'   => new CategoryResource($this->whenLoaded('pickupAddress')),
-            'pickup_code'         => $this->pickup_code,
-            'pickup_expired'      => $this->pickup_expired,
-            'payment_channel'     => $this->payment_channel,
-            'va_number'           => $this->va_number,
-            'grandtotal'          => $this->grandtotal,
-            'order_completed'     => $this->order_completed,
-            // 'products'       => Product::collection($this->whenLoaded('products')),
-            // 'supplier_deliveries' => Product::collection($this->whenLoaded('supplierDeliveries')),
+            'order_key'         => $this->order_key,
+            'user_order_status' => $this->user_order_status,
+            'pickup_info'       => $this->pickup_info,
+            'pickup_code'       => $this->pickup_code,
+            'pickup_expired'    => $this->pickup_expired,
+            'payment_channel'   => $this->payment_channel,
+            'va_number'         => $this->va_number,
+            'grandtotal'        => $this->grandtotal,
+            'order_completed'   => $this->order_completed,
+            'product_count'     => $this->whenCounted('products'),
+            'created_at'        => $createdDate,
+            'status_icon'       => $orderService->getStatusIcon($this->user_order_status),
+            'products'          => OrderProductResource::collection($this->whenLoaded('products')),
         ];
     }
 
