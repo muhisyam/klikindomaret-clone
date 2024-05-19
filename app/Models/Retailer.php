002da3e1c84a4abef667b6e7471d89896a789b5c
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-05-19 20:56:51 +0700

    feat: add scope to get retailer order

--- app/Models/Retailer.php
+++ app/Models/Retailer.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Retailer extends Model
 {
@@ -19,13 +21,46 @@ class Retailer extends Model
         'latitude' => 0,
     ];
 
-    public function region()
+    public function region(): BelongsTo
     {
         return $this->belongsTo(Region::class);
     }
 
-    public function products()
+    public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class);
+    }
+
+    public function orders(): BelongsToMany
+    {
+        return $this->belongsToMany(Order::class, 'order_retailer', 'retailer_id', 'order_id')
+            ->withPivot('retailer_order_status', 'message')
+            ->withTimestamps();
+    }
+
+    public function scopeGetRetailerOrders($query, array $containerIds): object|null
+    {
+        $createOrder = Order::$retailerStatus['create'];
+        $supplierId  = $containerIds[0];
+        $retailerId  = $containerIds[1];
+        $result      = $this
+            ->orders()
+            ->with([
+                'user',
+                'pickupAddress',
+                'supplierDeliveries' => fn($delivery) => $delivery->whereIn('supplier_id', $supplierId),
+                'products' => function($products) use ($supplierId, $retailerId) {
+                    /**
+                     * If $retailerId null it means the user retailer is official store supplier, then it will return all
+                     * products from official store retailer. Else it will return all products that provide from the retailer.
+                    */
+                    return is_null($retailerId) 
+                        ? $products->whereIn('supplier_id', $supplierId) 
+                        : $products->whereHas('retailers', fn ($retailers) => $retailers->where('retailer_id', $retailerId));
+                }, 
+            ])
+            ->whereNot('retailer_order_status', $createOrder);
+
+        return $result;
     }
 }
