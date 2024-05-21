3d6033e3e862e344a066983708f0cbf8402ecfde
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-05-21 22:43:22 +0700

    feat: add order filter table

---
+++ app/Livewire/Admin/Order/FilterTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Order;
+
+use Livewire\Component;
+
+class FilterTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.order.filter-table');
+    }
+}
