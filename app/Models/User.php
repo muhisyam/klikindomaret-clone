48368e45585c27aa56c350851ad7832a4e4aacc7
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-05-22 00:04:55 +0700

    feat: add route key, default attribute value, static gender,  user retailer relation eager

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -14,20 +16,15 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $guarded = [];
+
     /**
-     * The attributes that are mass assignable.
+     * The model's default values for attributes.
      *
-     * @var array<int, string>
+     * @var array<string, string>
      */
-    protected $fillable = [
-        'fullname',
-        'username',
-        'email',
-        'password',
-        'gender',
-        'birthdate',
-        'mobile_number',
-        'last_login',
+    protected $attributes = [
+        'role_id' => 1,
     ];
 
     /**
@@ -41,27 +38,44 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast.
+     * Enum value of user gender.
      *
      * @var array<string, string>
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'mobile_number_verified_at' => 'datetime',
+    public static $gender = [
+        'men'   => 'laki-laki',
+        'women' => 'peremnpuan',
     ];
 
-    public function roleAs()
+    /**
+     * Override the route key use other database column for the model class.
+     */
+    public function getRouteKeyName(): string
+    {
+        return 'username';
+    }
+
+    // All relations of user model.
+
+    public function roleAs(): BelongsTo
     {
         return $this->belongsTo(Role::class);
     }
 
     public function carts(): BelongsToMany
     {
-        return $this->belongsToMany(Product::class, 'carts')->withPivot('quantity')->withTimestamps();
+        return $this->belongsToMany(Product::class, 'carts')
+            ->withPivot('quantity')
+            ->withTimestamps();
     }
 
     public function pickupMethod(): HasMany
     {
         return $this->hasMany(UserPickupMethod::class)->orderBy('updated_at', 'desc');;
+    }
+
+    public function retailer(): HasOne
+    {
+        return $this->hasOne(Retailer::class);
     }
 }
