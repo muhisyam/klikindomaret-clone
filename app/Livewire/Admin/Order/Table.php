52abd8937ea5e0a908fc1fe56d10fe63064b7c17
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-05-27 00:45:54 +0700

    refactor: move the logic of fetching data to the web controller

--- app/Livewire/Admin/Order/Table.php
+++ app/Livewire/Admin/Order/Table.php
@@ -2,26 +2,11 @@
 
 namespace App\Livewire\Admin\Order;
 
-use App\Actions\ClientRequestAction;
-use App\DataTransferObjects\ClientRequestDto;
+use App\Http\Controllers\Web\Admin\OrderController;
 use Livewire\Component;
 
 class Table extends Component
 {
-    /**
-     * Api client request handler
-     *
-     * @var object
-     */
-    protected object $clientAction;
-
-    /**
-     * Api endpoint
-     *
-     * @var string
-     */
-    protected string $endpoint;
-    
     /**
      * Order data instance.
      * 
@@ -35,37 +20,12 @@ class Table extends Component
      * @var string
      */
     public string $sortBy, $orderBy;
- 
-    /**
-     * Create a new resource instance.
-     *
-     * @return void
-     */
-    public function __construct(
-    ) {
-        $this->clientAction = app(ClientRequestAction::class);
-        $this->endpoint = config('api.url') . 'orders/admin/';
-    }
 
     public function loadContent()
     {
-        $this->orders = $this->getDataRetailerOrders()['data'];
-    }
+        $this->orders = app(OrderController::class)->getDataRetailerOrders()['data'];
 
-    private function getDataRetailerOrders()
-    {
-        $username  = session('user')['username'];
-        $userToken = session('auth_token');
-
-        return $this->clientAction->request(
-            new ClientRequestDto(
-                method: 'GET',
-                endpoint: $this->endpoint . $username,
-                headers: [
-                    'Authorization' => 'Bearer ' . $userToken
-                ],
-            )
-        );
+        $this->dispatch('content-loaded');
     }
 
     public function render()
