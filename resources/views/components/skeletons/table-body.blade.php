1a9d60cd89996dcb1d8532fa203e9a74b7db025b
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-05-27 00:47:22 +0700

    feat: add table body skeleton component

---
+++ resources/views/components/skeletons/table-body.blade.php
@@ -0,0 +1,15 @@
+@props(['rows', 'cols', 'height' => '36px'])
+
+@for ($i = 0; $i < $rows; $i++)
+            
+<tr class="border-b">
+    
+    @for ($j = 0; $j < $cols; $j++)
+    
+    <td class="py-2 px-3 h-[{{ $height }}]"><div class="rounded-lg h-3 w-full bg-light-gray-100 animate-pulse"></div></td>
+
+    @endfor
+
+</tr>
+
+@endfor
