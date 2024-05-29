61b7350e4af8c22c26f92b08bef01ae077f36a7f
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-05-29 19:03:59 +0700

    feat: add name prefix route api

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -31,6 +31,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
+                ->name('api.')
                 ->prefix('api/v1')
                 ->group(base_path('routes/api.php'));
 
