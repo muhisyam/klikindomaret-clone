1996bc052fda114fb5d3c183bda79cb40562ad78
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-05-30 19:09:43 +0700

    refactor: change entire category api controller

--- app/Http/Controllers/Api/v1/CategoryController.php
+++ app/Http/Controllers/Api/v1/CategoryController.php
@@ -2,135 +2,123 @@
 
 namespace App\Http\Controllers\Api\v1;
 
-use App\Actions\IsCategoryParentAction;
-use App\DataTransferObjects\FindDataDto;
+use App\Enums\MetaStatus;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CategoryRequest;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
-use App\Services\Backend\ApiCallService;
 use App\Services\Backend\ImageService;
+use App\Services\CategoryService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CategoryController extends Controller
 {
-    public function __construct(
-        protected ApiCallService $apiService,
-        protected ImageService $imageService,
-        protected IsCategoryParentAction $isParentAction,
-    ) {}
-
+    /**
+     * Retrieving categories data with request filter parameters, including:
+     * - (int)    depth    => between 2 - 3 level
+     * - (string) sortBy   => sort by category column
+     * - (string) orderBy  => asc or desc
+     * - (bool)   paginate => Get data in paginate or not
+     * - (int)    perPage  => Limit data pagination
+     * 
+     * @param \Illuminate\Http\Request $request
+    */
     public function index(Request $request): JsonResource
     {
-        // TODO: Sync like product controller
-        $query = Category::where('parent_id', null)->withCount('children'); 
-        $query = $request->depth === '3' ? $query->with('children.children') : $query;
-        $categories = $request->has('withoutPagination') ? $query->get() : $query->paginate(10);
+        $categories = Category::query()
+            ->where('parent_id', null)
+            ->withCount('children')
+            ->filterModel($request)
+            ->getData($request)
+            ->withPath(urlRouteApp('categories.index'));
 
-        return CategoryResource::collection($categories);
-    }
-
-    public function subIndex(Request $request, string $category_slug): JsonResource
-    {  
-        // TODO: Simplying this
-        $parent = Category::where('category_slug', $category_slug)->first();
-        $query = Category::where('parent_id', $parent->id)
-            ->with('children')
-            ->withCount('children');
-         
-        $categories = $request->has('withoutPagination') ? $query->get() : $query->paginate(5);
-
-        return CategoryResource::collection($categories);
-    }
-
-    public function store(CategoryRequest $request): CategoryResource
-    {
-        $category = new Category();
-        $data = $request->validated();
-        $data['parent_id'] = $category->setParentId($data['parent_id']);
-
-        if (isset($data['category_image'])) {
-            $category->category_image_name = $this->imageService->storeImage($data['category_image'], 'categories');
-            $category->original_category_image_name = $this->imageService->storeImageName($data['category_image']);
-        }
-
-        $category->fill($data);
-        $category->save();
-
-        return new CategoryResource($category);
-    }
-
-    public function show(string $category_slug): CategoryResource
-    {
-        $category = $this->apiService->findData(
-            new FindDataDto(
-                model: new Category(),
-                whereSchema: [
-                    ['category_slug', $category_slug],
-                ],
-                withSchema: ['parent'],
-            )
-        );
-
-        return new CategoryResource($category);
-    }
-
-    public function update(CategoryRequest $request, string $category_slug): CategoryResource
-    {
-        $category = $this->getSpesificData($category_slug);
-        $data = $request->validated();
-        $data['parent_id'] = $category->setParentId($data['parent_id']);
-        $data['category_image'] = $category->setImageValue($data);
-        
-        if (isset($data['category_image']) || isset($data['delete_image'])) {
-            $this->imageService->deleteExistsImage($category->category_image_name, 'categories');
-            $category->category_image_name = $this->imageService->storeImage($data['category_image'], 'categories');
-            $category->original_category_image_name = $this->imageService->storeImageName($data['category_image']);
-        }
-
-        $category->fill($data);
-        $category->save();
-
-        return new CategoryResource($category);
-    }
-
-    public function destroy(string $category_slug): JsonResponse
-    {
-        $category = $this->getSpesificData($category_slug);
-        $categoryName = ['category_name' => $category->category_name];
-        
-        $category->delete();
-        $this->imageService->deleteExistsImage($category, 'categories');
-
-        return response()->json(['data' => $categoryName], 200);
+        return CategoryResource::collection($categories)->additional(MetaStatus::get('OK'));
     }
 
     /**
-     * Get top level category with/without chilren by request and without pagination
-     */
-    // TODO: Pindah ke index
-    public function getTopLevelWithChildren(bool $withChildren): JsonResource
-    {
-        $categories = Category::where('parent_id', null)
-            ->when($withChildren, function ($query) {
-                $query->with('children');
-            })
-            ->get();
+     * Retrieving children categories data for admin interface.
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Category $category
+    */
+    public function indexChildren(Request $request, Category $category): JsonResource
+    {  
+        $categories = $category
+            ->children()
+            ->with('children')
+            ->withCount('children')
+            ->getData($request);
 
-        return CategoryResource::collection($categories);
+        return CategoryResource::collection($categories)->additional(MetaStatus::get('OK'));
     }
 
-    private function getSpesificData(string $category_slug)
+    /**
+     * Store new category data.
+     * 
+     * @param \App\Http\Requests\CategoryRequest $request
+     * @param \App\Services\CategoryService $categoryService
+    */
+    public function store(CategoryRequest $request, CategoryService $categoryService): CategoryResource
     {
-        return $this->apiService->findData(
-            new FindDataDto(
-                model: new Category(),
-                whereSchema: [
-                    ['category_slug', $category_slug],
-                ],
-            )
+        $formData  = $request->validated();
+        $formData  = $categoryService->setNecessaryData($formData, ['parent_id', 'category_image_name']);
+        $category  = Category::create($formData);
+        $addtional = array_merge(MetaStatus::get('CREATED'), [
+            'data' => [
+                'content_name' => $category->category_name
+            ]
+        ]);
+
+        return (new CategoryResource($category))->additional($addtional);
+    }
+
+    /**
+     * Get spesific data category by route model binding.
+     * 
+     * @param \App\Models\Category $category
+    */
+    public function show(Category $category): CategoryResource
+    {
+        return (new CategoryResource($category->load('parent')))->additional(MetaStatus::get('OK'));
+    }
+
+    /**
+     * Update spesific data category by route model binding.
+     * 
+     * @param \App\Http\Requests\CategoryRequest $request
+     * @param \App\Models\Category $category
+     * @param \App\Services\CategoryService $categoryService
+    */
+    public function update(CategoryRequest $request, Category $category, CategoryService $categoryService): JsonResponse
+    {
+        $contentName = ['data' => ['content_name' => $category->category_name]];
+        $formData    = $request->validated();
+        $formData    = $categoryService->setNecessaryData(
+            formData: $formData, 
+            category: $category,
+            column:   ['parent_id', 'set_image_value', 'category_image_name'],
         );
+
+        $category->update($formData);
+
+        return response()->json(array_merge($contentName, MetaStatus::get('OK')), 200);
+    }
+
+    /**
+     * Delete spesific data category by route model binding and remove the image if exists.
+     * 
+     * @param \App\Models\Category $category
+     * @param \App\Services\Backend\ImageService $imageService
+    */
+    public function destroy(Category $category, ImageService $imageService): JsonResponse
+    {
+        $contentName = ['data' => ['content_name' => $category->category_name]];
+        
+        $imageService->deleteExistsImage($category, 'categories');
+        $category->delete();
+
+        return response()->json(array_merge($contentName, MetaStatus::get('OK')), 200);
     }
 }
