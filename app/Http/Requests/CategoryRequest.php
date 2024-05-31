21339012e9ca508919474745c217b5074e9c8244
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-05-31 19:17:59 +0700

    refactor: change template category request

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Actions\ErrorTraceAction;
+use App\Enums\DeployStatus;
 use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class CategoryRequest extends FormRequest
 {
@@ -23,32 +26,49 @@ class CategoryRequest extends FormRequest
      */
     public function rules(): array
     {
-        if ($this->isMethod('post')) {
-            return [
-                'parent_id' => ['required', 'numeric'],
-                'category_name' => ['required', 'max:100'],
-                'category_slug' => ['required', 'unique:categories', 'max:200'],
-                'category_status' => ['required'],
-                'category_image' => ['image', 'mimes:jpg,png,jpeg', 'max:512'],
-            ];
+        $rules = [
+            'parent_id'              => ['required', 'numeric'],
+            'category_name'          => ['required', 'string', 'max:100'],
+            'category_slug'          => ['required', 'string', 'max:200'],
+            'category_deploy_status' => ['required', Rule::enum(DeployStatus::class)],
+            'category_image_name'    => ['image', 'mimes:jpg,png,jpeg', 'max:512'],
+        ];
+
+        switch ($this->method()) {
+            case 'POST':
+                $rules['category_slug'] = array_merge($rules['category_slug'], ['unique:categories,category_slug']);
+                break;
+            
+            case 'PUT':
+                $rules = array_merge($rules, ['delete_category_image' => ['nullable']]);
+                break;
         }
 
-        if ($this->isMethod('put')) {
-            return [
-                'parent_id' => ['required', 'numeric'],
-                'category_name' => ['required', 'max:100'],
-                'category_slug' => ['required', 'max:200'],
-                'category_status' => ['required'],
-                'category_image' => ['image', 'mimes:jpg,png,jpeg', 'max:512'],
-                'delete_image' => ['nullable'],
-            ];
-        }
+        return $rules;
     }
 
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  Validator $validator
+     * @return void
+     *
+     * @throws HttpResponseException
+     */
     protected function failedValidation(Validator $validator)
     {
+        $trace = app(ErrorTraceAction::class)->execute();
+        
         throw new HttpResponseException(response([
-            "errors" => $validator->getMessageBag()
+            'errors' => $validator->getMessageBag(),
+            'meta'   => [
+                'status_code' => 400,
+                'message'     => 'Bad Request',
+                'trace'       => [
+                    'File' => $trace['filename'],
+                    'Line' => $trace['line'],
+                ],
+            ],
         ], 400));
     }
 }
