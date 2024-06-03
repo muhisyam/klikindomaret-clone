230c1fdbf68417b5db5c55b6ba13d3d0cd45b35e
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-06-03 19:46:01 +0700

    refactor: remove intial class modal component

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -4,12 +4,18 @@
     $overlay = $withOverlay ? 'overlay = ""' : '';
 @endphp
 
+{{-- 
+    Modal slot content component always has element with class modal. If the modal content position conflict 
+    with outermost parent position (relative), use class "separated-modal" in slot content, then push the 
+    content to "components" in layout component.
+--}}
+
 <div {{ $attributes->class(['active' => $showCondition])->merge(['class' => 'relative']) }} data-modal="{{ $section }}" {!! $overlay !!}>
     <x-button :class="$trigger->attributes['class']" :buttonStyle="$trigger->attributes['buttonStyle']" :data-target-modal="$section">
         {{ $trigger }}
     </x-button>
 
-    <div {{ $content->attributes->class(['show' => $showCondition])->merge(['class' => 'modal']) }} data-trigger-modal={{ $section }}>
+    <div {{ $content->attributes->class(['show' => $showCondition]) }}>
         {{ $content }}
     </div>
 </div>
