af3959ed0928796d5054efd8f7e5e903f09f2c7e
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-06-08 17:09:51 +0700

    feat: add category modal input

---
+++ app/Livewire/Admin/Category/Index/ModalInput.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Livewire\Admin\Category\Index;
+
+use App\Http\Controllers\Web\Admin\CategoryController;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class ModalInput extends Component
+{
+    use WithFileUploads;
+    
+    /**
+     * Modal section name.
+     *
+     * @var string
+    */
+    public string $section;
+
+    /**
+     * Modal show condition.
+     *
+     * @var bool
+     */
+    public bool $showCondition;
+
+    /**
+     * Instance of data input error when input has error.
+    */
+    public array $error = ['errors' => []];
+
+    /**
+     * Data category for option select parent category.
+    */
+    public array $categoryOption = [];
+    
+    // All category data according to the existing columns for input fields.
+    public int $parent_id                 = 0;
+    public string $category_name          = '';
+    public string $category_slug          = '';
+    public string $category_deploy_status = 'Draft';
+
+    /**
+     * Pay attention to the variable data type below, it must be either 
+     * "\Livewire\Features\SupportFileUploads\TemporaryUploadedFile" or unset. When 
+     * using the "Temporary" type, this variable cannot be debugged from dd() helper, 
+     * updated method or reset. Therefore, it is important to adjust according to 
+     * the module's requirements.
+    */
+    public $category_image_name = '';
+
+    /**
+     * Fetch data category in format select resource for input select parent category.
+    */
+    public function fetchDataOptionSelect(): void
+    {
+        $this->categoryOption = app(CategoryController::class)->getDataCategories(
+            extendedUrl: 'index/minimal?minimal=true',
+            header: [],
+        )['data'];
+
+        $this->dispatch('content-option-loaded');
+
+        // This statement will prevent the modal from opening automatically after executing 
+        // the request to fetch option data.
+        $this->showCondition = false;
+    }
+
+    /**
+     * This method will prevent modal from opening automatically when a component is created.
+    */
+    public function mount()
+    {
+        $this->showCondition = false;
+    }
+
+    /**
+     * This method will always open the modal on every request, so essentially prevents the modal 
+     * from closing when there is an unexpected request event.
+    */
+    public function boot()
+    {
+        $this->showCondition = true;
+    }
+
+    /**
+     * listen when @var category_name value had changes.
+    */
+    public function updatedCategoryName(): void
+    {
+        $this->category_slug = Str::slug($this->category_name);
+    }
+
+    /**
+     * Store form category data through API request.
+    */
+    public function store()
+    {
+        $formData = [
+            'parent_id'              => $this->parent_id,
+            'category_name'          => $this->category_name,
+            'category_slug'          => $this->category_slug,
+            'category_deploy_status' => $this->category_deploy_status,
+        ];
+
+        if (! empty($this->category_image_name)) {
+            $formData['category_image_name'] = $this->category_image_name;
+        }
+
+        $response = app(CategoryController::class)->postDataRequest($formData);
+
+        if ($response['meta']['status_code'] != 201) {
+            return $this->error = $response;
+        }
+
+        $this->showCondition = false;
+
+        $this->reset(
+            'parent_id', 
+            'category_name', 
+            'category_slug', 
+            'category_deploy_status',
+            'category_image_name', 
+        );
+
+        $this->dispatch('content-stored');
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.category.index.modal-input');
+    }
+}
---
+++ resources/views/livewire/admin/category/index/modal-input.blade.php
@@ -0,0 +1,111 @@
+<div class="modal w-96 bg-white rounded-xl{{ $showCondition ? ' show' : '' }}" data-trigger-modal="{{ $section }}" wire:init="fetchDataOptionSelect">
+    <section class="p-3 border-b border-light-gray-100 flex items-center justify-center">
+        <x-button class="absolute top-0 left-0 h-12 w-12" data-target-modal="{{ $section }}">
+            <x-icon class="m-auto w-3" src="{{ asset('img/icons/icon-header-arrow-left.webp') }}"/>
+        </x-button>
+
+        <x-icon class="w-24" src="{{ asset('img/header/logo.png') }}"/>
+
+        <span class="!absolute top-2.5 right-4 inline-flex loader-spin" wire:loading></span>
+    </section>
+    <section class="p-4">
+        <form wire:submit="store">
+            <div class="mb-4">
+                <x-input-label for="form-select-parent" value="Kategori Induk"/>
+                <x-input-select id="form-select-parent" name="parent_id" wire:model.change="parent_id">
+                    <option value="0" @selected(0 == $parent_id)>Kategori Level 0|Induk Kategori</option>
+
+                @foreach ($categoryOption as $option)
+                    <option value="{{ $option['category_id'] }}" @selected($option['category_id'] == $parent_id)>{{ $option['category_level'] . '|' .  $option['category_name']}}</option>
+                @endforeach
+
+                </x-input-select>
+                <x-input-error field="parent_id" :error="$error"/>
+            </div>
+            <div class="mb-4">
+                <x-input-label for="form-input-category-name" value="Nama Kategori"/>
+                <x-input-field id="form-input-category-name" name="category_name" :error="$error" wire:model.blur="category_name"/>
+                <x-input-error field="category_name" :error="$error"/>
+            </div>
+            <div class="mb-4">
+                <x-input-label for="form-input-category-slug" value="Slug Kategori"/>
+                <x-input-field id="form-input-category-slug" name="category_slug" :error="$error" wire:model="category_slug"/>
+                <x-input-error field="category_slug" :error="$error"/>
+            </div>
+            <div class="mb-4">
+                <x-input-label for="form-select-category-status" value="Status"/>
+                <x-input-select id="form-select-category-status" name="category_deploy_status" wire:model.change="category_deploy_status">
+                
+                @foreach (array_diff(\App\Enums\DeployStatus::values(), ['Expired']) as $status)
+                    <option value="{{ $status }}">{{ $status }}</option>
+                @endforeach
+                
+                </x-input-select>
+            </div>
+            <div class="mb-4" wire:ignore>
+                <x-input-label for="form-input-image" value="Gambar Kategori(optional)"/>
+                <x-input-image-simple id="form-input-image" type="file" accept=".jpg, .jpeg, .png, .webp" name="category_image_name" :error="$error" wire:model="category_image_name"/>
+                <x-input-error field="category_image_name" :error="$error"/>
+            </div>
+            <x-button type="submit" class="justify-center h-10 px-8 text-sm" buttonStyle="secondary" value="Tambah"/>
+        </form>
+    </section>
+</div>
+
+@push('scripts')
+<script>
+    document.addEventListener('livewire:initialized', () => {
+        Livewire.on('content-option-loaded', event => {
+            setTimeout(() => {
+                initSelect2();
+            }, 1);
+        });
+    });
+
+    function initSelect2() { 
+        // Jquery for convert purpose(✌ ͡• ₃ ͡•)✌
+        $('#form-select-parent').select2({
+            templateResult: formatCategoryDropdown,
+            templateSelection: formatCategorySelection,
+            width: '100%',
+        });
+
+        $('#form-select-parent').parent().attr('wire:ignore', '');
+
+        $('#form-select-parent').on('change', function(e) {
+            Livewire
+                .getByName("admin.category.index.modal-input")[0]
+                .set('parent_id', $(this).val());
+        });
+        // Thanks for the tolerance(👍 ͡• ₃ ͡•)👍
+
+        /**
+         * Template result dropdown option.
+         * 
+         * @param {object} data
+        */
+        function formatCategoryDropdown(data) { 
+            if (data.loading) return data.text;
+
+            return formatCategorySelection(data);
+        }
+
+        /**
+         * Template result selected option.
+         * 
+         * @param {object} data
+        */
+        function formatCategorySelection(data) { 
+            let textSplit = data.text.split('|');
+
+            return $(
+                `<div class="pe-1 flex gap-1.5 text-sm" prevent-close="">
+                    <span>${textSplit[0]}</span>
+                    <span>|</span>
+                    <span class="font-bold italic">${textSplit[1]}</span>
+                </div>`
+            );
+        }
+    }
+</script>
+@endpush
