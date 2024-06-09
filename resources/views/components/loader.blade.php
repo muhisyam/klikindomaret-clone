5099f77092b3aae82d16c2c532e39fa1778b4acc
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-06-09 20:16:24 +0700

    refactor: loader attribute element

--- resources/views/components/loader.blade.php
+++ resources/views/components/loader.blade.php
@@ -1,3 +1,3 @@
-<div id="page-loader" class="fixed left-0 top-0 z-[80] !my-0 h-full w-full bg-black/50 hidden">
+<div {{ $attributes->merge(['id' => 'page-loader', 'class' => 'fixed left-0 top-0 z-[80] !my-0 h-full w-full bg-black/50 hidden']) }}>
     <img class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 rounded-full loader-pulse" src="{{ asset('img/icons/icon-loader-domar.webp') }}">
 </div>
