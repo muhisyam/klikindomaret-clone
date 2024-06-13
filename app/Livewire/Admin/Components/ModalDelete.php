03ac1d4f50006ce8f844343d673b8925dcdc4e2e
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-06-13 15:51:20 +0700

    refactor: change entire logic, blade modal delete

--- app/Livewire/Admin/Components/ModalDelete.php
+++ app/Livewire/Admin/Components/ModalDelete.php
@@ -2,50 +2,142 @@
 
 namespace App\Livewire\Admin\Components;
 
+use App\Http\Responses\ClientErrorResponse;
 use Livewire\Component;
 use Livewire\Attributes\On;
+use Livewire\Features\SupportEvents\Event;
 
 class ModalDelete extends Component
 {
-    public $dataModal = [];
-    
-    public $showModal = '';
-    public $checkbox = false;
+    /**
+     * Modal section name.
+     *
+     * @var string
+    */
+    public string $section = 'modal-delete';
 
-    public $catalogLists = [
-        'category_name' => 'category',
-        'product_name' => 'product',
-        'featured_name' => 'featured',
+    /**
+     * Modal show condition.
+     *
+     * @var bool
+     */
+    public bool $showCondition = false;
+
+    /**
+     * Checkbox checked.
+     *
+     * @var bool
+    */
+    public bool $checkbox = false;
+
+    /**
+     * List model controller name.
+     * 
+     * @var array
+    */
+    public array $modelController = [
+        'category' => 'Category',
+        'product'  => 'Product',
+        'featured' => 'Featured',
     ];
-    
-    public $deleteRouteLists = [
-        'category' => 'categories.destroy',
-        'product' => 'products.destroy',
-        'featured' => 'featured-contents.destroy',
-    ]; 
 
-    public $catalog;
-    public $deleteRoute;
+    /**
+     * List catalog name in indonesian.
+     * 
+     * @var array
+    */
+    public array $catalogList = [
+        'category' => 'Kategori',
+        'product'  => 'Produk',
+        'featured' => 'Konten Unggulan',
+    ];
 
-    #[On('modal-delete')] 
-    public function addModalInfo($data)
+    /**
+     * Model name instance.
+     * 
+     * @var string
+    */
+    public string $model = '';
+
+    /**
+     * Catalog name instance.
+     * 
+     * @var string
+    */
+    public string $catalogName = '';
+
+    /**
+     * Content name instance.
+     * 
+     * @var string
+    */
+    public string $contentName = '';
+
+    /**
+     * Content slug instance.
+     * 
+     * @var string
+    */
+    public string $contentSlug = '';
+
+    /**
+     * Set show condition in false when component first created.
+    */
+    public function mount(): void
     {
-        foreach ($data as $key => $value) {
-            if (array_key_exists($key, $this->catalogLists)) {
-                $this->catalog = $this->catalogLists[$key];
-                $this->deleteRoute = $this->deleteRouteLists[$this->catalog];
-            } 
-        }
-
-        $this->reset('checkbox');
-        $this->dataModal = $data;
-        $this->showModal = 'show';
+        $this->showCondition = false;
     }
 
-    public function closeModal()
+    /**
+     * Reinitialized checkbox and show condition, every request.
+    */
+    public function boot(): void
     {
-        $this->showModal = '';
         $this->checkbox = false;
+        $this->showCondition = true;
+    }
+
+    /**
+     * Set global props.
+     * 
+     * @param array $data Data from component that providing the event.
+    */
+    #[On('modal-delete')] 
+    public function addModalInfo(array $data): void
+    {
+        $this->model       = $this->modelController[$data['catalog_model']];
+        $this->catalogName = $this->catalogList[$data['catalog_model']];
+        $this->contentName = $data['content_name'];
+        $this->contentSlug = $data['content_slug'];
+    }
+
+    /**
+     * Call delete request and validate response status code.
+    */
+    public function delete(): ClientErrorResponse|Event
+    {
+        // Set local properties, then call request delete from catalog controller.
+        $controller = $this->getModelWebControllerClass($this->model);
+        $response   = app($controller)->deleteDataRequest($this->contentSlug);
+
+        if ($response['meta']['status_code'] >= 400) {
+            return new ClientErrorResponse($response);
+        }
+
+        $this->showCondition = false;
+
+        return $this->dispatch('content-deleted', notif: [
+            'title'   => 'Hapus ' . $this->catalogName,
+            'message' => $response['data']['content_name'],
+        ]);
+    }
+
+    /**
+     * Create path of model controller.
+    */
+    private function getModelWebControllerClass(string $model): string
+    {
+        return 'App\\Http\\Controllers\\Web\\Admin\\' . $model . 'Controller';
     }
     
     public function render()
--- resources/views/livewire/admin/components/modal-delete.blade.php
+++ resources/views/livewire/admin/components/modal-delete.blade.php
@@ -1,60 +1,40 @@
-<div class="modal-delete fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[60] modal {{ $showModal }}" data-trigger-modal>
-    <div class="modal-wrapper min-w-[500px] bg-white rounded">
-        <section class="header-section flex items-center justify-between border-b border-[#eee] p-2">
-            <div class="left ms-2">
-                <figure class="media w-24">
-                    <img class="rounded-md" src="{{ asset('img/header/logo.png') }}" alt="">
-                </figure>
-            </div>
-            <div class="right min-w-[50px]">
-                <button id="header-button-close" class="block rounded p-1 px-2 ml-auto hover:bg-[#fbde7e] hover:text-[#0079c2]" wire:click="closeModal">
-                    <div class="icon h-6 pt-0.5"><i class="ri-close-fill"></i></div>
-                </button>
-            </div>
-        </section>
-        <section class="body-section flex flex-col items-center pb-4">
-            <figure class="media w-52">
-                <img src="{{ url('https://img.freepik.com/premium-vector/flat-design-no-data-illustration_23-2150527115.jpg?w=740') }}" alt="">
-            </figure>
-            <h2 class="text-lg font-bold mb-2">Delete <span class="text-red-700">"{{ !empty($dataModal) ? $dataModal[$catalog . '_name'] : '' }}"</span> {{ $catalog }}?</h2>
-            <div class="text-sm text-center">
-                <p>Are you sure want to delete? You can't undo this action.</p>
-                <div class="input-group flex items-center justify-center gap-1">
-                    <input id="form-input-delete-checkbox" type="checkbox" wire:model.live="checkbox">
-                    <label for="form-input-delete-checkbox">Sure, i want to delete this.</label>
-                </div>
-            </div>
-        </section>
-        <section class="footer-section pt-2 pb-6">
-            <div class="flex justify-center gap-2">
-                <button id="footer-button-close" class="w-40 border border-[#0079C2] text-[#0079C2] rounded py-2 hover:bg-[#0079c2] hover:text-white" wire:click="closeModal">Close</button>
-                @if ($checkbox)
-                <form action="{{ route($deleteRoute, [$catalog => !empty($dataModal) ? $dataModal[$catalog . '_slug'] : '']) }}" method="POST">
-                    @csrf
-                    @method('delete')
+<div class="modal rounded-xl min-w-[500px] bg-white{{ $showCondition ? ' show' : '' }}" data-trigger-modal="{{ $section }}" data-trigger-modal>
+    <section class="p-2 border-b border-light-gray-100 flex items-center justify-center">
+        <x-icon class="ms-2 w-24" src="{{ asset('img/header/logo.png') }}"/>
     
-                    <button type="submit" class="w-40 bg-[#C33] border border-[#C33] text-white rounded py-2">Delete</button>
-                </form>
-                @else
-                <div class="w-40 bg-[#c93636] border border-[#C3] rounded opacity-40 py-1.5">
-                    <div class="loader-spin mx-auto"></div>
-                </div>
-                @endif
+        <x-button class="ml-auto p-2 h-7 w-7 group hover:bg-tertiary" data-target-modal="{{ $section }}">
+            <x-icon class="h-3 grayscale group-hover:grayscale-0" src="{{ asset('img/icons/icon-header-close.webp') }}"/>
+        </x-button>
+
+        <span class="!absolute top-2 right-2 inline-flex loader-spin" wire:loading></span>
+    </section>
+    <section class="pb-6 flex flex-col items-center">
+        <img class="w-52" src="{{ url('https://img.freepik.com/premium-vector/flat-design-no-data-illustration_23-2150527115.jpg?w=740') }}" alt="">
+        <div class="mb-2 text-lg font-bold">
+            Hapus {{ $catalogName }} <span class="text-danger">"{{ $contentName }}"</span>?
+        </div>
+        <div class="mb-6 text-sm text-center">
+            <p>Yakin ingin menghapus? Tindakan ini tidak dapat dibatalkan</p>
+            <div class="flex items-center justify-center gap-1">
+                <input id="form-input-delete-checkbox" type="checkbox" wire:model.live="checkbox">
+                <label for="form-input-delete-checkbox">Tentu, saya ingin menghapus ini.</label>
             </div>
-        </section>
-    </div>
+        </div>
+        <div class="flex justify-center gap-2">
+            <x-button class="w-40 justify-center" data-target-modal="{{ $section }}" buttonStyle="outline-secondary" value="Close"/>
+
+        @if ($checkbox)
+
+            <x-button class="py-2 w-40 justify-center" buttonStyle="danger" value="Delete" wire:click="delete"/>
+                
+        @else
+            
+            <div class="rounded-md py-1.5 border border-danger w-40 bg-danger opacity-40 ">
+                <div class="loader-spin mx-auto"></div>
+            </div>
+
+        @endif
+
+        </div>
+    </section>
 </div>
-
-@push('scripts')
-    <script>
-        window.addEventListener('livewire:init', () => {
-            Livewire.on('modal-show', (event) => {
-                const modalTarget = document.querySelector('.modal-delete');
-                const overlay = document.querySelector('#bg-overlay');
-
-                // Show the overlay bg again
-                openModal(modalTarget, overlay);
-            });
-        });
-    </script>
-@endpush
