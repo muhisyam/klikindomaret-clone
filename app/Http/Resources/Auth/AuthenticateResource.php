158ae39c7b3db3badd1d106ceb83c5886ee2b2c6
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-06-20 20:42:46 +0700

    feat: add some essential data for auth session

--- app/Http/Resources/Auth/AuthenticateResource.php
+++ app/Http/Resources/Auth/AuthenticateResource.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Resources\Auth;
 
-use App\Http\Resources\UserResource;
+use App\Services\PickupMethodService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -17,17 +17,68 @@ class AuthenticateResource extends JsonResource
     {
         return [
             'token' => $this->token,
-            'user' => new UserResource($this),
+            'user'  => [
+                'fullname'        => $this->fullname,
+                'username'        => $this->username,
+                'email'           => $this->email,
+                'mobile_number'   => $this->mobile_number,
+                'user_image_name' => $this->user_image_name,
+                'role'            => $this->whenLoaded('roleAs', $this->getRole()),
+                'retailer'        => $this->whenLoaded('retailer', $this->getRetailer()),
+                'pickup_address'  => $this->whenLoaded('pickupMethod', $this->getPickupDetailAddress()),
+            ],
         ];
     }
-    
-    public function with(Request $request): array
+
+    private function getRole(): array
     {
         return [
-            'meta' => [
-                'status_code' => 200,
-                'message' => 'Success',
+            'role_id'          => $this->roleAs->id,
+            'role_name'        => $this->roleAs->role_name,
+            'has_admin_access' => $this->roleAs->admin_access,
+        ];
+    }
+
+    private function getRetailer(): array
+    {
+        if (is_null($this->retailer)) {
+            return [];
+        }
+
+        return [
+            'retailer_id'   => $this->retailer->id,
+            'retailer_name' => $this->retailer->retailer_name,
+            'supplier'      => [
+                'supplier_id'   => $this->retailer->supplier->id,
+                'supplier_name' => $this->retailer->supplier->supplier_name
             ],
+        ];
+    }
+
+    /**
+     * Retrieves detailed of selected pickup address information based on the pickup method.
+    */
+    private function getPickupDetailAddress(): array
+    {
+        if ($this->pickupMethod->isEmpty()) {
+            return [];
+        }
+
+        $detailAddress = app(PickupMethodService::class)->getPickupDetailAddress($this->pickupMethod);
+        $detailAddress = $detailAddress
+            ->filter(function ($address) {
+                if (! isset($address->is_selected_method)) {
+                    return false;
+                }
+
+                return $address->is_selected_method;
+            })
+            ->first();
+
+        return [
+            'place_name'        => $detailAddress['place_detail']['place_name'],
+            'place_address'     => $detailAddress['place_detail']['place_address'],
+            'place_postal_code' => $detailAddress['place_detail']['place_postal_code'],
         ];
     }
 }
