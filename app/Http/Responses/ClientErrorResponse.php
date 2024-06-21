2e6a9991215b7df0f81302e398cde41d756eafbf
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-06-21 20:46:15 +0700

    refactor: remove implement in client error response

--- app/Http/Responses/ClientErrorResponse.php
+++ app/Http/Responses/ClientErrorResponse.php
@@ -2,18 +2,16 @@
 
 namespace App\Http\Responses;
 
-use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Http\RedirectResponse;
 
-class ClientErrorResponse implements Responsable
+class ClientErrorResponse
 {
-    public function __construct(
-        protected array $response,
-    ) {}
-
-    public function toResponse($request)
+    public function toResponse($response): RedirectResponse
     {
-        return match ($this->response['meta']['status_code']) {
+        return match ($response['meta']['status_code']) {
             404 => redirect()->route('error.404'),
+            403 => redirect()->route('error.403'),
+            401 => redirect()->route('homepage', ['auth' => 'login']),
         };
     }
 }
