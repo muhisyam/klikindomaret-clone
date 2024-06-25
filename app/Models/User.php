7f75fcbfb6e7635e2af1fa7ab170ee9fd0a66f49
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-06-25 21:18:52 +0700

    fix: user role relation foreign key

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,7 +59,7 @@ class User extends Authenticatable
 
     public function roleAs(): BelongsTo
     {
-        return $this->belongsTo(Role::class);
+        return $this->belongsTo(Role::class, 'role_id');
     }
 
     public function carts(): BelongsToMany
