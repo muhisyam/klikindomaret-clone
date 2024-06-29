6ac8d6a6b2a1712795f9bd105bfe6bfc8e958eee
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-06-29 21:40:27 +0700

    feat: add invalid class to input

--- resources/views/components/input-field.blade.php
+++ resources/views/components/input-field.blade.php
@@ -9,4 +9,4 @@
     $isError = isset($error['errors'][$attributes['name']]);
 @endphp
 
-<input {{ $attributes->class(['border-light-gray-50' => ! $isError, 'border-red-600' => $isError])->merge(['class' => 'h-10 w-full rounded-md border border-light-gray-50 py-2 px-3 text-sm text-black' . $classes, 'type' => 'text']) }}>
+<input {{ $attributes->class(['border-light-gray-50' => ! $isError, 'border-red-600 is-invalid' => $isError])->merge(['class' => 'h-10 w-full rounded-md border border-light-gray-50 py-2 px-3 text-sm text-black' . $classes, 'type' => 'text']) }}>
--- resources/views/components/input-select.blade.php
+++ resources/views/components/input-select.blade.php
@@ -9,6 +9,6 @@
     $isError = array_key_exists($attributes['name'], is_array($error) ? $error['errors'] : ['errors' => []]);
 @endphp
 
-<select {{ $attributes->class(['border-light-gray-50' => ! $isError, 'border-red-600' => $isError])->merge(['class' => 'h-10 w-full rounded-md border border-light-gray-50 py-2 px-3 text-sm text-black' . $classes]) }}>
+<select {{ $attributes->class(['border-light-gray-50' => ! $isError, 'border-red-600 is-invalid' => $isError])->merge(['class' => 'h-10 w-full rounded-md border border-light-gray-50 py-2 px-3 text-sm text-black' . $classes]) }}>
     {{ $slot }}
 </select>
