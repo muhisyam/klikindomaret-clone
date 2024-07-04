3a5f463c927a268e34069db74943228716fbcbe4
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-07-04 13:29:08 +0700

    feat: add controller official store filter pipeline

---
+++ app/Filters/HomepageSearch/OfficialStoreFilter.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace App\Filters\HomepageSearch;
+
+use App\Http\Resources\SearchFilterOfficialStoreResource;
+use App\Models\OfficialStore;
+use Closure;
+
+class OfficialStoreFilter
+{
+    function handle(array $result, Closure $next)
+    {
+        $search         = request('search');
+        $officialStores = OfficialStore::where('store_name', 'LIKE', "%{$search}%")->get();
+
+        $result['official_stores'] = SearchFilterOfficialStoreResource::collection($officialStores);
+
+        return $next($result);
+    }
+}  
