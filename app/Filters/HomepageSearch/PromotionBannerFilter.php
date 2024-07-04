06af656267b86d777da78db69347cffe43a6ae5b
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-07-04 13:29:20 +0700

    feat: add controller promo banner filter pipeline

---
+++ app/Filters/HomepageSearch/PromotionBannerFilter.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace App\Filters\HomepageSearch;
+
+use App\Enums\DeployStatus;
+use App\Http\Resources\SearchFilterPromotionBannerResource;
+use App\Models\PromotionBanner;
+use Closure;
+
+class PromotionBannerFilter
+{
+    function handle(array $result, Closure $next)
+    {
+        $search = request('search');
+        $promos = PromotionBanner::query()
+            // ->where('banner_deploy_status', DeployStatus::PUBLISHED->value)
+            ->whereHas('keywords', function ($query) use ($search) {
+                $query
+                    ->where('keyword_deploy_status', DeployStatus::PUBLISHED->value)
+                    ->where('keyword_name', 'LIKE', "%{$search}%");
+            })
+            ->take(5)
+            ->get();
+        
+        $result['banners'] = SearchFilterPromotionBannerResource::collection($promos);
+
+        return $next($result);
+    }
+}
