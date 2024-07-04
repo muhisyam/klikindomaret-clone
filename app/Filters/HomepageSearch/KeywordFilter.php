7d26c6fa75bd6cf726729a0abf818ff3d0059f46
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-07-04 13:28:52 +0700

    feat: add controller keyword filter pipeline

---
+++ app/Filters/HomepageSearch/KeywordFilter.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace App\Filters\HomepageSearch;
+
+use App\Enums\DeployStatus;
+use App\Http\Resources\SearchFilterKeywordResource;
+use App\Models\MetaKeyword;
+use Closure;
+
+class KeywordFilter
+{
+    function handle(array $result, Closure $next)
+    {
+        $search   = request('search');
+        $keywords = MetaKeyword::query()
+            ->where('keyword_deploy_status', DeployStatus::PUBLISHED->value)
+            ->where('keyword_name', 'LIKE', "%{$search}%")
+            ->take(5)
+            ->get();
+        
+        $result['keywords'] = SearchFilterKeywordResource::collection($keywords);
+
+        return $next($result);
+    }
+}
