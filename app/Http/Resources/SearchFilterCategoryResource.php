cd5537ebce997da5831b0dde4ee10179f5e8dcc3
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-07-05 13:30:05 +0700

    feat: add api resource category filter

---
+++ app/Http/Resources/SearchFilterCategoryResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SearchFilterCategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $keywordName = $this->whenLoaded('keywords', $this->keywords[0]->keyword_name, '');
+
+        return [
+            'keyword_name'  => $keywordName,
+            'category_name' => $this->category_name,
+            'category_slug' => $this->category_slug,
+        ];
+    }
+}
