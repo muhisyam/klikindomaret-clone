3bba211c39d56e859bc3eae52b1f174f1056cb51
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-07-09 20:59:47 +0700

    feat: add filter search product method

--- app/Http/Controllers/Api/v1/SearchController.php
+++ app/Http/Controllers/Api/v1/SearchController.php
@@ -2,18 +2,23 @@
 
 namespace App\Http\Controllers\Api\v1;
 
+use App\Enums\DeployStatus;
+use App\Enums\MetaStatus;
 use App\Filters\HomepageSearch\CategoryFilter;
 use App\Filters\HomepageSearch\KeywordFilter;
 use App\Filters\HomepageSearch\OfficialStoreFilter;
 use App\Filters\HomepageSearch\PromotionBannerFilter;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CardProductMinimalResource;
+use App\Models\Product;
+use Illuminate\Http\Request;
 use Illuminate\Pipeline\Pipeline;
 
 class SearchController extends Controller
 {
-    public function __invoke(Pipeline $pipeline)
+    public function navbar(Pipeline $pipeline)
     {
-        $search = request('search');
+        $search = request('key');
         $result = [
             'banners'         => [],
             'keywords'        => [],
@@ -21,7 +26,7 @@ class SearchController extends Controller
             'official_stores' => [],
         ];
 
-        if (strlen($search) < 4) {
+        if (strlen($search) < 3) {
             return $result;
         }
         
@@ -36,5 +41,63 @@ class SearchController extends Controller
             ->send($result)
             ->through($pipeThrough)
             ->thenReturn();
+    }
+
+    public function product(Request $request)
+    {
+        $search    = $request->key;
+        $baseQuery = Product::query()
+            ->leftJoin('categories', 'products.category_id', '=', 'categories.id')
+            ->leftJoin('brands', 'products.brand_id', '=', 'brands.id')
+            ->leftJoin('suppliers', 'products.supplier_id', '=', 'suppliers.id')
+            ->where(function($query) use ($search) {
+                $query
+                    ->where('product_name', 'LIKE', "%$search%")
+                    ->orWhereHas('keywords', function ($keywords) use ($search) {
+                        $keywords
+                            ->where('keyword_deploy_status', DeployStatus::PUBLISHED->value)
+                            ->where('keyword_name', 'LIKE', "%{$search}%");
+                    });
+            });
+        
+        $queryProduct = clone $baseQuery; 
+
+        $categoriesListFilter = $queryProduct
+            ->select('categories.category_name', 'categories.category_slug')
+            ->distinct()
+            ->get();
+
+        $brandsListFilter = $queryProduct
+            ->select('brands.brand_name', 'brands.brand_slug')
+            ->distinct()
+            ->get();
+
+        $suppliersListFilter = $queryProduct
+            ->select('suppliers.supplier_name', 'suppliers.flag_name')
+            ->distinct()
+            ->get();
+
+        $additional = array_merge(MetaStatus::get('OK'), [
+            'list_categories' => $categoriesListFilter,
+            'list_brands'     => $brandsListFilter,
+            'list_suppliers'  => $suppliersListFilter,
+        ]);
+
+        $resultProducts = $queryProduct
+            ->select(
+                'products.*',
+                'categories.category_slug',
+                'brands.brand_slug',
+                'suppliers.flag_name',
+            )
+            ->filterModel($request)
+            ->with([
+                'images',
+                'supplier',
+                'category',
+            ])
+            ->getData($request);
+
+        return CardProductMinimalResource::collection($resultProducts)->additional($additional);
     }
 }
