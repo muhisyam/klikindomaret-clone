51bcae10102ec7fa144a6bcabfd873ee31a0e8df
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-07-12 21:29:37 +0700

    feat: add highlighted and loader

--- resources/views/livewire/general/home/search.blade.php
+++ resources/views/livewire/general/home/search.blade.php
@@ -2,18 +2,22 @@
     'relative search-bar w-[39rem]', 
     'open before:!-z-10' => ! $isAllEmpty
 ])>
-    <form action="{{ route('home.search', ['key' => $search_key]) }}" @class([
+    <form action="{{ route('home.search', ['key' => $key]) }}" @class([
         'overflow-hidden border flex items-center gap-2 bg-white',
-        'before:absolute before:z-10 before:bottom-1 before:h-2.5 before:w-[99.7%] before:bg-white'                 => ! $isAllEmpty,
-        'after:absolute after:z-10 after:bottom-2 after:ml-4 after:h-[1px] after:w-[94.5%] after:bg-light-gray-100' => ! $isAllEmpty,
+        'before:absolute before:z-10 before:-bottom-1 before:h-2.5 before:w-[99.7%] before:bg-white'                 => ! $isAllEmpty,
+        'after:absolute after:z-10 after:bottom-0 after:ml-4 after:h-[1px] after:w-[94.5%] after:bg-light-gray-100' => ! $isAllEmpty,
         'shadow-input rounded-t-md rounded-x-md border-secondary !border-b-white'                                   => ! $isAllEmpty,
         'rounded-md border-white'                                                                                   => $isAllEmpty,
     ])>
-        <input type="search" class="py-1.5 px-4 w-full text-sm" name="search_key" placeholder="Mau beli apa hari ini?" wire:model.live.debounce.250ms="search_key">
-        <button type="submit" class="mr-4 rounded py-1 px-3 bg-tertiary">
+        <input type="search" class="py-1.5 px-4 w-full text-sm" name="key" placeholder="Mau beli apa hari ini?" wire:model.live.debounce.250ms="key">
+        <button type="submit" class="mr-4 rounded py-1 px-3 bg-tertiary" wire:loading.class="hidden">
             <x-icon class="w-4" src="{{ asset('img/icons/icon-header-search.webp') }}"/>
         </button>
+        <button type="submit" class="mr-4 rounded py-[7px] px-[15px] bg-tertiary hidden" wire:loading.class.remove="hidden">
+            <div class="loader-bullet-flash"></div>
+        </button>
     </form>
+    
     <div @class([
         'absolute overflow-auto shadow-input rounded-b-md border-x border-b border-secondary max-h-[520px] w-full bg-white pb-2',
         'hidden' => $isAllEmpty,
@@ -28,7 +32,7 @@
                 @foreach ($result['banners'] as $banner)
 
                     <div class="swiper-slide">
-                        <a href="{{ $banner['banner_redirect_url'] ?? route('page.banner', ['banner' => $banner['banner_slug']]) }}" data-banner-name="{{ $banner['banner_name'] }}" >
+                        <a href="{{ $banner['banner_redirect_url'] ?? route('page.promo', ['promo' => $banner['banner_slug']]) }}" data-banner-name="{{ $banner['banner_name'] }}" >
                             <img class="rounded" src="{{ asset('img/uploads/promo-banners/' . $banner['banner_image_name']) }}" alt="{{ $banner['banner_name'] }}">
                         </a>
                     </div>
@@ -62,7 +66,9 @@
             @foreach ($result['keywords'] as $keyword)
 
                 <li>
-                    <x-nav-link href="{{ route('home.search', ['key' => $search_key]) }}" class="py-1 px-4 hover:bg-secondary-50" :value="$keyword['keyword_name']"/>
+                    <x-nav-link href="{{ route('home.search', ['key' => $keyword['keyword_name']]) }}" class="py-1 px-4 hover:bg-secondary-50">
+                        {!! $keyword['highlighted'] !!}
+                    </x-nav-link>
                 </li>
 
             @endforeach
@@ -86,9 +92,9 @@
             @foreach ($result['categories'] as $keyword)
 
                 <li>
-                    <a href="{{ route('home.search', ['key' => $search_key, 'category' => $keyword['category_name']]) }}" class="block py-1 px-4 hover:bg-secondary-50">
-                        <div>{{ $keyword['keyword_name'] }}</div>
-                        <h4>di Kategori <span class="text-primary-600">{{ $keyword['category_name'] }}</span></div>
+                    <a href="{{ route('home.search', ['key' => $keyword['keyword_name'], 'categories' => $keyword['category_name']]) }}" class="py-1 px-4 block hover:bg-secondary-50">
+                        <div>{!! $keyword['highlighted'] !!}</div>
+                        <div>di Kategori <span class="text-primary-600">{{ $keyword['category_name'] }}</span></div>
                     </a>
                 </li>
 
@@ -117,7 +123,7 @@
                         <figure class="rounded border border-light-gray-100 h-16 group-hover:border-light-gray-200">
                             <img class="mx-auto h-full w-14 object-cover" src="{{ asset('img/uploads/official-stores/' . $store['store_image_name']) }}" alt="Official store image">
                         </figure>
-                        <h4 class="min-h-[40px] text-center text-sm line-clamp-2">{{ $store['store_name'] }}</h4>
+                        <h4 class="min-h-[40px] text-center text-sm line-clamp-2">{!! $store['store_name'] !!}</h4>
                     </a>
                 </li>
 
@@ -133,7 +139,8 @@
 
 @push('scripts')
 <script>
-    const searchInput = document.querySelector('[name="search_key"]');
+    const searchInput = document.querySelector('[name="key"]');
+
     searchInput.addEventListener('focus', () => {
         const formElement = event.target.parentNode;
 
