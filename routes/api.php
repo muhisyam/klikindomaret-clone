e4bbb98810222d622c4938fcc59843ee81ac788e
mhisyam25 <muhammadhisyam25031@gmail.com>
2024-07-13 21:56:39 +0700

    feat: add new web and api routes

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,42 @@
 <?php
 
 use App\Http\Controllers\Api\v1\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Api\v1\Auth\RegisteredController;
+use App\Http\Controllers\Api\v1\Auth\VerifiedMobileController;
+use App\Http\Controllers\Api\v1\CartController;
 use App\Http\Controllers\Api\v1\CategoryController;
+use App\Http\Controllers\Api\v1\CheckoutController;
+use App\Http\Controllers\Api\v1\FeaturedSectionController;
+use App\Http\Controllers\Api\v1\MinimalBrandController;
+use App\Http\Controllers\Api\v1\MinimalCategoryController;
+use App\Http\Controllers\Api\v1\MinimalRetailerController;
+use App\Http\Controllers\Api\v1\MinimalSupplierController;
+use App\Http\Controllers\Api\v1\OrderController;
+use App\Http\Controllers\Api\v1\PickupMethodController;
 use App\Http\Controllers\Api\v1\ProductController;
-use App\Http\Controllers\Api\v1\ProductImageController;
+use App\Http\Controllers\Api\v1\PromotionBannerController;
 use App\Http\Controllers\Api\v1\RegionController;
-use App\Http\Controllers\Api\v1\StoreController;
+use App\Http\Controllers\Api\v1\RetailerController;
+use App\Http\Controllers\Api\v1\SearchController;
 use App\Http\Controllers\Api\v1\SupplierController;
-use App\Http\Controllers\Api\v1\UserController;
 use App\Http\Controllers\DebuggingController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes Debug
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::get('refresh', [DebuggingController::class, 'apiRefresh'])->name('apiDebug');
+Route::get('refresh2', [DebuggingController::class, 'apiRefresh2'])->name('apiDebug2');
+Route::get('check-auth', [DebuggingController::class, 'apiCheckAuth'])->middleware('auth:sanctum');
+
 
 /*
 |--------------------------------------------------------------------------
@@ -24,17 +50,51 @@ use Illuminate\Support\Facades\Route;
 */
     
 Route::middleware('guest')->group(function() {
-    Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login');
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    Route::post('verify-mobile', [VerifiedMobileController::class, 'verifyMobile']);
+    Route::post('verify-otp', [VerifiedMobileController::class, 'verifyOtp']);
+    Route::post('register', [RegisteredController::class, 'store']);
 });
 
-// group categories
 Route::apiResource('categories', CategoryController::class);
-Route::group(['prefix' => 'categories'], function() {
-    Route::get('/sub/{category}', [CategoryController::class, 'subIndex'])->name('category.subIndex');
-    Route::get('/top-children/{category}', [CategoryController::class, 'getTopLevelWithChildren'])->name('category.topChildren');
-});
+Route::get('categories/sub/{category}', [CategoryController::class, 'indexChildren'])->name('categories.children');
+
 Route::apiResource('regions', RegionController::class);
 Route::apiResource('suppliers', SupplierController::class);
-Route::apiResource('stores', StoreController::class);
+Route::apiResource('retailers', RetailerController::class);
+Route::apiResource('featured-sections', FeaturedSectionController::class);
+Route::apiResource('promotion-banners', PromotionBannerController::class);
+Route::get('promotion-min', [PromotionBannerController::class, 'indexMinified']);
+
 Route::apiResource('products', ProductController::class);
-Route::apiResource('product-images', ProductImageController::class);
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::apiResource('pickup-methods', PickupMethodController::class);
+    Route::apiResource('carts', CartController::class);
+    Route::post('checkouts', CheckoutController::class)->name('checkout');
+    Route::post('checkouts/complete', [CheckoutController::class, 'paymented'])->name('checkout.complete');
+
+    Route::controller(OrderController::class)->prefix('orders')->group(function () {
+        /* User Order Endpoints. Endpoint => orders/{users} */
+        Route::get('{user}', 'indexUserOrder');
+        Route::get('{user}/modal', 'showUserModalOrder');
+        Route::put('{user}/pending', 'updateOnPending');
+        Route::put('{user}/process', 'updateOnProcess');
+        Route::delete('{user}', 'destroy');
+        
+        Route::prefix('admin')->group(function () {
+            /* Retailer Order Endpoints. Endpoint => orders/admin/{users} */
+            Route::get('{user}', 'indexRetailerOrder');
+            Route::get('{user}/order/{order}', 'showRetailerDetailOrder');
+            Route::put('{user}/order/{order}', 'retailerUpdateStatus');
+        });
+    });
+});
+
+Route::get('search/navbar', [SearchController::class, 'navbar']);
+Route::get('search/products', [SearchController::class, 'product']);
+
+Route::get('categories/resource/minimal', MinimalCategoryController::class);
+Route::get('brands/resource/minimal', MinimalBrandController::class);
+Route::get('suppliers/resource/minimal', MinimalSupplierController::class);
+Route::get('retailers/resource/minimal', MinimalRetailerController::class);
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,36 @@
 <?php
 
-use App\Http\Controllers\App\Admin\CategoryController;
-use App\Http\Controllers\App\Admin\ProductController;
+use App\Http\Controllers\App\Auth\LoginController;
+use App\Http\Controllers\App\Auth\RegisterController;
+use App\Http\Controllers\App\Auth\VerifyMobileController;
+use App\Http\Controllers\DebuggingController;
+use App\Http\Controllers\Web\Admin\CategoryController;
+use App\Http\Controllers\Web\Admin\FeaturedSectionController;
+use App\Http\Controllers\Web\Admin\OrderController;
+use App\Http\Controllers\Web\Admin\ProductController;
+use App\Http\Controllers\Web\Admin\PromotionBannerController;
+use App\Http\Controllers\Web\General\GeneralController;
+use App\Http\Controllers\Web\General\SearchController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes Debug
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('test', [DebuggingController::class, 'test']);
+Route::get('execute', [DebuggingController::class, 'execute']);
+Route::get('refresh', [DebuggingController::class, 'refresh']);
+Route::get('refresh2', [DebuggingController::class, 'apiRefresh2']);
+Route::get('page/{promo}', [DebuggingController::class, 'pagePromoTest'])->name('page.promo');
+Route::get('page/{store}', [DebuggingController::class, 'pagePromoTest'])->name('page.store');
+Route::get('check-auth', [DebuggingController::class, 'checkAuth']);
 
 /*
 |--------------------------------------------------------------------------
@@ -15,22 +43,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('frontend.home.index');
+// Authentication
+Route::post('login', [LoginController::class, 'login'])->name('login.attempt');
+Route::post('verify-mobile', [VerifyMobileController::class, '__invoke'])->name('verify.mobile'); //TODO: Ganti ke method mobile
+Route::post('verify-otp', [VerifyMobileController::class, 'store'])->name('verify.otp'); //TODO: Ganti ke method otp
+Route::post('register', [RegisterController::class, 'register'])->name('register.complete');
+
+// Admin Interface
+Route::prefix('admin')->group(function() {
+    Route::resource('categories', CategoryController::class);
+    Route::get('categories/sub/{category}', [CategoryController::class, 'indexChildren'])->name('categories.sub-index');
+    Route::resource('products', ProductController::class);
+    Route::prefix('content')->group(function() {
+        Route::resource('featureds', FeaturedSectionController::class);
+        Route::resource('promotions', PromotionBannerController::class);
+    });
+    Route::prefix('orders')->group(function() {
+        Route::get('/', [OrderController::class, 'index'])->name('orders.index');
+        Route::get('{user}/order/{order}', [OrderController::class, 'show'])->name('orders.show');
+    });
 });
 
-Route::resource('categories', CategoryController::class);
-Route::get('categories/sub/{category}', [CategoryController::class, 'subIndex'])->name('categories.subIndex');
-Route::resource('products', ProductController::class);
+// General Interface
+Route::get('/', [GeneralController::class, 'homepage'])->name('homepage');
+Route::get('search', [SearchController::class, 'index'])->name('home.search');
+Route::get('products/{product}', [GeneralController::class, 'detailProduct'])->name('home.detail-product');
 
-Route::get('/orders', function () {
-    return view('admin.order.index');
+Route::middleware('auth.web')->group(function() {
+    Route::get('checkouts', [GeneralController::class, 'checkout'])->name('checkout');
+    Route::get('info/order/status', [GeneralController::class, 'order'])->name('info.order');
 });
 
 Route::get('/users', function () {
     return view('admin.users.index');
 });
 
-Route::get('/error/notfound', function () {
-    return view('livewire.pages.error-404', );
-})->name('error.404');
+Route::get('/userss', function () {
+    return view('frontend.category.index');
+})->name('coba');
+
+Route::prefix('errors')->name('error.')->group(function() {
+    Route::get('/not-found', fn() => view('livewire.pages.error-404'))->name('404');
+    Route::get('/fobidden', fn() => view('livewire.pages.error-403'))->name('403');
+});
